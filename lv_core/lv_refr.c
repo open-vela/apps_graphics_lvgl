09cb15b623cfed68863a765c5a264a17908ef224
liujinye <liujinye@xiaomi.com>
2019-01-03 11:19:45 +0800

    Merge pull request #691 from Ferruck/fix/limits-error
    lv_log: Fix type limits error

--- lv_core/lv_refr.c
+++ lv_core/lv_refr.c
@@ -16,9 +16,6 @@
 /*********************
  *      DEFINES
  *********************/
-#ifndef LV_INV_FIFO_SIZE
-#define LV_INV_FIFO_SIZE    32    /*The average count of objects on a screen */
-#endif
 
 /**********************
  *      TYPEDEFS
@@ -76,9 +73,9 @@ void lv_refr_init(void)
 
 /**
  * Redraw the invalidated areas now.
- * Normally the redrawing is periodically executed in `lv_task_handler` but a long blocking process can
+ * Normally the redarwing is peridocally executed in `lv_task_handler` but a long blocking process can
  * prevent the call of `lv_task_handler`. In this case if the the GUI is updated in the process (e.g. progress bar)
- * this function can be called when the screen should be updated.
+ * this function can be called when the screen shoud be updated.
  */
 void lv_refr_now(void)
 {
@@ -198,19 +195,7 @@ static void lv_refr_task(void * param)
 
     /* In the callback lv_obj_inv can occur
      * therefore be sure the inv_buf is cleared prior to it*/
-    if(refr_done) {
-#if LV_VDB_TRUE_DOUBLE_BUFFERED
-        lv_vdb_t * vdb_p = lv_vdb_get();
-        vdb_p->area.x1 = 0;
-        vdb_p->area.x2 = LV_HOR_RES-1;
-        vdb_p->area.y1 = 0;
-        vdb_p->area.y2 = LV_VER_RES - 1;
-
-        /*Flush the content of the VDB*/
-        lv_vdb_flush();
-#endif
-
-
+    if(refr_done != false) {
         if(monitor_cb != NULL) {
             monitor_cb(lv_tick_elaps(start), px_num);
         }
@@ -311,8 +296,6 @@ static void lv_refr_area_no_vdb(const lv_area_t * area_p)
  */
 static void lv_refr_area_with_vdb(const lv_area_t * area_p)
 {
-
-#if LV_VDB_TRUE_DOUBLE_BUFFERED == 0
     /*Calculate the max row num*/
     lv_coord_t w = lv_area_get_width(area_p);
     lv_coord_t h = lv_area_get_height(area_p);
@@ -383,14 +366,6 @@ static void lv_refr_area_with_vdb(const lv_area_t * area_p)
         /*Refresh this part too*/
         lv_refr_area_part_vdb(area_p);
     }
-#else
-    lv_vdb_t * vdb_p = lv_vdb_get();
-    vdb_p->area.x1 = 0;
-    vdb_p->area.x2 = LV_HOR_RES-1;
-    vdb_p->area.y1 = 0;
-    vdb_p->area.y2 = LV_VER_RES - 1;
-    lv_refr_area_part_vdb(area_p);
-#endif
 }
 
 /**
@@ -421,12 +396,8 @@ static void lv_refr_area_part_vdb(const lv_area_t * area_p)
     lv_refr_obj_and_children(lv_layer_top(), &start_mask);
     lv_refr_obj_and_children(lv_layer_sys(), &start_mask);
 
-    /* In true double buffered mode flush only once when all areas were rendered.
-     * In normal mode flush after every area */
-#if LV_VDB_TRUE_DOUBLE_BUFFERED == 0
     /*Flush the content of the VDB*/
     lv_vdb_flush();
-#endif
 }
 
 #endif /*LV_VDB_SIZE == 0*/
@@ -543,9 +514,11 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
     if(union_ok != false) {
 
         /* Redraw the object */
-        obj->design_func(obj, &obj_ext_mask, LV_DESIGN_DRAW_MAIN);
-        //usleep(5 * 1000);  /*DEBUG: Wait after every object draw to see the order of drawing*/
-
+        lv_style_t * style = lv_obj_get_style(obj);
+        if(style->body.opa != LV_OPA_TRANSP) {
+            obj->design_func(obj, &obj_ext_mask, LV_DESIGN_DRAW_MAIN);
+            //tick_wait_ms(100);  /*DEBUG: Wait after every object draw to see the order of drawing*/
+        }
 
         /*Create a new 'obj_mask' without 'ext_size' because the children can't be visible there*/
         lv_obj_get_coords(obj, &obj_area);
@@ -574,7 +547,8 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
         }
 
         /* If all the children are redrawn make 'post draw' design */
-        obj->design_func(obj, &obj_ext_mask, LV_DESIGN_DRAW_POST);
-
+        if(style->body.opa != LV_OPA_TRANSP) {
+            obj->design_func(obj, &obj_ext_mask, LV_DESIGN_DRAW_POST);
+        }
     }
 }
