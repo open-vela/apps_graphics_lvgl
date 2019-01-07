085b67ba836c76ddabb8a686490fa21f945624f4
liujinye <liujinye@xiaomi.com>
2019-01-07 11:31:27 +0800

    Merge pull request #713 from joltwallet/roller_visible_row_count_align_fix
    Roller visible height fix

--- lv_draw/lv_draw_label.c
+++ lv_draw/lv_draw_label.c
@@ -185,9 +185,7 @@ void lv_draw_label(const lv_area_t * coords, const lv_area_t * mask, const lv_st
             letter_fp(&pos, mask, font, letter, color, opa);
             letter_w = lv_font_get_width(font, letter);
 
-            if(letter_w > 0){
-                pos.x += letter_w + style->text.letter_space;
-            }
+            pos.x += letter_w + style->text.letter_space;
         }
         /*Go to next line*/
         line_start = line_end;
