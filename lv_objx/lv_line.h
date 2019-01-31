e991fa8ae28b8e44801507d550fda34ad4c3632c
openvela-robot <openvela-robot@xiaomi.com>
2019-01-31 10:20:11 +0800

    Update README.md

--- lv_objx/lv_line.h
+++ lv_objx/lv_line.h
@@ -70,20 +70,18 @@ void lv_line_set_points(lv_obj_t * line, const lv_point_t * point_a, uint16_t po
  * Enable (or disable) the auto-size option. The size of the object will fit to its points.
  * (set width to x max and height to y max)
  * @param line pointer to a line object
- * @param en true: auto size is enabled, false: auto size is disabled
+ * @param autosize_en true: auto size is enabled, false: auto size is disabled
  */
-void lv_line_set_auto_size(lv_obj_t * line, bool en);
+void lv_line_set_auto_size(lv_obj_t * line, bool autosize_en);
 
 /**
  * Enable (or disable) the y coordinate inversion.
  * If enabled then y will be subtracted from the height of the object,
  * therefore the y=0 coordinate will be on the bottom.
  * @param line pointer to a line object
- * @param en true: enable the y inversion, false:disable the y inversion
+ * @param yinv_en true: enable the y inversion, false:disable the y inversion
  */
-void lv_line_set_y_invert(lv_obj_t * line, bool en);
-
-#define lv_line_set_y_inv lv_line_set_y_invert      /*The name was inconsistent. In v.6.0 only `lv_line_set_y_invert`will work */
+void lv_line_set_y_invert(lv_obj_t * line, bool yinv_en);
 
 /**
  * Set the style of a line
@@ -121,7 +119,7 @@ bool lv_line_get_auto_size(const lv_obj_t * line);
  * @param line pointer to a line object
  * @return true: y inversion is enabled, false: disabled
  */
-bool lv_line_get_y_invert(const lv_obj_t * line);
+bool lv_line_get_y_inv(const lv_obj_t * line);
 
 /**
  * Get the style of an line object
