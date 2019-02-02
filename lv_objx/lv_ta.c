f3dd18c54c67db1e1d06485aaa8fab9aa595ed0a
liujinye <liujinye@xiaomi.com>
2019-02-02 22:45:02 +0800

    Changed POS1 to HOME

--- lv_objx/lv_ta.c
+++ lv_objx/lv_ta.c
@@ -351,6 +351,17 @@ void lv_ta_del_char(lv_obj_t * ta)
     lv_ta_set_cursor_pos(ta, ext->cursor.pos - 1);
 }
 
+/**
+ * Delete the right character from the current cursor position
+ * @param ta pointer to a text area object
+ */
+void lv_ta_del_char_forward(lv_obj_t * ta)
+{
+	uint16_t cp = lv_ta_get_cursor_pos(ta);
+	lv_ta_set_cursor_pos(ta, cp + 1);
+	if(cp != lv_ta_get_cursor_pos(ta)) lv_ta_del_char(ta);
+}
+
 /*=====================
  * Setter functions
  *====================*/
@@ -1051,11 +1062,9 @@ static lv_res_t lv_ta_signal(lv_obj_t * ta, lv_signal_t sign, void * param)
         else if(c == LV_GROUP_KEY_UP)   lv_ta_cursor_up(ta);
         else if(c == LV_GROUP_KEY_DOWN) lv_ta_cursor_down(ta);
         else if(c == LV_GROUP_KEY_BACKSPACE) lv_ta_del_char(ta);
-        else if(c == LV_GROUP_KEY_DEL)  {
-            uint16_t cp = lv_ta_get_cursor_pos(ta);
-            lv_ta_set_cursor_pos(ta, cp + 1);
-            if(cp != lv_ta_get_cursor_pos(ta)) lv_ta_del_char(ta);
-        }
+        else if(c == LV_GROUP_KEY_DEL)  lv_ta_del_char_forward(ta);
+        else if(c == LV_GROUP_KEY_HOME) lv_ta_set_cursor_pos(ta, 0);
+        else if(c == LV_GROUP_KEY_END)  lv_ta_set_cursor_pos(ta, LV_TA_CURSOR_LAST);
         else {
             lv_ta_add_char(ta, c);
         }
@@ -1091,30 +1100,6 @@ static lv_res_t lv_ta_signal(lv_obj_t * ta, lv_signal_t sign, void * param)
         }
 #endif
     }
-    else if(sign == LV_SIGNAL_PRESSED) {
-        lv_indev_t * indev = (lv_indev_t *)param;
-        lv_area_t label_coords;
-        uint16_t index_of_char_at_position;
-
-        lv_obj_get_coords(ext->label, &label_coords);
-
-        lv_point_t relative_position;
-        relative_position.x = indev->proc.act_point.x - label_coords.x1;
-        relative_position.y = indev->proc.act_point.y - label_coords.y1;
-
-        lv_coord_t label_width = lv_obj_get_width(ext->label);
-
-        /*Check if the click happened on the left side of the area outside the label*/
-        if (relative_position.x < label_width / 2) {
-            index_of_char_at_position = 0;
-        }
-        /*Check if the click happened on the right side of the area outside the label*/
-        else if (relative_position.x >=  label_width / 2) {
-            index_of_char_at_position = LV_TA_CURSOR_LAST;
-        }
-
-        lv_ta_set_cursor_pos(ta, index_of_char_at_position);
-    }
     return res;
 }
 
@@ -1133,44 +1118,14 @@ static lv_res_t lv_ta_scrollable_signal(lv_obj_t * scrl, lv_signal_t sign, void 
     res = scrl_signal(scrl, sign, param);
     if(res != LV_RES_OK) return res;
 
-    lv_obj_t * ta = lv_obj_get_parent(scrl);
-    lv_ta_ext_t * ext = lv_obj_get_ext_attr(ta);
     if(sign == LV_SIGNAL_REFR_EXT_SIZE) {
         /*Set ext. size because the cursor might be out of this object*/
+        lv_obj_t * ta = lv_obj_get_parent(scrl);
+        lv_ta_ext_t * ext = lv_obj_get_ext_attr(ta);
         lv_style_t * style_label = lv_obj_get_style(ext->label);
         lv_coord_t font_h = lv_font_get_height(style_label->text.font);
         scrl->ext_size = LV_MATH_MAX(scrl->ext_size, style_label->text.line_space + font_h);
     }
-    else if(sign == LV_SIGNAL_PRESSED) {
-        lv_indev_t * indev = (lv_indev_t *)param;
-        lv_area_t label_coords;
-        uint16_t index_of_char_at_position;
-
-        lv_obj_get_coords(ext->label, &label_coords);
-
-        lv_point_t relative_position;
-        relative_position.x = indev->proc.act_point.x - label_coords.x1;
-        relative_position.y = indev->proc.act_point.y - label_coords.y1;
-
-
-        lv_coord_t label_width = lv_obj_get_width(ext->label);
-
-        /*Check if the click happened on the left side of the area outside the label*/
-        if (relative_position.x < 0) {
-            index_of_char_at_position = 0;
-        }
-        /*Check if the click happened on the right side of the area outside the label*/
-        else if (relative_position.x >= label_width) {
-            index_of_char_at_position = LV_TA_CURSOR_LAST;
-        }
-        else {
-            index_of_char_at_position = lv_label_get_letter_on(ext->label, &relative_position);
-
-        }
-
-        lv_ta_set_cursor_pos(ta, index_of_char_at_position);
-    }
-
 
     return res;
 }
