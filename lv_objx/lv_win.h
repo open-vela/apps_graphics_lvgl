b7b95a02411af90749f5e5a4f0ceb26cd4535c2e
openvela-robot <openvela-robot@xiaomi.com>
2019-02-27 06:17:37 +0800

    btnm fixes

--- lv_objx/lv_win.h
+++ lv_objx/lv_win.h
@@ -105,10 +105,10 @@ void lv_win_clean(lv_obj_t *obj);
  * Add control button to the header of the window
  * @param win pointer to a window object
  * @param img_src an image source ('lv_img_t' variable, path to file or a symbol)
- * @param event_cb specify the an event handler function. NULL if unused
+ * @param rel_action a function pointer to call when the button is released
  * @return pointer to the created button object
  */
-lv_obj_t * lv_win_add_btn(lv_obj_t * win, const void * img_src, lv_event_cb_t event_cb);
+lv_obj_t * lv_win_add_btn(lv_obj_t * win, const void * img_src, lv_action_t rel_action);
 
 /*=====================
  * Setter functions
