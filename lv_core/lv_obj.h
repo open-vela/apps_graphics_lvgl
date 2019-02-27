68e36a50fb0adbeff08c3c089b6a7843db2fd4b0
openvela-robot <openvela-robot@xiaomi.com>
2019-02-27 10:55:39 +0800

    Apply ta label size fix from dev-6.0

--- lv_core/lv_obj.h
+++ lv_core/lv_obj.h
@@ -27,20 +27,32 @@ extern "C" {
 #include "../lv_misc/lv_ll.h"
 #include "../lv_misc/lv_color.h"
 #include "../lv_misc/lv_log.h"
-#include "../lv_hal/lv_hal.h"
 
 /*********************
  *      DEFINES
  *********************/
 
 /*Error check of lv_conf.h*/
-#if LV_HOR_RES_MAX == 0 || LV_VER_RES_MAX == 0
+#if LV_HOR_RES == 0 || LV_VER_RES == 0
 #error "LittlevGL: LV_HOR_RES and LV_VER_RES must be greater than 0"
 #endif
 
 #if LV_ANTIALIAS > 1
 #error "LittlevGL: LV_ANTIALIAS can be only 0 or 1"
 #endif
+
+#if LV_VDB_SIZE == 0 && LV_ANTIALIAS != 0
+#error "LittlevGL: If LV_VDB_SIZE == 0 the anti-aliasing must be disabled"
+#endif
+
+#if LV_VDB_SIZE > 0 && LV_VDB_SIZE < LV_HOR_RES
+#error "LittlevGL: Small Virtual Display Buffer (lv_conf.h: LV_VDB_SIZE >= LV_HOR_RES)"
+#endif
+
+#if LV_VDB_SIZE == 0 && USE_LV_REAL_DRAW == 0
+#error "LittlevGL: If LV_VDB_SIZE = 0 Real drawing function are required (lv_conf.h: USE_LV_REAL_DRAW 1)"
+#endif
+
 
 #define LV_ANIM_IN              0x00    /*Animation to show an object. 'OR' it with lv_anim_builtin_t*/
 #define LV_ANIM_OUT             0x80    /*Animation to hide an object. 'OR' it with lv_anim_builtin_t*/
@@ -62,7 +74,7 @@ enum
 };
 typedef uint8_t lv_design_mode_t;
 
-typedef bool (* lv_design_cb_t) (struct _lv_obj_t * obj, const lv_area_t * mask_p, lv_design_mode_t mode);
+typedef bool (* lv_design_func_t) (struct _lv_obj_t * obj, const lv_area_t * mask_p, lv_design_mode_t mode);
 
 enum
 {
@@ -71,39 +83,15 @@ enum
 };
 typedef uint8_t lv_res_t;
 
-
-typedef enum {
-    LV_EVENT_PRESSED,
-    LV_EVENT_PRESSING,
-    LV_EVENT_PRESS_LOST,
-    LV_EVENT_RELEASED,
-    LV_EVENT_CLICKED,
-    LV_EVENT_LONG_PRESSED,
-    LV_EVENT_LONG_PRESSED_REPEAT,
-    LV_EVENT_LONG_HOVER_IN,
-    LV_EVENT_LONG_HOVER_OUT,
-    LV_EVENT_DRAG_BEGIN,
-    LV_EVENT_DRAG_END,
-    LV_EVENT_DRAG_THROW_BEGIN,
-    LV_EVENT_FOCUSED,
-    LV_EVENT_DEFOCUSED,
-    LV_EVENT_VALUE_CHANGED,
-    LV_EVENT_REFRESH,
-    LV_EVENT_APPLY,         /*"Ok", "Apply" or similar specific button has clicked*/
-    LV_EVENT_CANCEL,        /*"Close", "Cancel" or similar specific button has clicked*/
-}lv_event_t;
-
-typedef void (*lv_event_cb_t)(struct _lv_obj_t * obj, lv_event_t event);
-
 enum
 {
     /*General signals*/
     LV_SIGNAL_CLEANUP,
     LV_SIGNAL_CHILD_CHG,
     LV_SIGNAL_CORD_CHG,
-    LV_SIGNAL_PARENT_SIZE_CHG,
     LV_SIGNAL_STYLE_CHG,
     LV_SIGNAL_REFR_EXT_SIZE,
+    LV_SIGNAL_LANG_CHG,
     LV_SIGNAL_GET_TYPE,
 
 	_LV_SIGNAL_FEEDBACK_SECTION_START,
@@ -126,7 +114,7 @@ enum
 };
 typedef uint8_t lv_signal_t;
 
-typedef lv_res_t (* lv_signal_cb_t) (struct _lv_obj_t * obj, lv_signal_t sign, void * param);
+typedef lv_res_t (* lv_signal_func_t) (struct _lv_obj_t * obj, lv_signal_t sign, void * param);
 
 enum
 {
@@ -173,9 +161,8 @@ typedef struct _lv_obj_t
 
     lv_area_t coords;               /*Coordinates of the object (x1, y1, x2, y2)*/
 
-    lv_event_cb_t event_cb;
-    lv_signal_cb_t signal_cb;     /*Object type specific signal function*/
-    lv_design_cb_t design_cb;     /*Object type specific design function*/
+    lv_signal_func_t signal_func;     /*Object type specific signal function*/
+    lv_design_func_t design_func;     /*Object type specific design function*/
 
     void * ext_attr;                 /*Object type specific extended data*/
     lv_style_t * style_p;       /*Pointer to the object's style*/
@@ -207,6 +194,8 @@ typedef struct _lv_obj_t
     LV_OBJ_FREE_NUM_TYPE free_num;          /*Application specific identifier (set it freely)*/
 #endif
 } lv_obj_t;
+
+typedef lv_res_t (*lv_action_t) (struct _lv_obj_t * obj);
 
 /*Protect some attributes (max. 8 bit)*/
 enum
@@ -283,6 +272,16 @@ void lv_obj_invalidate(const lv_obj_t * obj);
 /*=====================
  * Setter functions
  *====================*/
+
+/*--------------
+ * Screen set
+ *--------------*/
+
+/**
+ * Load a new screen
+ * @param scr pointer to a screen
+ */
+void lv_scr_load(lv_obj_t * scr);
 
 /*--------------------
  * Parent/children set
@@ -485,41 +484,19 @@ void lv_obj_set_protect(lv_obj_t * obj, uint8_t prot);
 void lv_obj_clear_protect(lv_obj_t * obj, uint8_t prot);
 
 /**
- * Set a an event handler function for an object.
- * Used by the user to react on event which happens with the object.
- * @param obj pointer to an object
- * @param cb the new event function
- */
-void lv_obj_set_event_cb(lv_obj_t * obj, lv_event_cb_t cb);
-
-/**
- * Send an event to the object
- * @param obj pointer to an object
- * @param event the type of the event from `lv_event_t`.
- */
-void lv_obj_send_event(lv_obj_t * obj, lv_event_t event);
-
-/**
- * Set the a signal function of an object. Used internally by the library.
+ * Set the signal function of an object.
  * Always call the previous signal function in the new.
  * @param obj pointer to an object
- * @param cb the new signal function
+ * @param fp the new signal function
  */
-void lv_obj_set_signal_cb(lv_obj_t * obj, lv_signal_cb_t cb);
-
-/**
- * Send an event to the object
- * @param obj pointer to an object
- * @param event the type of the event from `lv_event_t`.
- */
-void lv_obj_send_signal(lv_obj_t * obj, lv_signal_t signal, void * param);
+void lv_obj_set_signal_func(lv_obj_t * obj, lv_signal_func_t fp);
 
 /**
  * Set a new design function for an object
  * @param obj pointer to an object
- * @param cb the new design function
+ * @param fp the new design function
  */
-void lv_obj_set_design_cb(lv_obj_t * obj, lv_design_cb_t cb);
+void lv_obj_set_design_func(lv_obj_t * obj, lv_design_func_t fp);
 
 /*----------------
  * Other set
@@ -575,19 +552,35 @@ void lv_obj_animate(lv_obj_t * obj, lv_anim_builtin_t type, uint16_t time, uint1
  * Getter functions
  *======================*/
 
+/*------------------
+ * Screen get
+ *-----------------*/
+
+/**
+ * Return with a pointer to the active screen
+ * @return pointer to the active screen object (loaded by 'lv_scr_load()')
+ */
+lv_obj_t * lv_scr_act(void);
+
+/**
+ * Return with the top layer. (Same on every screen and it is above the normal screen layer)
+ * @return pointer to the top layer object  (transparent screen sized lv_obj)
+ */
+lv_obj_t * lv_layer_top(void);
+
+/**
+ * Return with the system layer. (Same on every screen and it is above the all other layers)
+ * It is used for example by the cursor
+ * @return pointer to the system layer object (transparent screen sized lv_obj)
+ */
+lv_obj_t * lv_layer_sys(void);
+
 /**
  * Return with the screen of an object
  * @param obj pointer to an object
  * @return pointer to a screen
  */
 lv_obj_t * lv_obj_get_screen(const lv_obj_t * obj);
-
-/**
- * Get the display of an object
- * @param scr pointer to an object
- * @return pointer the object's display
- */
-lv_disp_t * lv_obj_get_disp(const lv_obj_t * obj);
 
 /*---------------------
  * Parent/children get
@@ -770,14 +763,14 @@ bool lv_obj_is_protected(const lv_obj_t * obj, uint8_t prot);
  * @param obj pointer to an object
  * @return the signal function
  */
-lv_signal_cb_t lv_obj_get_signal_func(const lv_obj_t * obj);
+lv_signal_func_t lv_obj_get_signal_func(const lv_obj_t * obj);
 
 /**
  * Get the design function of an object
  * @param obj pointer to an object
  * @return the design function
  */
-lv_design_cb_t lv_obj_get_design_func(const lv_obj_t * obj);
+lv_design_func_t lv_obj_get_design_func(const lv_obj_t * obj);
 
 /*------------------
  * Other get
