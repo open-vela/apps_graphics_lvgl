d50de06c37e869ff58cefc07137b7a1c6bfd77c2
liujinye <liujinye@xiaomi.com>
2019-02-28 09:40:11 +0800

    Merge pull request #916 from manison/patch/fix-ta-variable-hides-arg
    Remove unnecessary variable definition

--- lv_objx/lv_cb.c
+++ lv_objx/lv_cb.c
@@ -30,9 +30,9 @@ static lv_res_t lv_cb_signal(lv_obj_t * cb, lv_signal_t sign, void * param);
 /**********************
  *  STATIC VARIABLES
  **********************/
-static lv_design_cb_t ancestor_bg_design;
-static lv_design_cb_t ancestor_bullet_design;
-static lv_signal_cb_t ancestor_signal;
+static lv_design_func_t ancestor_bg_design;
+static lv_design_func_t ancestor_bullet_design;
+static lv_signal_func_t ancestor_signal;
 
 /**********************
  *      MACROS
@@ -68,8 +68,8 @@ lv_obj_t * lv_cb_create(lv_obj_t * par, const lv_obj_t * copy)
     ext->bullet = NULL;
     ext->label = NULL;
 
-    lv_obj_set_signal_cb(new_cb, lv_cb_signal);
-    lv_obj_set_design_cb(new_cb, lv_cb_design);
+    lv_obj_set_signal_func(new_cb, lv_cb_signal);
+    lv_obj_set_design_func(new_cb, lv_cb_design);
 
     /*Init the new checkbox object*/
     if(copy == NULL) {
@@ -107,7 +107,7 @@ lv_obj_t * lv_cb_create(lv_obj_t * par, const lv_obj_t * copy)
         lv_obj_refresh_style(new_cb);
     }
 
-    lv_obj_set_design_cb(ext->bullet, lv_bullet_design);
+    lv_obj_set_design_func(ext->bullet, lv_bullet_design);
 
 
     LV_LOG_INFO("check box created");
@@ -273,13 +273,13 @@ static bool lv_bullet_design(lv_obj_t * bullet, const lv_area_t * mask, lv_desig
     } else if(mode == LV_DESIGN_DRAW_MAIN) {
 #if USE_LV_GROUP
         /* If the check box is the active in a group and
-         * the background is not visible (transparent)
+         * the background is not visible (transparent or empty)
          * then activate the style of the bullet*/
         lv_style_t * style_ori = lv_obj_get_style(bullet);
         lv_obj_t * bg = lv_obj_get_parent(bullet);
         lv_style_t * style_page = lv_obj_get_style(bg);
         lv_group_t * g = lv_obj_get_group(bg);
-        if(style_page->body.opa == LV_OPA_TRANSP) { /*Is the Background visible?*/
+        if(style_page->body.empty != 0 || style_page->body.opa == LV_OPA_TRANSP) { /*Background is visible?*/
             if(lv_group_get_focused(g) == bg) {
                 lv_style_t * style_mod;
                 style_mod = lv_group_mod_style(g, style_ori);
