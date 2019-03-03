6763d2c4887827be3d68c1a2aa747c8299e0bec3
liujinye <liujinye@xiaomi.com>
2019-03-03 11:20:49 +0800

    focus/defocus event updates

--- lv_core/lv_group.h
+++ lv_core/lv_group.h
@@ -36,6 +36,8 @@ extern "C" {
 #define LV_GROUP_KEY_ENTER          10      /*0x0A, '\n'*/
 #define LV_GROUP_KEY_NEXT           9       /*0x09, '\t'*/
 #define LV_GROUP_KEY_PREV           11      /*0x0B, '*/
+#define LV_GROUP_KEY_HOME           2       /*0x02, STX*/
+#define LV_GROUP_KEY_END            3       /*0x03, ETX*/
 
 #if USE_LV_GROUP  != 0
 /**********************
@@ -43,7 +45,7 @@ extern "C" {
  **********************/
 struct _lv_group_t;
 
-typedef void (*lv_group_style_mod_func_t)(lv_style_t *);
+typedef void (*lv_group_style_mod_func_t)(struct _lv_group_t *, lv_style_t *);
 typedef void (*lv_group_focus_cb_t)(struct _lv_group_t *);
 
 typedef struct _lv_group_t
@@ -54,6 +56,16 @@ typedef struct _lv_group_t
     lv_group_style_mod_func_t style_mod_edit;/*A function which modifies the style of the focused object*/
     lv_group_focus_cb_t focus_cb;           /*A function to call when a new object is focused (optional)*/
     lv_style_t style_tmp;                   /*Stores the modified style of the focused object */
+#if USE_LV_USER_DATA_SINGLE
+    lv_group_user_data_t    user_data;
+#endif
+
+#if USE_LV_USER_DATA_MULTI
+    lv_group_user_data_t    focus_user_data;
+    lv_group_user_data_t    style_mod_user_data;
+    lv_group_user_data_t    style_mod_edit_user_data;
+#endif
+
     uint8_t frozen          :1;             /*1: can't focus to new object*/
     uint8_t editing         :1;             /*1: Edit mode, 0: Navigate mode*/
     uint8_t click_focus     :1;             /*1: If an object in a group is clicked by an indev then it will be focused */
@@ -69,6 +81,12 @@ typedef enum _lv_group_refocus_policy_t {
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
+
+/**
+* Init. the group module
+* @remarks Internal function, do not call directly.
+*/
+void lv_group_init(void);
 
 /**
  * Create a new object group
@@ -233,6 +251,12 @@ bool lv_group_get_click_focus(const lv_group_t * group);
  * @param en: true: wrapping enabled; false: wrapping disabled
  */
 bool lv_group_get_wrap(lv_group_t * group);
+
+/**
+ * Notify the group that current theme changed and style modification callbacks need to be refreshed.
+ * @param group pointer to group. If NULL then all groups are notified.
+ */
+void lv_group_report_style_mod(lv_group_t * group);
 
 /**********************
  *      MACROS
