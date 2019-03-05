d6d082c993a8dca67788d021d8e11a2fb9d0fa42
openvela-robot <openvela-robot@xiaomi.com>
2019-03-05 15:34:36 +0800

    Merge branch 'master' of https://github.com/littlevgl/lvgl

--- lv_objx/lv_sw.h
+++ lv_objx/lv_sw.h
@@ -32,7 +32,7 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
-#define LV_SW_MAX_VALUE 100
+#define LV_SWITCH_SLIDER_ANIM_MAX 1000
 
 /**********************
  *      TYPEDEFS
@@ -79,24 +79,50 @@ lv_obj_t * lv_sw_create(lv_obj_t * par, const lv_obj_t * copy);
 /**
  * Turn ON the switch
  * @param sw pointer to a switch object
- * @param anim true: set the value with an animation; false: change the value immediatelly
  */
-void lv_sw_on(lv_obj_t *sw, bool anim);
+void lv_sw_on(lv_obj_t *sw);
 
 /**
  * Turn OFF the switch
  * @param sw pointer to a switch object
- * @param anim true: set the value with an animation; false: change the value immediatelly
  */
-void lv_sw_off(lv_obj_t *sw, bool anim);
+void lv_sw_off(lv_obj_t *sw);
 
 /**
  * Toggle the position of the switch
  * @param sw pointer to a switch object
- * @param anim true: set the value with an animation; false: change the value immediatelly
  * @return resulting state of the switch.
  */
-bool lv_sw_toggle(lv_obj_t *sw, bool anim);
+bool lv_sw_toggle(lv_obj_t *sw);
+
+/**
+ * Turn ON the switch with an animation
+ * @param sw pointer to a switch object
+ */
+void lv_sw_on_anim(lv_obj_t * sw);
+
+/**
+ * Turn OFF the switch with an animation
+ * @param sw pointer to a switch object
+ */
+void lv_sw_off_anim(lv_obj_t * sw);
+
+/**
+ * Toggle the position of the switch with an animation
+ * @param sw pointer to a switch object
+ * @return resulting state of the switch.
+ */
+bool lv_sw_toggle_anim(lv_obj_t *sw);
+
+/**
+ * Set a function which will be called when the switch is toggled by the user
+ * @param sw pointer to switch object
+ * @param action a callback function
+ */
+static inline void lv_sw_set_action(lv_obj_t * sw, lv_action_t action)
+{
+    lv_slider_set_action(sw, action);
+}
 
 /**
  * Set a style of a switch
@@ -127,7 +153,17 @@ void lv_sw_set_anim_time(lv_obj_t *sw, uint16_t anim_time);
  */
 static inline bool lv_sw_get_state(const lv_obj_t *sw)
 {
-    return lv_bar_get_value(sw) < LV_SW_MAX_VALUE / 2 ? false : true;
+    return lv_bar_get_value(sw) < LV_SWITCH_SLIDER_ANIM_MAX / 2 ? false : true;
+}
+
+/**
+ * Get the switch action function
+ * @param slider pointer to a switch object
+ * @return the callback function
+ */
+static inline lv_action_t lv_sw_get_action(const lv_obj_t * slider)
+{
+    return lv_slider_get_action(slider);
 }
 
 /**
