8fdb9ba9d7fcf3886298b7e091727cb008e8b122
openvela-robot <openvela-robot@xiaomi.com>
2019-03-06 22:59:58 +0800

    marge master

--- lv_objx/lv_img.h
+++ lv_objx/lv_img.h
@@ -40,12 +40,9 @@ typedef struct
     /*No inherited ext. because inherited from the base object*/ /*Ext. of ancestor*/
     /*New data for this type */
     const void * src;             /*Image source: Pointer to an array or a file or a symbol*/
-    lv_point_t offset;
+
     lv_coord_t w;               /*Width of the image (Handled by the library)*/
     lv_coord_t h;               /*Height of the image (Handled by the library)*/
-#if USE_LV_MULTI_LANG
-    uint16_t lang_txt_id;       /*The ID of the image to display. */
-#endif
     uint8_t src_type  :2;       /*See: lv_img_src_t*/
     uint8_t auto_size :1;       /*1: automatically set the object size to the image size*/
     uint8_t cf :5;              /*Color format from `lv_img_color_format_t`*/
@@ -74,15 +71,6 @@ lv_obj_t * lv_img_create(lv_obj_t * par, const lv_obj_t * copy);
  */
 void lv_img_set_src(lv_obj_t * img, const void * src_img);
 
-#if USE_LV_MULTI_LANG
-/**
- * Set an ID which means a the same source but on different languages
- * @param img pointer to an image object
- * @param src_id ID of the source
- */
-void lv_img_set_src_id(lv_obj_t * img, uint32_t txt_id);
-#endif
-
 /**
  * Obsolete since v5.1. Just for compatibility with v5.0. Will be removed in v6.0.
  * Use 'lv_img_set_src()' instead.
@@ -103,31 +91,6 @@ static inline void lv_img_set_file(lv_obj_t * img, const char * fn)
  */
 void lv_img_set_auto_size(lv_obj_t * img, bool autosize_en);
 
-/**
- * Set an offset for the source of an image.
- * so the image will be displayed from the new origin.
- * @param img pointer to an image
- * @param x: the new offset along x axis.
- * @param y: the new offset along y axis.
- */
-void lv_img_set_offset(lv_obj_t *img, lv_coord_t x, lv_coord_t y);
-
-/**
- * Set an offset for the source of an image.
- * so the image will be displayed from the new origin.
- * @param img pointer to an image
- * @param x: the new offset along x axis.
- */
-void lv_img_set_offset_x(lv_obj_t *img, lv_coord_t x);
-
-/**
- * Set an offset for the source of an image.
- * so the image will be displayed from the new origin.
- * @param img pointer to an image
- * @param y: the new offset along y axis.
- */
-void lv_img_set_offset_y(lv_obj_t *img, lv_coord_t y);
-    
 /**
  * Set the style of an image
  * @param img pointer to an image object
@@ -166,15 +129,6 @@ const void * lv_img_get_src(lv_obj_t * img);
  * @return file name
  */
 const char * lv_img_get_file_name(const lv_obj_t * img);
-
-#if USE_LV_MULTI_LANG
-/**
- * Get the source ID of the image. (Used by the multi-language feature)
- * @param img pointer to an image
- * @return ID of the source
- */
-uint16_t lv_img_get_src_id(lv_obj_t * img);
-#endif
 
 /**
  * Get the auto size enable attribute
