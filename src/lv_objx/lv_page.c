6dd2944e280f186c6551e27f86e97c7f73afd1ba
openvela-robot <openvela-robot@xiaomi.com>
2019-03-22 21:27:01 +0800

    Merge branch 'new_tabview_scroll' of github.com:littlevgl/lvgl into new_tabview_scroll

--- src/lv_objx/lv_page.c
+++ src/lv_objx/lv_page.c
@@ -6,6 +6,7 @@
 /*********************
  *      INCLUDES
  *********************/
+#include <stdio.h>
 #include "../lv_objx/lv_page.h"
 #if LV_USE_PAGE != 0
 
@@ -386,6 +387,37 @@ lv_style_t * lv_page_get_style(const lv_obj_t * page, lv_page_style_t type)
 /*=====================
  * Other functions
  *====================*/
+
+/**
+ * Find whether the page has been scrolled to a certain edge.
+ * @param page Page object
+ * @param edge Edge to check
+ * @return true if the page is on the specified edge
+ */
+bool lv_page_on_edge(lv_obj_t *page, lv_page_edge_t edge) {
+	lv_style_t * page_style = lv_obj_get_style(page);
+	lv_obj_t * scrl = lv_page_get_scrl(page);
+	lv_area_t page_coords;
+	lv_area_t scrl_coords;
+
+	lv_obj_get_coords(scrl, &scrl_coords);
+	lv_obj_get_coords(page, &page_coords);
+
+	if(edge == LV_PAGE_EDGE_LEFT) {
+		printf("scrl_coords.x1 %d\n", scrl_coords.x1);
+		printf("page_coords.x1 + page_style->body.padding.left %d\n", page_coords.x1 + page_style->body.padding.left);
+	}
+	if(edge == LV_PAGE_EDGE_TOP && scrl_coords.y1 == page_coords.y1 + page_style->body.padding.top)
+		return true;
+	else if(edge == LV_PAGE_EDGE_BOTTOM && scrl_coords.y2 == page_coords.y2 - page_style->body.padding.bottom)
+		return true;
+	else if(edge == LV_PAGE_EDGE_LEFT && scrl_coords.x1 == page_coords.x1 + page_style->body.padding.left)
+		return true;
+	else if(edge == LV_PAGE_EDGE_RIGHT && scrl_coords.x2 == page_coords.x2 - page_style->body.padding.right)
+		return true;
+
+	return false;
+}
 
 /**
  * Glue the object to the page. After it the page can be moved (dragged) with this object too.
@@ -917,7 +949,7 @@ static lv_res_t lv_page_scrollable_signal(lv_obj_t * scrl, lv_signal_t sign, voi
                 }
             }
             else if(scrl_coords.x1 > page_coords.x1 + page_style->body.padding.left) {
-                new_x = hpad;  /*Left align*/
+                new_x = page_style->body.padding.left;  /*Left align*/
                 refr_x = true;
                 if(page_ext->edge_flash.enabled &&
                         page_ext->edge_flash.left_ip == 0 && page_ext->edge_flash.right_ip == 0 &&
@@ -928,7 +960,7 @@ static lv_res_t lv_page_scrollable_signal(lv_obj_t * scrl, lv_signal_t sign, voi
             }
         }
 
-        /*scrollable height smaller then page height? -> align to left*/
+        /*scrollable height smaller then page height? -> align to top*/
         if(lv_area_get_height(&scrl_coords) + vpad <= lv_area_get_height(&page_coords)) {
             if(scrl_coords.y1 != page_coords.y1 + page_style->body.padding.top) {
                 new_y = page_style->body.padding.top;
