b7c2a563d428d5c1a1856e0095c9f45145d1dbeb
openvela-robot <openvela-robot@xiaomi.com>
2019-05-17 08:47:18 +0800

    Merge remote-tracking branch 'origin/dev-6.0' into count_children_recursive

--- src/lv_objx/lv_list.c
+++ src/lv_objx/lv_list.c
@@ -288,8 +288,7 @@ void lv_list_set_single_mode(lv_obj_t * list, bool mode)
 /**
  * Make a button selected
  * @param list pointer to a list object
- * @param btn pointer to a button to select
- *            NULL to not select any buttons
+ * @param btn pointer to a button to selectthe
  */
 void lv_list_set_btn_selected(lv_obj_t * list, lv_obj_t * btn)
 {
@@ -304,9 +303,6 @@ void lv_list_set_btn_selected(lv_obj_t * list, lv_obj_t * btn)
     }
 
     ext->selected_btn = btn;
-
-    /*Don't forgat whci hbutton was selected.
-     * It will be restored when the list is focused.*/
     if(btn != NULL) {
         ext->last_sel = btn;
     }
