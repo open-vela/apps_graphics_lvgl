6e21f8f4e78841afde320d4a9d5283679e27c3d2
openvela-robot <openvela-robot@xiaomi.com>
2019-05-29 05:31:03 +0800

    merge new font API

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -145,11 +145,11 @@ typedef void * lv_group_user_data_t;
 /* 1: Enable alpha indexed images */
 #define LV_IMG_CF_ALPHA     1
 
-/*1: Add a `user_data` to drivers and objects*/
-#define LV_USE_USER_DATA_SINGLE 1
+/*Declare the type of the user data of image decoder (can be e.g. `void *`, `int`, `struct`)*/
+typedef void * lv_img_decoder_user_data_t;
 
-/*1: Add separate `user_data` for every callback*/
-#define LV_USE_USER_DATA_MULTI  0
+/*1: Add a `user_data` to drivers and objects*/
+#define LV_USE_USER_DATA 1
 
 /*=====================
  *  Compiler settings
@@ -164,7 +164,6 @@ typedef void * lv_group_user_data_t;
  * 4 or 8 byte boundary. This alignment will be explicitly applied where needed.
  * E.g. __attribute__((aligned(4))) */
 #define LV_ATTRIBUTE_MEM_ALIGN
-
 
 /* 1: Variable length array is supported*/
 #define LV_COMPILER_VLA_SUPPORTED            1
@@ -395,7 +394,7 @@ typedef void * lv_obj_user_data_t;
 /*Page (dependencies: lv_cont)*/
 #define LV_USE_PAGE     1
 
-/*Preload (dependencies: lv_arc)*/
+/*Preload (dependencies: lv_arc, lv_anim)*/
 #define LV_USE_PRELOAD      1
 #if LV_USE_PRELOAD != 0
 #  define LV_PRELOAD_DEF_ARC_LENGTH   60      /*[deg]*/
@@ -425,8 +424,8 @@ typedef void * lv_obj_user_data_t;
 /*Text area (dependencies: lv_label, lv_page)*/
 #define LV_USE_TA       1
 #if LV_USE_TA != 0
-#  define LV_TA_CURSOR_BLINK_TIME 400     /*ms*/
-#  define LV_TA_PWD_SHOW_TIME     1500    /*ms*/
+#  define LV_TA_DEF_CURSOR_BLINK_TIME 400     /*ms*/
+#  define LV_TA_DEF_PWD_SHOW_TIME     1500    /*ms*/
 #endif
 
 /*Table (dependencies: lv_label)*/
--- src/lv_objx/lv_ddlist.c
+++ src/lv_objx/lv_ddlist.c
@@ -22,7 +22,7 @@
  *      DEFINES
  *********************/
 #if LV_USE_ANIMATION
-#ifndef LV_DDLIST_ANIM_TIME
+#ifndef LV_DDLIST_DEF_ANIM_TIME
 #define LV_DDLIST_DEF_ANIM_TIME 200 /*ms*/
 #endif
 #else
@@ -44,9 +44,11 @@ static lv_res_t release_handler(lv_obj_t * ddlist);
 static void lv_ddlist_refr_size(lv_obj_t * ddlist, bool anim_en);
 static void lv_ddlist_pos_current_option(lv_obj_t * ddlist);
 static void lv_ddlist_refr_width(lv_obj_t* ddlist);
+#if LV_USE_ANIMATION
 static void lv_ddlist_anim_ready_cb(lv_anim_t * a);
 static void lv_ddlist_anim_finish(lv_obj_t* ddlist);
-static void lv_ddlist_adjust_height(lv_obj_t * ddlist, int32_t height);
+static void lv_ddlist_adjust_height(lv_obj_t * ddlist, lv_anim_value_t height);
+#endif
 
 /**********************
  *  STATIC VARIABLES
@@ -791,6 +793,7 @@ static lv_res_t release_handler(lv_obj_t * ddlist)
         /*Leave edit mode once a new item is selected*/
         lv_indev_t * indev = lv_indev_get_act();
         if(lv_indev_get_type(indev) == LV_INDEV_TYPE_ENCODER) {
+            ext->sel_opt_id_ori = ext->sel_opt_id;
             lv_group_t * g = lv_obj_get_group(ddlist);
             if(lv_group_get_editing(g)) {
                 lv_group_set_editing(g, false);
@@ -820,9 +823,8 @@ static lv_res_t release_handler(lv_obj_t * ddlist)
             }
 
             ext->sel_opt_id = new_opt;
+            ext->sel_opt_id_ori = ext->sel_opt_id;
         }
-
-        ext->sel_opt_id_ori = ext->sel_opt_id;
 
         lv_res_t res = lv_event_send(ddlist, LV_EVENT_VALUE_CHANGED, &ext->sel_opt_id);
         if(res != LV_RES_OK) return res;
@@ -883,29 +885,31 @@ static void lv_ddlist_refr_size(lv_obj_t * ddlist, bool anim_en)
 
         /*Force animation complete to fix highlight selection*/
         lv_ddlist_anim_finish(ddlist);
-
-
     } else {
-        lv_anim_t a;
-        a.var            = ddlist;
-        a.start          = lv_obj_get_height(ddlist);
-        a.end            = new_height;
-        a.exec_cb         = (lv_anim_exec_cb_t)lv_ddlist_adjust_height;
-        a.path_cb        = lv_anim_path_linear;
-        a.ready_cb       = lv_ddlist_anim_ready_cb;
-        a.act_time       = 0;
-        a.time           = ext->anim_time;
-        a.playback       = 0;
-        a.playback_pause = 0;
-        a.repeat         = 0;
-        a.repeat_pause   = 0;
+        /*Run the animation only if the the size will be different*/
+        if(lv_obj_get_height(ddlist) != new_height) {
+            lv_anim_t a;
+            a.var            = ddlist;
+            a.start          = lv_obj_get_height(ddlist);
+            a.end            = new_height;
+            a.exec_cb         = (lv_anim_exec_cb_t)lv_ddlist_adjust_height;
+            a.path_cb        = lv_anim_path_linear;
+            a.ready_cb       = lv_ddlist_anim_ready_cb;
+            a.act_time       = 0;
+            a.time           = ext->anim_time;
+            a.playback       = 0;
+            a.playback_pause = 0;
+            a.repeat         = 0;
+            a.repeat_pause   = 0;
 
-        ext->force_sel = 1; /*Keep the list item selected*/
-        lv_anim_create(&a);
+            ext->force_sel = 1; /*Keep the list item selected*/
+            lv_anim_create(&a);
+        }
 #endif
     }
 }
 
+#if LV_USE_ANIMATION
 /**
  * Position the list and remove the selection highlight if it's closed.
  * Called at end of list animation.
@@ -936,11 +940,12 @@ static void lv_ddlist_anim_finish(lv_obj_t* ddlist)
  * @param ddlist Drop down list object
  * @param height New drop down list height
  */
-static void lv_ddlist_adjust_height(lv_obj_t * ddlist, int32_t height)
+static void lv_ddlist_adjust_height(lv_obj_t * ddlist, lv_anim_value_t height)
 {
     lv_obj_set_height(ddlist, height);
     lv_ddlist_pos_current_option(ddlist);
 }
+#endif
 
 /**
  * Set the position of list when it is closed to show the selected item
