3ce7084421129a01d73dbe45495228c85ee6499d
liujinye <liujinye@xiaomi.com>
2019-06-25 15:14:47 +0800

    minor API updates

--- src/lv_draw/lv_img_cache.h
+++ src/lv_draw/lv_img_cache.h
@@ -24,11 +24,7 @@ extern "C" {
  **********************/
 typedef struct
 {
-    lv_img_decoder_dsc_t dsc;
-    const uint8_t * img_data;
-
-    /* How much time did it take to open the image.*/
-    uint32_t time_to_open;
+    lv_img_decoder_dsc_t dec_dsc;
 
     /* Count the cache entries's life. Add `time_tio_open` to `life` when the entry is used.
      * Decrement all lifes by one every in every `lv_img_cache_open`.
