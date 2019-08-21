8dccae60a45e1293a9e0b03a1d7b02978296eba9
openvela-robot <openvela-robot@xiaomi.com>
2019-08-21 15:44:35 +0800

    add real draw functions

--- src/lv_misc/lv_math.c
+++ src/lv_misc/lv_math.c
@@ -9,6 +9,7 @@
 #include "lv_math.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*********************
  *      DEFINES
@@ -92,6 +93,36 @@ int32_t lv_bezier3(uint32_t t, int32_t u0, int32_t u1, int32_t u2, int32_t u3)
     uint32_t v4 = ((uint32_t)t3 * u3) >> 10;
 
     return v1 + v2 + v3 + v4;
+}
+#define BITSPERLONG 32
+
+#define TOP2BITS(x) ((x & (3L << (BITSPERLONG-2))) >> (BITSPERLONG-2))
+
+void lv_sqrt(uint32_t x, lv_sqrt_res_t * q)
+{
+    /*
+     * Source:
+     * http://web.archive.org/web/20080303101624/http://c.snippets.org/snip_lister.php?fname=isqrt.c
+     * https://stackoverflow.com/questions/1100090/looking-for-an-efficient-integer-square-root-algorithm-for-arm-thumb2
+     */
+
+    uint32_t a = 0L;                   /* accumulator      */
+    uint32_t r = 0L;                   /* remainder        */
+    uint32_t e = 0L;                   /* trial product    */
+
+    uint32_t i;
+    for (i = 0; i < BITSPERLONG / 2 + 8; i++) {
+          r = (r << 2) + TOP2BITS(x); x <<= 2;
+          a <<= 1;
+          e = (a << 1) + 1;
+          if (r >= e)  {
+                r -= e;
+                a++;
+          }
+    }
+
+    q->f = a & 0xFF;
+    q->i = a >> 8;
 }
 
 /**********************
