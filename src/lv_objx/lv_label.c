f4b9cf397bcef26d06e025c01c14817d63ac4d5d
liujinye <liujinye@xiaomi.com>
2019-08-27 06:11:33 +0800

    img draw add alpha and chroma key support

--- src/lv_objx/lv_label.c
+++ src/lv_objx/lv_label.c
@@ -585,13 +585,14 @@ uint16_t lv_label_get_letter_on(const lv_obj_t * label, lv_point_t * pos)
     lv_txt_cmd_state_t cmd_state = LV_TXT_CMD_STATE_WAIT;
 
     uint32_t i         = line_start;
-    uint32_t i_act = i;
+    uint32_t i_current = i;
     uint32_t letter;
     uint32_t letter_next;
 
     if(new_line_start > 0) {
-        while(i < new_line_start) {
-            /* Get the current letter.*/
+        while(i <= new_line_start - 1) {
+            /* Get the current letter.
+             * Be careful 'i' already points to the next character*/
             letter = lv_txt_encoded_next(txt, &i);
 
             /*Get the next letter too for kerning*/
@@ -605,14 +606,12 @@ uint16_t lv_label_get_letter_on(const lv_obj_t * label, lv_point_t * pos)
             }
 
             x += lv_font_get_glyph_width(font, letter, letter_next);
-
-            /*Finish if the x position or the last char of the line is reached*/
-            if(pos->x < x || i == new_line_start) {
-                i = i_act;
+            if(pos->x < x) {
+                i = i_current;
                 break;
             }
             x += style->text.letter_space;
-            i_act = i;
+            i_current = i;
         }
     }
 
