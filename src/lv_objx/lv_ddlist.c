32561663eac60306becbe4465a66d33810f7f9cb
liujinye <liujinye@xiaomi.com>
2019-09-18 11:48:31 +0800

    Initial commit of porting @AloyseTech's color picker from lvgl v5 to v6

--- src/lv_objx/lv_ddlist.c
+++ src/lv_objx/lv_ddlist.c
@@ -33,7 +33,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * clip_area, lv_design_mode_t mode);
+static bool lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * mask, lv_design_mode_t mode);
 static lv_res_t lv_ddlist_signal(lv_obj_t * ddlist, lv_signal_t sign, void * param);
 static lv_res_t lv_ddlist_scrl_signal(lv_obj_t * scrl, lv_signal_t sign, void * param);
 static lv_res_t release_handler(lv_obj_t * ddlist);
@@ -494,22 +494,22 @@ static lv_txt_flag_t lv_ddlist_get_txt_flag(const lv_obj_t * ddlist)
 /**
  * Handle the drawing related tasks of the drop down lists
  * @param ddlist pointer to an object
- * @param clip_area the object will be drawn only in this area
+ * @param mask the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return an element of `lv_design_res_t`
+ * @param return true/false, depends on 'mode'
  */
-static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * clip_area, lv_design_mode_t mode)
+static bool lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * mask, lv_design_mode_t mode)
 {
     /*Return false if the object is not covers the mask_p area*/
     if(mode == LV_DESIGN_COVER_CHK) {
-        return ancestor_design(ddlist, clip_area, mode);
+        return ancestor_design(ddlist, mask, mode);
     }
     /*Draw the object*/
     else if(mode == LV_DESIGN_DRAW_MAIN) {
-        ancestor_design(ddlist, clip_area, mode);
+        ancestor_design(ddlist, mask, mode);
 
         lv_ddlist_ext_t * ext = lv_obj_get_ext_attr(ddlist);
         lv_opa_t opa_scale    = lv_obj_get_opa_scale(ddlist);
@@ -529,7 +529,7 @@ static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * cli
             rect_area.x1 = ddlist->coords.x1;
             rect_area.x2 = ddlist->coords.x2;
 
-            lv_draw_rect(&rect_area, clip_area, ext->sel_style, opa_scale);
+            lv_draw_rect(&rect_area, mask, ext->sel_style, opa_scale);
         }
     }
     /*Post draw when the children are drawn*/
@@ -554,7 +554,7 @@ static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * cli
             area_sel.x2 = ddlist->coords.x2;
             lv_area_t mask_sel;
             bool area_ok;
-            area_ok = lv_area_intersect(&mask_sel, clip_area, &area_sel);
+            area_ok = lv_area_intersect(&mask_sel, mask, &area_sel);
             if(area_ok) {
                 const lv_style_t * sel_style = lv_ddlist_get_style(ddlist, LV_DDLIST_STYLE_SEL);
                 lv_style_t new_style;
@@ -580,21 +580,16 @@ static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * cli
                 new_style.text.color = sel_style->text.color;
                 new_style.text.opa   = sel_style->text.opa;
                 lv_area_t area_arrow;
-                lv_coord_t arrow_width = lv_txt_get_width(LV_SYMBOL_DOWN, strlen(LV_SYMBOL_DOWN), sel_style->text.font, 0, 0);
-                if(lv_label_get_align(ext->label) != LV_LABEL_ALIGN_RIGHT) {
-                    area_arrow.x2 = ddlist->coords.x2 - style->body.padding.right;
-                    area_arrow.x1 = area_arrow.x2 - arrow_width;
-                } else {
-                    area_arrow.x1 = ddlist->coords.x1 + style->body.padding.left;
-                    area_arrow.x2 = area_arrow.x1 + arrow_width;
-                }
+                area_arrow.x2 = ddlist->coords.x2 - style->body.padding.right;
+                area_arrow.x1 = area_arrow.x2 -
+                                lv_txt_get_width(LV_SYMBOL_DOWN, strlen(LV_SYMBOL_DOWN), sel_style->text.font, 0, 0);
 
                 area_arrow.y1 = ddlist->coords.y1 + style->text.line_space;
                 area_arrow.y2 = area_arrow.y1 + font_h;
 
                 lv_area_t mask_arrow;
                 bool area_ok;
-                area_ok = lv_area_intersect(&mask_arrow, clip_area, &area_arrow);
+                area_ok = lv_area_intersect(&mask_arrow, mask, &area_arrow);
                 if(area_ok) {
                     lv_draw_label(&area_arrow, &mask_arrow, &new_style, opa_scale, LV_SYMBOL_DOWN, LV_TXT_FLAG_NONE,
                                   NULL, -1, -1, NULL); /*Use a down arrow in ddlist, you can replace it with your
@@ -603,10 +598,10 @@ static lv_design_res_t lv_ddlist_design(lv_obj_t * ddlist, const lv_area_t * cli
             }
         }
         /*Draw the scrollbar in the ancestor page design function*/
-        ancestor_design(ddlist, clip_area, mode);
+        ancestor_design(ddlist, mask, mode);
     }
 
-    return LV_DESIGN_RES_OK;
+    return true;
 }
 
 /**
--- src/lv_objx/lv_list.c
+++ src/lv_objx/lv_list.c
@@ -363,36 +363,6 @@ void lv_list_set_style(lv_obj_t * list, lv_list_style_t type, const lv_style_t *
     }
 }
 
-/**
- * Set layout of a list
- * @param list pointer to a list object
- * @param layout which layout should be used
- */
- void lv_list_set_layout(lv_obj_t * list, lv_layout_t layout)
- {
-	/* Update list layout if necessary */
-	if (layout == lv_list_get_layout(list)) return;
-
-    /* Get the first button on the list */
-    lv_obj_t * btn = lv_list_get_prev_btn(list, NULL);
-
-    /* Visit all buttons on the list and update their layout  */
-    while(btn != NULL) {
-        /*If a column layout set the buttons' width to list width*/
-        if(layout == LV_LAYOUT_COL_M || layout == LV_LAYOUT_COL_L || layout == LV_LAYOUT_COL_R) {
-            lv_btn_set_fit2(list, LV_FIT_FLOOD, LV_FIT_TIGHT);
-        }
-        /*If a row layout set the buttons' width according to the content*/
-        else if (layout == LV_LAYOUT_ROW_M || layout == LV_LAYOUT_ROW_T || layout == LV_LAYOUT_ROW_B) {
-            lv_btn_set_fit(list, LV_FIT_TIGHT);
-        }
-
-        btn = lv_list_get_prev_btn(list, btn);
-    }
-
-    lv_page_set_scrl_layout(list, layout);
- }
-
 /*=====================
  * Getter functions
  *====================*/
@@ -556,24 +526,15 @@ lv_obj_t * lv_list_get_btn_selected(const lv_obj_t * list)
     lv_list_ext_t * ext = lv_obj_get_ext_attr(list);
     return ext->selected_btn;
 }
-#endif
 
-/**
- * Get layout of a list
- * @param list pointer to a list object
- * @return layout of the list object
- */
-lv_layout_t lv_list_get_layout(lv_obj_t * list)
-{
-    return lv_page_get_scrl_layout(list);
-}
+#endif
 
 /**
  * Get a style of a list
  * @param list pointer to a list object
  * @param type which style should be get
  * @return style pointer to a style
- */
+ *  */
 const lv_style_t * lv_list_get_style(const lv_obj_t * list, lv_list_style_t type)
 {
     const lv_style_t * style = NULL;
@@ -594,7 +555,6 @@ const lv_style_t * lv_list_get_style(const lv_obj_t * list, lv_list_style_t type
 
     return style;
 }
-
 /*=====================
  * Other functions
  *====================*/
@@ -877,6 +837,7 @@ static lv_res_t lv_list_btn_signal(lv_obj_t * btn, lv_signal_t sign, void * para
     if(sign == LV_SIGNAL_RELEASED) {
         lv_obj_t * list          = lv_obj_get_parent(lv_obj_get_parent(btn));
         lv_list_ext_t * ext      = lv_obj_get_ext_attr(list);
+        ext->page.scroll_prop_ip = 0;
 
 #if LV_USE_GROUP
         lv_group_t * g = lv_obj_get_group(list);
@@ -904,6 +865,10 @@ static lv_res_t lv_list_btn_signal(lv_obj_t * btn, lv_signal_t sign, void * para
         if(lv_indev_is_dragging(lv_indev_get_act()) == false && ext->single_mode) {
             lv_list_btn_single_select(btn);
         }
+    } else if(sign == LV_SIGNAL_PRESS_LOST) {
+        lv_obj_t * list          = lv_obj_get_parent(lv_obj_get_parent(btn));
+        lv_list_ext_t * ext      = lv_obj_get_ext_attr(list);
+        ext->page.scroll_prop_ip = 0;
     } else if(sign == LV_SIGNAL_CLEANUP) {
 
 #if LV_USE_GROUP
