1b4c74bb754998ab2cde02daea595801648501e6
liujinye <liujinye@xiaomi.com>
2019-09-20 10:57:20 +0800

    lv_tabview: calculate with paddings

--- src/lv_core/lv_obj.c
+++ src/lv_core/lv_obj.c
@@ -18,9 +18,10 @@
 #include "../lv_misc/lv_async.h"
 #include "../lv_misc/lv_fs.h"
 #include "../lv_hal/lv_hal.h"
+#include "../lv_misc/lv_gc.h"
+#include "../lv_misc/lv_math.h"
 #include <stdint.h>
 #include <string.h>
-#include "../lv_misc/lv_gc.h"
 
 #if defined(LV_GC_INCLUDE)
 #include LV_GC_INCLUDE
@@ -51,7 +52,7 @@ static void refresh_children_style(lv_obj_t * obj);
 static void delete_children(lv_obj_t * obj);
 static void lv_event_mark_deleted(lv_obj_t * obj);
 static void lv_obj_del_async_cb(void * obj);
-static bool lv_obj_design(lv_obj_t * obj, const lv_area_t * mask_p, lv_design_mode_t mode);
+static lv_design_res_t lv_obj_design(lv_obj_t * obj, const lv_area_t * clip_area, lv_design_mode_t mode);
 static lv_res_t lv_obj_signal(lv_obj_t * obj, lv_signal_t sign, void * param);
 
 /**********************
@@ -279,7 +280,7 @@ lv_obj_t * lv_obj_create(lv_obj_t * parent, const lv_obj_t * copy)
         /*Set attributes*/
         new_obj->click        = 1;
         new_obj->drag         = 0;
-        new_obj->drag_dir     = LV_DRAG_DIR_ALL;
+        new_obj->drag_dir     = LV_DRAG_DIR_BOTH;
         new_obj->drag_throw   = 0;
         new_obj->drag_parent  = 0;
         new_obj->hidden       = 0;
@@ -1625,13 +1626,13 @@ void lv_obj_get_coords(const lv_obj_t * obj, lv_area_t * cords_p)
 void lv_obj_get_inner_coords(const lv_obj_t * obj, lv_area_t * coords_p)
 {
     const lv_style_t * style = lv_obj_get_style(obj);
-    if(style->body.border.part & LV_BORDER_LEFT) coords_p->x1 += style->body.border.width;
+    if(style->body.border.part & LV_BORDER_PART_LEFT) coords_p->x1 += style->body.border.width;
 
-    if(style->body.border.part & LV_BORDER_RIGHT) coords_p->x2 -= style->body.border.width;
+    if(style->body.border.part & LV_BORDER_PART_RIGHT) coords_p->x2 -= style->body.border.width;
 
-    if(style->body.border.part & LV_BORDER_TOP) coords_p->y1 += style->body.border.width;
+    if(style->body.border.part & LV_BORDER_PART_TOP) coords_p->y1 += style->body.border.width;
 
-    if(style->body.border.part & LV_BORDER_BOTTOM) coords_p->y2 -= style->body.border.width;
+    if(style->body.border.part & LV_BORDER_PART_BOTTOM) coords_p->y2 -= style->body.border.width;
 }
 
 /**
@@ -2122,28 +2123,28 @@ static void lv_obj_del_async_cb(void * obj)
 /**
  * Handle the drawing related tasks of the base objects.
  * @param obj pointer to an object
- * @param mask the object will be drawn only in this area
+ * @param clip_area the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
- * @param return true/false, depends on 'mode'
+ * @param return an element of `lv_design_res_t`
  */
-static bool lv_obj_design(lv_obj_t * obj, const lv_area_t * mask_p, lv_design_mode_t mode)
+static lv_design_res_t lv_obj_design(lv_obj_t * obj, const lv_area_t * clip_area, lv_design_mode_t mode)
 {
     if(mode == LV_DESIGN_COVER_CHK) {
 
         /*Most trivial test. Is the mask fully IN the object? If no it surely doesn't cover it*/
-        if(lv_area_is_in(mask_p, &obj->coords) == false) return false;
+        if(lv_area_is_in(clip_area, &obj->coords) == false) return LV_DESIGN_RES_NOT_COVER;
 
         /*Can cover the area only if fully solid (no opacity)*/
         const lv_style_t * style = lv_obj_get_style(obj);
-        if(style->body.opa < LV_OPA_MAX) return false;
+        if(style->body.opa < LV_OPA_MAX) return LV_DESIGN_RES_NOT_COVER;
 
         /* Because of the radius it is not sure the area is covered
          * Check the areas where there is no radius*/
         lv_coord_t r = style->body.radius;
 
-        if(r == LV_RADIUS_CIRCLE) return false;
+        if(r == LV_RADIUS_CIRCLE) return LV_DESIGN_RES_NOT_COVER;
 
         lv_area_t area_tmp;
 
@@ -2151,20 +2152,22 @@ static bool lv_obj_design(lv_obj_t * obj, const lv_area_t * mask_p, lv_design_mo
         lv_obj_get_coords(obj, &area_tmp);
         area_tmp.x1 += r;
         area_tmp.x2 -= r;
-        if(lv_area_is_in(mask_p, &area_tmp) == false) return false;
+        if(lv_area_is_in(clip_area, &area_tmp) == false) return LV_DESIGN_RES_NOT_COVER;
 
         /*Check vertically without radius*/
         lv_obj_get_coords(obj, &area_tmp);
         area_tmp.y1 += r;
         area_tmp.y2 -= r;
-        if(lv_area_is_in(mask_p, &area_tmp) == false) return false;
+        if(lv_area_is_in(clip_area, &area_tmp) == false) return LV_DESIGN_RES_NOT_COVER;
+
+        return  LV_DESIGN_RES_COVER;
 
     } else if(mode == LV_DESIGN_DRAW_MAIN) {
         const lv_style_t * style = lv_obj_get_style(obj);
-        lv_draw_rect(&obj->coords, mask_p, style, lv_obj_get_opa_scale(obj));
+        lv_draw_rect(&obj->coords, clip_area, style, lv_obj_get_opa_scale(obj));
     }
 
-    return true;
+    return LV_DESIGN_RES_OK;
 }
 
 /**
@@ -2186,7 +2189,11 @@ static lv_res_t lv_obj_signal(lv_obj_t * obj, lv_signal_t sign, void * param)
         /*Return 'invalid' if the child change signal is not enabled*/
         if(lv_obj_is_protected(obj, LV_PROTECT_CHILD_CHG) != false) res = LV_RES_INV;
     } else if(sign == LV_SIGNAL_REFR_EXT_DRAW_PAD) {
-        if(style->body.shadow.width > obj->ext_draw_pad) obj->ext_draw_pad = style->body.shadow.width;
+        lv_coord_t shadow = (style->body.shadow.width >> 1) + 1;
+        shadow += style->body.shadow.spread;
+        shadow += LV_MATH_MAX(LV_MATH_ABS(style->body.shadow.offset.x), LV_MATH_ABS(style->body.shadow.offset.y));
+
+        if(shadow > obj->ext_draw_pad) obj->ext_draw_pad = shadow;
     } else if(sign == LV_SIGNAL_STYLE_CHG) {
         lv_obj_refresh_ext_draw_pad(obj);
     } else if(sign == LV_SIGNAL_GET_TYPE) {
