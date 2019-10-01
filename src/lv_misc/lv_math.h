b9310a064323e7ee10ee1d8f1b7c7771d885cf0b
liujinye <liujinye@xiaomi.com>
2019-10-01 22:16:25 +0800

    merge dev-6.1

--- src/lv_misc/lv_math.h
+++ src/lv_misc/lv_math.h
@@ -32,6 +32,12 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
+typedef struct {
+     uint16_t i;
+     uint16_t f;
+ }lv_sqrt_res_t;
+
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
@@ -54,21 +60,7 @@ int16_t lv_trigo_sin(int16_t angle);
  */
 int32_t lv_bezier3(uint32_t t, int32_t u0, int32_t u1, int32_t u2, int32_t u3);
 
-/**
- * Calculate the atan2 of a vector.
- * @param x
- * @param y
- * @return the angle in degree calculated from the given parameters in range of [0..360]
- */
-uint16_t lv_atan2(int x, int y);
-
-/**
- * Calculate the sqrt of an integer.
- * @param x
- * @return the sqrt of x
- */
-uint16_t lv_sqrt(uint32_t x);
-
+void lv_sqrt(uint32_t x, lv_sqrt_res_t * q);
 /**********************
  *      MACROS
  **********************/
