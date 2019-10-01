4ae3577e245d033c27528fba076b493931d122bd
openvela-robot <openvela-robot@xiaomi.com>
2019-10-01 20:22:57 +0800

    Merge remote-tracking branch 'littlevgl/feat-cpicker' into feat-cpicker

--- src/lv_core/lv_refr.c
+++ src/lv_core/lv_refr.c
@@ -217,7 +217,7 @@ void lv_disp_refr_task(lv_task_t * task)
         }
     }
 
-    lv_draw_buf_free_all();
+    lv_draw_free_buf();
 
     LV_LOG_TRACE("lv_refr_task: ready");
 }
@@ -419,10 +419,6 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
 
     /*If this object is fully cover the draw area check the children too */
     if(lv_area_is_in(area_p, &obj->coords) && obj->hidden == 0) {
-
-        lv_design_res_t design_res = obj->design_cb(obj, area_p, LV_DESIGN_COVER_CHK);
-        if(design_res == LV_DESIGN_RES_MASKED) return NULL;
-
         lv_obj_t * i;
         LV_LL_READ(obj->child_ll, i)
         {
@@ -437,11 +433,8 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
         /*If no better children check this object*/
         if(found_p == NULL) {
             const lv_style_t * style = lv_obj_get_style(obj);
-            if(style->body.opa == LV_OPA_COVER && design_res == LV_DESIGN_RES_COVER &&
-               lv_obj_get_opa_scale(obj) == LV_OPA_COVER &&
-               style->body.blend_mode == LV_BLEND_MODE_NORMAL &&
-               style->body.border.blend_mode == LV_BLEND_MODE_NORMAL &&
-               style->image.blend_mode == LV_BLEND_MODE_NORMAL) {
+            if(style->body.opa == LV_OPA_COVER && obj->design_cb(obj, area_p, LV_DESIGN_COVER_CHK) != false &&
+               lv_obj_get_opa_scale(obj) == LV_OPA_COVER) {
                 found_p = obj;
             }
         }
--- src/lv_objx/lv_lmeter.c
+++ src/lv_objx/lv_lmeter.c
@@ -30,7 +30,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static lv_design_res_t lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * clip_area, lv_design_mode_t mode);
+static bool lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * mask, lv_design_mode_t mode);
 static lv_res_t lv_lmeter_signal(lv_obj_t * lmeter, lv_signal_t sign, void * param);
 static lv_coord_t lv_lmeter_coord_round(int32_t x);
 
@@ -253,18 +253,18 @@ uint16_t lv_lmeter_get_scale_angle(const lv_obj_t * lmeter)
 /**
  * Handle the drawing related tasks of the line meters
  * @param lmeter pointer to an object
- * @param clip_area the object will be drawn only in this area
+ * @param mask the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return an element of `lv_design_res_t`
+ * @param return true/false, depends on 'mode'
  */
-static lv_design_res_t lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * clip_area, lv_design_mode_t mode)
+static bool lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * mask, lv_design_mode_t mode)
 {
     /*Return false if the object is not covers the mask_p area*/
     if(mode == LV_DESIGN_COVER_CHK) {
-        return LV_DESIGN_RES_NOT_COVER;
+        return false;
     }
     /*Draw the object*/
     else if(mode == LV_DESIGN_DRAW_MAIN) {
@@ -329,7 +329,7 @@ static lv_design_res_t lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * cli
                     lv_color_mix(style->body.grad_color, style->body.main_color, (255 * i) / ext->line_cnt);
             }
 
-            lv_draw_line(&p1, &p2, clip_area, &style_tmp, opa_scale);
+            lv_draw_line(&p1, &p2, mask, &style_tmp, opa_scale);
         }
 
     }
@@ -337,7 +337,7 @@ static lv_design_res_t lv_lmeter_design(lv_obj_t * lmeter, const lv_area_t * cli
     else if(mode == LV_DESIGN_DRAW_POST) {
     }
 
-    return LV_DESIGN_RES_OK;
+    return true;
 }
 
 /**
