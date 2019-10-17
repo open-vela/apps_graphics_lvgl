27e956b7bdbbdf116c60610228dfbae7d84bf0f5
liujinye <liujinye@xiaomi.com>
2019-10-18 02:13:41 +0800

    Support text wrap for bidi text
    TODO: conditional LV_USE_BIDI, allocate global buffer once, pass dir as parameter to lv_draw_label

--- src/lv_misc/lv_color.h
+++ src/lv_misc/lv_color.h
@@ -305,14 +305,9 @@ static inline uint32_t lv_color_to32(lv_color_t color)
 #elif LV_COLOR_DEPTH == 16
 #if LV_COLOR_16_SWAP == 0
     lv_color32_t ret;
-    /**
-     * Per https://docs.google.com/spreadsheets/d/1PppX8FJpddauAPasHwlNgIPGIuPGPNvRbhilIQ8w-7g/edit#gid=0
-     * Truly any of the listed multipliers and adders would work.
-     * The below numbers seem the most precise.
-     */    
-    ret.ch.red   = ( color.ch.red * 8423 + 7 ) >> 10; 
-    ret.ch.green = ( color.ch.green * 259 + 3 ) >> 6;
-    ret.ch.blue  = ( color.ch.blue * 8423 + 7 ) >> 10;
+    ret.ch.red   = color.ch.red * 8;   /*(2^8 - 1)/(2^5 - 1) = 255/31 = 8*/
+    ret.ch.green = color.ch.green * 4; /*(2^8 - 1)/(2^6 - 1) = 255/63 = 4*/
+    ret.ch.blue  = color.ch.blue * 8;  /*(2^8 - 1)/(2^5 - 1) = 255/31 = 8*/
     ret.ch.alpha = 0xFF;
     return ret.full;
 #else
