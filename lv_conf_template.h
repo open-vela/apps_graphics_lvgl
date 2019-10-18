7805c5a6da2222ac29d72cb30228226c6450afcd
liujinye <liujinye@xiaomi.com>
2019-10-19 04:04:07 +0800

    Experimented and found "smaller" math

--- lv_conf_template.h
+++ lv_conf_template.h
@@ -199,10 +199,8 @@ typedef void * lv_img_decoder_user_data_t;
 /* Export integer constant to binding.
  * This macro is used with constants in the form of LV_<CONST> that
  * should also appear on lvgl binding API such as Micropython
- *
- * The default value just prevents a GCC warning.
  */
-#define LV_EXPORT_CONST_INT(int_value) struct _silence_gcc_warning
+#define LV_EXPORT_CONST_INT(int_value)
 
 /*===================
  *  HAL settings
@@ -360,12 +358,6 @@ typedef void * lv_font_user_data_t;
 #  define lv_snprintf     snprintf
 #  define lv_vsnprintf    vsnprintf
 #endif  /*LV_SPRINTF_CUSTOM*/
-
- /* Set the pixel order of the display.
-  * Important only if "subpx fonts" are used.
-  * With "normal" font it doesn't matter.
-  */
- #define LV_SUBPX_BGR    0
 
 /*===================
  *  LV_OBJ SETTINGS
