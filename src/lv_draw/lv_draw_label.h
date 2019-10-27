f3fea6a6db6a4d0bcf2d0ec3806ea50ba9505e46
liujinye <liujinye@xiaomi.com>
2019-10-27 09:10:41 +0800

    Merge remote-tracking branch 'paulpv/feat-cpicker' into feat-cpicker

--- src/lv_draw/lv_draw_label.h
+++ src/lv_draw/lv_draw_label.h
@@ -14,7 +14,6 @@ extern "C" {
  *      INCLUDES
  *********************/
 #include "lv_draw.h"
-#include "../lv_misc/lv_bidi.h"
 
 /*********************
  *      DEFINES
@@ -59,7 +58,7 @@ typedef struct {
  */
 void lv_draw_label(const lv_area_t * coords, const lv_area_t * mask, const lv_style_t * style, lv_opa_t opa_scale,
                    const char * txt, lv_txt_flag_t flag, lv_point_t * offset, uint16_t sel_start, uint16_t sel_end,
-                   lv_draw_label_hint_t * hint, lv_bidi_dir_t bidi_dir);
+                   lv_draw_label_hint_t * hint);
 
 /**********************
  *      MACROS
