ee05d84c171959aef194b2976eda36a57214f9cd
liujinye <liujinye@xiaomi.com>
2019-11-05 12:30:33 +0800

    minor fixes

--- src/lv_core/lv_obj.c
+++ src/lv_core/lv_obj.c
@@ -18,13 +18,13 @@
 #include "../lv_misc/lv_task.h"
 #include "../lv_misc/lv_async.h"
 #include "../lv_misc/lv_fs.h"
-#include "../lv_hal/lv_hal.h"
 #include "../lv_misc/lv_gc.h"
 #include "../lv_misc/lv_math.h"
+#include "../lv_misc/lv_gc.h"
+#include "../lv_misc/lv_math.h"
+#include "../lv_hal/lv_hal.h"
 #include <stdint.h>
 #include <string.h>
-#include "../lv_misc/lv_gc.h"
-#include "../lv_misc/lv_math.h"
 
 #if defined(LV_GC_INCLUDE)
 #include LV_GC_INCLUDE
@@ -204,6 +204,7 @@ lv_obj_t * lv_obj_create(lv_obj_t * parent, const lv_obj_t * copy)
         new_obj->drag         = 0;
         new_obj->drag_throw   = 0;
         new_obj->drag_parent  = 0;
+        new_obj->drag_dir     = 0;
         new_obj->hidden       = 0;
         new_obj->top          = 0;
         new_obj->protect      = LV_PROTECT_NONE;
@@ -211,11 +212,7 @@ lv_obj_t * lv_obj_create(lv_obj_t * parent, const lv_obj_t * copy)
         new_obj->opa_scale    = LV_OPA_COVER;
         new_obj->parent_event = 0;
 #if LV_USE_BIDI
-#if LV_BIDI_BASE_DIR_DEF == LV_BIDI_DIR_LTR || LV_BIDI_BASE_DIR_DEF == LV_BIDI_DIR_RTL || LV_BIDI_BASE_DIR_DEF == LV_BIDI_DIR_AUTO
         new_obj->base_dir     = LV_BIDI_BASE_DIR_DEF;
-#else
-#error "`LV_BIDI_BASE_DIR_DEF` should be `LV_BASE_DIR_LTR` or `LV_BASE_DIR_RTL` (See lv_conf.h)"
-#endif
 #else
         new_obj->base_dir     = LV_BIDI_DIR_LTR;
 #endif
@@ -1477,8 +1474,6 @@ lv_res_t lv_event_send(lv_obj_t * obj, lv_event_t event, const void * data)
  */
 lv_res_t lv_event_send_func(lv_event_cb_t event_xcb, lv_obj_t * obj, lv_event_t event, const void * data)
 {
-    LV_ASSERT_OBJ(obj, LV_OBJX_NAME);
-
     /* Build a simple linked list from the objects used in the events
      * It's important to know if an this object was deleted by a nested event
      * called from this `even_cb`. */
@@ -2410,10 +2405,8 @@ static lv_design_res_t lv_obj_design(lv_obj_t * obj, const lv_area_t * clip_area
         /*Most trivial test. Is the mask fully IN the object? If no it surely doesn't cover it*/
         if(lv_area_is_in(clip_area, &obj->coords) == false) return LV_DESIGN_RES_NOT_COVER;
 
-
         const lv_style_t * style = lv_obj_get_style(obj);
         if(style->body.corner_mask) return LV_DESIGN_RES_MASKED;
-
 
         /*Can cover the area only if fully solid (no opacity)*/
         if(style->body.opa < LV_OPA_MAX) return LV_DESIGN_RES_NOT_COVER;
--- src/lv_core/lv_refr.c
+++ src/lv_core/lv_refr.c
@@ -419,7 +419,6 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
 
     /*If this object is fully cover the draw area check the children too */
     if(lv_area_is_in(area_p, &obj->coords) && obj->hidden == 0) {
-
         lv_design_res_t design_res = obj->design_cb(obj, area_p, LV_DESIGN_COVER_CHK);
         if(design_res == LV_DESIGN_RES_MASKED) return NULL;
 
@@ -525,7 +524,12 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
 
 #if MASK_AREA_DEBUG
         static lv_color_t debug_color = LV_COLOR_RED;
-        lv_draw_fill(&obj_ext_mask, &obj_ext_mask, debug_color, LV_OPA_50);
+        LV_STYLE_CREATE(style_debug, &lv_style_plain);
+        style_debug.body.main_color = debug_color;
+        style_debug.body.grad_color = debug_color;
+        style_debug.body.border.width = 2;
+        style_debug.body.border.color.full = (debug_color.full + 0x13) * 9;
+        lv_draw_rect(&obj_ext_mask, &obj_ext_mask, &style_debug, LV_OPA_50);
         debug_color.full *= 17;
         debug_color.full += 0xA1;
 #endif
