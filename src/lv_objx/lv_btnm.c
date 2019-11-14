50bc96d4da518c11c28912b314a561c90103ddef
liujinye <liujinye@xiaomi.com>
2019-11-14 20:38:47 +0800

    Fix public -Wconversion warnings

--- src/lv_objx/lv_btnm.c
+++ src/lv_objx/lv_btnm.c
@@ -29,7 +29,7 @@
  *  STATIC PROTOTYPES
  **********************/
 static lv_res_t lv_btnm_signal(lv_obj_t * btnm, lv_signal_t sign, void * param);
-static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_area, lv_design_mode_t mode);
+static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mode_t mode);
 static uint8_t get_button_width(lv_btnm_ctrl_t ctrl_bits);
 static bool button_is_hidden(lv_btnm_ctrl_t ctrl_bits);
 static bool button_is_repeat_disabled(lv_btnm_ctrl_t ctrl_bits);
@@ -636,21 +636,22 @@ bool lv_btnm_get_one_toggle(const lv_obj_t * btnm)
 /**
  * Handle the drawing related tasks of the button matrixs
  * @param btnm pointer to a button matrix object
- * @param clip_area the object will be drawn only in this area
+ * @param mask the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return an element of `lv_design_res_t`
+ * @param return true/false, depends on 'mode'
  */
-static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_area, lv_design_mode_t mode)
+static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mode_t mode)
 {
     if(mode == LV_DESIGN_COVER_CHK) {
-        return ancestor_design_f(btnm, clip_area, mode);
+        return ancestor_design_f(btnm, mask, mode);
+        /*Return false if the object is not covers the mask_p area*/
     }
     /*Draw the object*/
     else if(mode == LV_DESIGN_DRAW_MAIN) {
-        ancestor_design_f(btnm, clip_area, mode);
+        ancestor_design_f(btnm, mask, mode);
 
         lv_btnm_ext_t * ext         = lv_obj_get_ext_attr(btnm);
         const lv_style_t * bg_style = lv_obj_get_style(btnm);
@@ -670,11 +671,6 @@ static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_ar
         lv_txt_flag_t txt_flag = LV_TXT_FLAG_NONE;
 
         if(ext->recolor) txt_flag = LV_TXT_FLAG_RECOLOR;
-#if LV_USE_BIDI
-        char * bidi_buf = lv_mem_alloc(64);
-        lv_bidi_dir_t base_dir = lv_obj_get_base_dir(btnm);
-#endif
-
         for(btn_i = 0; btn_i < ext->btn_cnt; btn_i++, txt_i++) {
             /*Search the next valid text in the map*/
             while(strcmp(ext->map_p[txt_i], "\n") == 0) {
@@ -711,25 +707,25 @@ static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_ar
             lv_style_copy(&style_tmp, btn_style);
 
             /*Remove borders on the edges if `LV_BORDER_INTERNAL`*/
-            if(style_tmp.body.border.part & LV_BORDER_PART_INTERNAL) {
+            if(style_tmp.body.border.part & LV_BORDER_INTERNAL) {
                 if(area_tmp.y1 == btnm->coords.y1 + bg_style->body.padding.top) {
-                    style_tmp.body.border.part &= ~LV_BORDER_PART_TOP;
+                    style_tmp.body.border.part &= ~LV_BORDER_TOP;
                 }
                 if(area_tmp.y2 == btnm->coords.y2 - bg_style->body.padding.bottom) {
-                    style_tmp.body.border.part &= ~LV_BORDER_PART_BOTTOM;
+                    style_tmp.body.border.part &= ~LV_BORDER_BOTTOM;
                 }
 
                 if(txt_i == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_PART_LEFT;
+                    style_tmp.body.border.part &= ~LV_BORDER_LEFT;
                 } else if(strcmp(ext->map_p[txt_i - 1], "\n") == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_PART_LEFT;
+                    style_tmp.body.border.part &= ~LV_BORDER_LEFT;
                 }
 
                 if(ext->map_p[txt_i + 1][0] == '\0' || strcmp(ext->map_p[txt_i + 1], "\n") == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_PART_RIGHT;
+                    style_tmp.body.border.part &= ~LV_BORDER_RIGHT;
                 }
             }
-            lv_draw_rect(&area_tmp, clip_area, &style_tmp, opa_scale);
+            lv_draw_rect(&area_tmp, mask, &style_tmp, opa_scale);
 
             /*Calculate the size of the text*/
             if(btn_style->glass) btn_style = bg_style;
@@ -743,24 +739,11 @@ static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_ar
             area_tmp.x2 = area_tmp.x1 + txt_size.x;
             area_tmp.y2 = area_tmp.y1 + txt_size.y;
 
-#if LV_USE_BIDI == 0
-            lv_draw_label(&area_tmp, clip_area, btn_style, opa_scale, ext->map_p[txt_i], txt_flag, NULL, NULL, NULL);
-#else
-            uint32_t txt_len = strlen(ext->map_p[txt_i]) + 1;
-            if(txt_len > lv_mem_get_size(bidi_buf)) {
-                bidi_buf = lv_mem_realloc(bidi_buf, txt_len);
-            }
-
-            lv_bidi_process(ext->map_p[txt_i], bidi_buf, base_dir);
-            lv_draw_label(&area_tmp, clip_area, btn_style, opa_scale, bidi_buf, txt_flag, NULL, NULL, NULL);
-#endif
+            lv_draw_label(&area_tmp, mask, btn_style, opa_scale, ext->map_p[txt_i], txt_flag, NULL, NULL, NULL, lv_obj_get_base_dir(btnm));
         }
-
-#if LV_USE_BIDI
-        lv_mem_free(bidi_buf);
-#endif
     }
-    return LV_DESIGN_RES_OK;
+
+    return true;
 }
 
 /**
