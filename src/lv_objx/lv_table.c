7c41f83f1e884e6ceaa215f89c06acb78fdaa306
openvela-robot <openvela-robot@xiaomi.com>
2019-11-19 13:47:38 +0800

    Merge branch 'master' into dev-6.1

--- src/lv_objx/lv_table.c
+++ src/lv_objx/lv_table.c
@@ -27,7 +27,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_area, lv_design_mode_t mode);
+static bool lv_table_design(lv_obj_t * table, const lv_area_t * mask, lv_design_mode_t mode);
 static lv_res_t lv_table_signal(lv_obj_t * table, lv_signal_t sign, void * param);
 static lv_coord_t get_row_height(lv_obj_t * table, uint16_t row_id);
 static void refr_size(lv_obj_t * table);
@@ -151,15 +151,15 @@ void lv_table_set_cell_value(lv_obj_t * table, uint16_t row, uint16_t col, const
     }
     /*Initialize the format byte*/
     else {
+#if LV_USE_BIDI
         lv_bidi_dir_t base_dir = lv_obj_get_base_dir(table);
         if(base_dir == LV_BIDI_DIR_LTR) format.s.align = LV_LABEL_ALIGN_LEFT;
         else if(base_dir == LV_BIDI_DIR_RTL) format.s.align = LV_LABEL_ALIGN_RIGHT;
-        else if(base_dir == LV_BIDI_DIR_AUTO)
-#if LV_USE_BIDI
-        	format.s.align = lv_bidi_detect_base_dir(txt);
+        else if(base_dir == LV_BIDI_DIR_AUTO) format.s.align = lv_bidi_detect_base_dir(txt);
 #else
-        	format.s.align = LV_LABEL_ALIGN_LEFT;
+        format.s.align = LV_LABEL_ALIGN_LEFT;
 #endif
+
         format.s.right_merge = 0;
         format.s.type        = 0;
         format.s.crop        = 0;
@@ -631,22 +631,22 @@ const lv_style_t * lv_table_get_style(const lv_obj_t * table, lv_table_style_t t
 /**
  * Handle the drawing related tasks of the tables
  * @param table pointer to an object
- * @param clip_area the object will be drawn only in this area
+ * @param mask the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return an element of `lv_design_res_t`
+ * @param return true/false, depends on 'mode'
  */
-static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_area, lv_design_mode_t mode)
+static bool lv_table_design(lv_obj_t * table, const lv_area_t * mask, lv_design_mode_t mode)
 {
     /*Return false if the object is not covers the mask_p area*/
     if(mode == LV_DESIGN_COVER_CHK) {
-        return LV_DESIGN_RES_NOT_COVER;
+        return false;
     }
     /*Draw the object*/
     else if(mode == LV_DESIGN_DRAW_MAIN) {
-        ancestor_scrl_design(table, clip_area, mode);
+        ancestor_scrl_design(table, mask, mode);
 
         lv_table_ext_t * ext        = lv_obj_get_ext_attr(table);
         const lv_style_t * bg_style = lv_obj_get_style(table);
@@ -701,7 +701,7 @@ static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_
                     }
                 }
 
-                lv_draw_rect(&cell_area, clip_area, cell_style, opa_scale);
+                lv_draw_rect(&cell_area, mask, cell_style, opa_scale);
 
                 if(ext->cell_data[cell]) {
 
@@ -735,7 +735,7 @@ static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_
 
                     lv_area_t label_mask;
                     bool label_mask_ok;
-                    label_mask_ok = lv_area_intersect(&label_mask, clip_area, &cell_area);
+                    label_mask_ok = lv_area_intersect(&label_mask, mask, &cell_area);
                     if(label_mask_ok) {
                         lv_draw_label(&txt_area, &label_mask, cell_style, opa_scale, ext->cell_data[cell] + 1,
                                       txt_flags, NULL, NULL, NULL, lv_obj_get_base_dir(table));
@@ -755,7 +755,7 @@ static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_
 
                             p1.y = txt_area.y1 + txt_size.y + cell_style->text.line_space / 2;
                             p2.y = txt_area.y1 + txt_size.y + cell_style->text.line_space / 2;
-                            lv_draw_line(&p1, &p2, clip_area, cell_style, opa_scale);
+                            lv_draw_line(&p1, &p2, mask, cell_style, opa_scale);
 
                             ext->cell_data[cell][i] = '\n';
                         }
@@ -771,7 +771,7 @@ static lv_design_res_t lv_table_design(lv_obj_t * table, const lv_area_t * clip_
     else if(mode == LV_DESIGN_DRAW_POST) {
     }
 
-    return LV_DESIGN_RES_OK;
+    return true;
 }
 
 /**
