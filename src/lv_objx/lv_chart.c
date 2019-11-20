b17a26f3826e4661e432bcbcf93298e7dcbe0771
liujinye <liujinye@xiaomi.com>
2019-11-20 15:55:38 +0800

    add style.text.underline and strike_through

--- src/lv_objx/lv_chart.c
+++ src/lv_objx/lv_chart.c
@@ -12,6 +12,7 @@
 #include "../lv_core/lv_debug.h"
 #include "../lv_core/lv_refr.h"
 #include "../lv_draw/lv_draw.h"
+#include "../lv_misc/lv_math.h"
 #include "../lv_themes/lv_theme.h"
 
 /*********************
@@ -46,7 +47,7 @@ typedef struct {
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static bool lv_chart_design(lv_obj_t * chart, const lv_area_t * mask, lv_design_mode_t mode);
+static lv_design_res_t lv_chart_design(lv_obj_t * chart, const lv_area_t * clip_area, lv_design_mode_t mode);
 static lv_res_t lv_chart_signal(lv_obj_t * chart, lv_signal_t sign, void * param);
 static void lv_chart_draw_div(lv_obj_t * chart, const lv_area_t * mask);
 static void lv_chart_draw_lines(lv_obj_t * chart, const lv_area_t * mask);
@@ -190,7 +191,7 @@ lv_chart_series_t * lv_chart_add_series(lv_obj_t * chart, lv_color_t color)
     ser->points = lv_mem_alloc(sizeof(lv_coord_t) * ext->point_cnt);
     LV_ASSERT_MEM(ser->points);
     if(ser->points == NULL) {
-        lv_ll_rem(&ext->series_ll, ser);
+        lv_ll_remove(&ext->series_ll, ser);
         lv_mem_free(ser);
         return NULL;
     }
@@ -463,6 +464,7 @@ void lv_chart_set_next(lv_obj_t * chart, lv_chart_series_t * ser, lv_coord_t y)
         if(ext->type & LV_CHART_TYPE_POINT) lv_chart_inv_points(chart, ser->start_point);
         if(ext->type & LV_CHART_TYPE_VERTICAL_LINE) lv_chart_inv_lines(chart, ser->start_point);
         if(ext->type & LV_CHART_TYPE_AREA) lv_chart_inv_lines(chart, ser->start_point);
+        if(ext->type & LV_CHART_TYPE_AREA_FADED) lv_chart_inv_lines(chart, ser->start_point);
 
         ser->start_point = (ser->start_point + 1) % ext->point_cnt; /*update the x for next incoming y*/
     }
@@ -631,7 +633,7 @@ lv_chart_type_t lv_chart_get_type(const lv_obj_t * chart)
  * @param chart pointer to chart object
  * @return point number on each data line
  */
-uint16_t lv_chart_get_point_cnt(const lv_obj_t * chart)
+uint16_t lv_chart_get_point_count(const lv_obj_t * chart)
 {
     LV_ASSERT_OBJ(chart, LV_OBJX_NAME);
 
@@ -713,44 +715,43 @@ uint16_t lv_chart_get_margin(lv_obj_t * chart)
 /**
  * Handle the drawing related tasks of the chart backgrounds
  * @param chart pointer to an object
- * @param mask the object will be drawn only in this area
+ * @param clip_area the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return true/false, depends on 'mode'
+ * @param return an element of `lv_design_res_t`
  */
-static bool lv_chart_design(lv_obj_t * chart, const lv_area_t * mask, lv_design_mode_t mode)
+static lv_design_res_t lv_chart_design(lv_obj_t * chart, const lv_area_t * clip_area, lv_design_mode_t mode)
 {
     if(mode == LV_DESIGN_COVER_CHK) {
-        /*Return false if the object is not covers the mask_p area*/
-        return ancestor_design_f(chart, mask, mode);
+        return ancestor_design_f(chart, clip_area, mode);
     } else if(mode == LV_DESIGN_DRAW_MAIN) {
         /*Draw the background*/
-        lv_draw_rect(&chart->coords, mask, lv_obj_get_style(chart), lv_obj_get_opa_scale(chart));
+        lv_draw_rect(&chart->coords, clip_area, lv_obj_get_style(chart), lv_obj_get_opa_scale(chart));
 
         lv_chart_ext_t * ext = lv_obj_get_ext_attr(chart);
 
-        lv_chart_draw_div(chart, mask);
+        lv_chart_draw_div(chart, clip_area);
 
         /* Adjust the mask to remove the margin (clips chart contents to be within background) */
 
         lv_area_t mask_tmp, adjusted_mask;
         lv_obj_get_coords(chart, &mask_tmp);
 
-        bool union_ok = lv_area_intersect(&adjusted_mask, mask, &mask_tmp);
+        bool union_ok = lv_area_intersect(&adjusted_mask, clip_area, &mask_tmp);
 
         if(union_ok) {
-                if(ext->type & LV_CHART_TYPE_LINE) lv_chart_draw_lines(chart, &adjusted_mask);
-                if(ext->type & LV_CHART_TYPE_COLUMN) lv_chart_draw_cols(chart, &adjusted_mask);
-                if(ext->type & LV_CHART_TYPE_POINT) lv_chart_draw_points(chart, &adjusted_mask);
-                if(ext->type & LV_CHART_TYPE_VERTICAL_LINE) lv_chart_draw_vertical_lines(chart, &adjusted_mask);
-                if(ext->type & LV_CHART_TYPE_AREA) lv_chart_draw_areas(chart, &adjusted_mask);
+            if(ext->type & LV_CHART_TYPE_LINE) lv_chart_draw_lines(chart, clip_area);
+            if(ext->type & LV_CHART_TYPE_COLUMN) lv_chart_draw_cols(chart, clip_area);
+            if(ext->type & LV_CHART_TYPE_POINT) lv_chart_draw_points(chart, clip_area);
+            if(ext->type & LV_CHART_TYPE_VERTICAL_LINE) lv_chart_draw_vertical_lines(chart, clip_area);
+            if((ext->type & LV_CHART_TYPE_AREA) || (ext->type & LV_CHART_TYPE_AREA_FADED)) lv_chart_draw_areas(chart, clip_area);
         }
 
-        lv_chart_draw_axes(chart, mask);
+        lv_chart_draw_axes(chart, clip_area);
     }
-    return true;
+    return LV_DESIGN_RES_OK;
 }
 
 /**
@@ -919,8 +920,8 @@ static void lv_chart_draw_points(lv_obj_t * chart, const lv_area_t * mask)
     lv_area_t cir_a;
     lv_coord_t w     = lv_obj_get_width(chart);
     lv_coord_t h     = lv_obj_get_height(chart);
-    lv_coord_t x_ofs = chart->coords.x1;
-    lv_coord_t y_ofs = chart->coords.y1;
+    lv_coord_t x_ofs = chart->coords.x1 - (ext->series.width & 0x1);
+    lv_coord_t y_ofs = chart->coords.y1 - (ext->series.width & 0x1 ? 0 : 1);
     int32_t y_tmp;
     lv_coord_t p_act;
     lv_chart_series_t * ser;
@@ -1113,11 +1114,19 @@ static void lv_chart_draw_areas(lv_obj_t * chart, const lv_area_t * mask)
     lv_style_t style;
     lv_style_copy(&style, &lv_style_plain);
 
+   int16_t mask_fade_id = LV_MASK_ID_INV;
+   lv_draw_mask_fade_param_t mask_fade_p;
+   if(ext->type & LV_CHART_TYPE_AREA_FADED) {
+       lv_draw_mask_fade_init(&mask_fade_p, &chart->coords, LV_OPA_COVER, chart->coords.y1 + (h >> 2), LV_OPA_TRANSP, chart->coords.y2 - (h >> 2));
+       mask_fade_id = lv_draw_mask_add(&mask_fade_p, NULL);
+   }
+
     /*Go through all data lines*/
     LV_LL_READ_BACK(ext->series_ll, ser)
     {
         lv_coord_t start_point = ext->update_mode == LV_CHART_UPDATE_MODE_SHIFT ? ser->start_point : 0;
         style.body.main_color  = ser->color;
+        style.body.grad_color  = ser->color;
         style.body.opa         = ext->series.opa;
 
         p2.x = 0 + x_ofs;
@@ -1138,20 +1147,26 @@ static void lv_chart_draw_areas(lv_obj_t * chart, const lv_area_t * mask)
             y_tmp = y_tmp / (ext->ymax - ext->ymin);
             p2.y  = h - y_tmp + y_ofs;
 
+
             if(ser->points[p_prev] != LV_CHART_POINT_DEF && ser->points[p_act] != LV_CHART_POINT_DEF) {
-                lv_point_t triangle_points[3];
-                triangle_points[0]   = p1;
-                triangle_points[1]   = p2;
-                triangle_points[2].x = p1.x;
-                triangle_points[2].y = chart->coords.y2;
-                lv_draw_triangle(triangle_points, mask, &style, opa_scale);
-                triangle_points[2].x = p2.x;
-                triangle_points[0].y = chart->coords.y2;
-                lv_draw_triangle(triangle_points, mask, &style, opa_scale);
+                int16_t mask_line_id;
+                lv_draw_mask_line_param_t mask_line_p;
+                lv_draw_mask_line_points_init(&mask_line_p, p1.x, p1.y, p2.x, p2.y, LV_DRAW_MASK_LINE_SIDE_BOTTOM);
+                mask_line_id = lv_draw_mask_add(&mask_line_p, NULL);
+
+                lv_area_t a;
+                a.x1 = p1.x;
+                a.x2 = p2.x - 1;
+                a.y1 = LV_MATH_MIN(p1.y, p2.y);
+                a.y2 = chart->coords.y2;
+                lv_draw_rect(&a, mask, &style, opa_scale);
+
+                lv_draw_mask_remove_id(mask_line_id);
             }
             p_prev = p_act;
         }
     }
+    lv_draw_mask_remove_id(mask_fade_id);
 }
 
 /**
