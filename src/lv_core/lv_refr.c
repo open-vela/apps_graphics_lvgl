b6df16b59988a24de36c7f55205c94b68826097a
liujinye <liujinye@xiaomi.com>
2019-12-19 22:44:18 +0800

    new_style: integrate label

--- src/lv_core/lv_refr.c
+++ src/lv_core/lv_refr.c
@@ -73,8 +73,6 @@ void lv_refr_init(void)
  */
 void lv_refr_now(lv_disp_t * disp)
 {
-	lv_anim_refr_now();
-
     if(disp) {
         lv_disp_refr_task(disp->refr_task);
     } else {
@@ -437,12 +435,8 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
 
         /*If no better children check this object*/
         if(found_p == NULL) {
-            const lv_style_t * style = lv_obj_get_style(obj);
-            if(style->body.opa == LV_OPA_COVER && design_res == LV_DESIGN_RES_COVER &&
-               lv_obj_get_opa_scale(obj) == LV_OPA_COVER &&
-               style->body.blend_mode == LV_BLEND_MODE_NORMAL &&
-               style->body.border.blend_mode == LV_BLEND_MODE_NORMAL &&
-               style->image.blend_mode == LV_BLEND_MODE_NORMAL) {
+            if(design_res == LV_DESIGN_RES_COVER &&
+               lv_obj_get_opa_scale(obj) == LV_OPA_COVER) {
                 found_p = obj;
             }
         }
