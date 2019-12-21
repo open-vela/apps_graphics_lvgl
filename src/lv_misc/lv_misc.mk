a0b2134c2454dbda67fdae2014286fdb47d5f99f
openvela-robot <openvela-robot@xiaomi.com>
2019-12-21 23:07:18 +0800

    Merge branch 'c64-test'

--- src/lv_misc/lv_misc.mk
+++ src/lv_misc/lv_misc.mk
@@ -15,6 +15,7 @@ CSRCS += lv_async.c
 CSRCS += lv_printf.c
 CSRCS += lv_bidi.c
 
+
 DEPPATH += --dep-path $(LVGL_DIR)/lvgl/src/lv_misc
 VPATH += :$(LVGL_DIR)/lvgl/src/lv_misc
 
--- src/lv_misc/lv_txt.c
+++ src/lv_misc/lv_txt.c
@@ -103,7 +103,7 @@ void lv_txt_get_size(lv_point_t * size_res, const char * text, const lv_font_t *
     uint32_t line_start     = 0;
     uint32_t new_line_start = 0;
     lv_coord_t act_line_length;
-    uint16_t letter_height = lv_font_get_line_height(font);
+    uint8_t letter_height = lv_font_get_line_height(font);
 
     /*Calc. the height and longest line*/
     while(text[line_start] != '\0') {
@@ -205,7 +205,7 @@ static uint16_t lv_txt_get_next_word(const char * txt, const lv_font_t * font,
         }
 
         /* Test if this character fits within max_width */
-        if(break_index == NO_BREAK_FOUND && (cur_w - letter_space) > max_width) {
+        if(break_index == NO_BREAK_FOUND && cur_w > max_width) {
             break_index = i; 
             break_letter_count = word_len - 1;
             /* break_index is now pointing at the character that doesn't fit */
