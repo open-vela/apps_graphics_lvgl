b634bd595b30d1b0d382fabbe13bc36c35bc0745
liujinye <liujinye@xiaomi.com>
2019-12-26 15:15:00 +0800

    Update azure-pipelines.yml for Azure Pipelines

--- src/lv_objx/lv_btnm.c
+++ src/lv_objx/lv_btnm.c
@@ -29,7 +29,7 @@
  *  STATIC PROTOTYPES
  **********************/
 static lv_res_t lv_btnm_signal(lv_obj_t * btnm, lv_signal_t sign, void * param);
-static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mode_t mode);
+static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_area, lv_design_mode_t mode);
 static uint8_t get_button_width(lv_btnm_ctrl_t ctrl_bits);
 static bool button_is_hidden(lv_btnm_ctrl_t ctrl_bits);
 static bool button_is_repeat_disabled(lv_btnm_ctrl_t ctrl_bits);
@@ -80,7 +80,10 @@ lv_obj_t * lv_btnm_create(lv_obj_t * par, const lv_obj_t * copy)
     /*Allocate the object type specific extended data*/
     lv_btnm_ext_t * ext = lv_obj_allocate_ext_attr(new_btnm, sizeof(lv_btnm_ext_t));
     LV_ASSERT_MEM(ext);
-    if(ext == NULL) return NULL;
+    if(ext == NULL) {
+        lv_obj_del(new_btnm);
+        return NULL;
+    }
 
     ext->btn_cnt                          = 0;
     ext->btn_id_pr                        = LV_BTNM_BTN_NONE;
@@ -636,22 +639,21 @@ bool lv_btnm_get_one_toggle(const lv_obj_t * btnm)
 /**
  * Handle the drawing related tasks of the button matrixs
  * @param btnm pointer to a button matrix object
- * @param mask the object will be drawn only in this area
+ * @param clip_area the object will be drawn only in this area
  * @param mode LV_DESIGN_COVER_CHK: only check if the object fully covers the 'mask_p' area
  *                                  (return 'true' if yes)
  *             LV_DESIGN_DRAW: draw the object (always return 'true')
  *             LV_DESIGN_DRAW_POST: drawing after every children are drawn
- * @param return true/false, depends on 'mode'
+ * @param return an element of `lv_design_res_t`
  */
-static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mode_t mode)
+static lv_design_res_t lv_btnm_design(lv_obj_t * btnm, const lv_area_t * clip_area, lv_design_mode_t mode)
 {
     if(mode == LV_DESIGN_COVER_CHK) {
-        return ancestor_design_f(btnm, mask, mode);
-        /*Return false if the object is not covers the mask_p area*/
+        return ancestor_design_f(btnm, clip_area, mode);
     }
     /*Draw the object*/
     else if(mode == LV_DESIGN_DRAW_MAIN) {
-        ancestor_design_f(btnm, mask, mode);
+        ancestor_design_f(btnm, clip_area, mode);
 
         lv_btnm_ext_t * ext         = lv_obj_get_ext_attr(btnm);
         const lv_style_t * bg_style = lv_obj_get_style(btnm);
@@ -707,25 +709,25 @@ static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mo
             lv_style_copy(&style_tmp, btn_style);
 
             /*Remove borders on the edges if `LV_BORDER_INTERNAL`*/
-            if(style_tmp.body.border.part & LV_BORDER_INTERNAL) {
+            if(style_tmp.body.border.part & LV_BORDER_PART_INTERNAL) {
                 if(area_tmp.y1 == btnm->coords.y1 + bg_style->body.padding.top) {
-                    style_tmp.body.border.part &= ~LV_BORDER_TOP;
+                    style_tmp.body.border.part &= ~LV_BORDER_PART_TOP;
                 }
                 if(area_tmp.y2 == btnm->coords.y2 - bg_style->body.padding.bottom) {
-                    style_tmp.body.border.part &= ~LV_BORDER_BOTTOM;
+                    style_tmp.body.border.part &= ~LV_BORDER_PART_BOTTOM;
                 }
 
                 if(txt_i == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_LEFT;
+                    style_tmp.body.border.part &= ~LV_BORDER_PART_LEFT;
                 } else if(strcmp(ext->map_p[txt_i - 1], "\n") == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_LEFT;
+                    style_tmp.body.border.part &= ~LV_BORDER_PART_LEFT;
                 }
 
                 if(ext->map_p[txt_i + 1][0] == '\0' || strcmp(ext->map_p[txt_i + 1], "\n") == 0) {
-                    style_tmp.body.border.part &= ~LV_BORDER_RIGHT;
+                    style_tmp.body.border.part &= ~LV_BORDER_PART_RIGHT;
                 }
             }
-            lv_draw_rect(&area_tmp, mask, &style_tmp, opa_scale);
+            lv_draw_rect(&area_tmp, clip_area, &style_tmp, opa_scale);
 
             /*Calculate the size of the text*/
             if(btn_style->glass) btn_style = bg_style;
@@ -739,11 +741,10 @@ static bool lv_btnm_design(lv_obj_t * btnm, const lv_area_t * mask, lv_design_mo
             area_tmp.x2 = area_tmp.x1 + txt_size.x;
             area_tmp.y2 = area_tmp.y1 + txt_size.y;
 
-            lv_draw_label(&area_tmp, mask, btn_style, opa_scale, ext->map_p[txt_i], txt_flag, NULL, NULL, NULL, lv_obj_get_base_dir(btnm));
+            lv_draw_label(&area_tmp, clip_area, btn_style, opa_scale, ext->map_p[txt_i], txt_flag, NULL, NULL, NULL, lv_obj_get_base_dir(btnm));
         }
     }
-
-    return true;
+    return LV_DESIGN_RES_OK;
 }
 
 /**
@@ -1055,7 +1056,7 @@ static uint16_t get_button_from_point(lv_obj_t * btnm, lv_point_t * p)
         btn_area.y1 += btnm_cords.y1;
         btn_area.x2 += btnm_cords.x1;
         btn_area.y2 += btnm_cords.y1;
-        if(lv_area_is_point_on(&btn_area, p) != false) {
+        if(lv_area_is_point_on(&btn_area, p, 0) != false) {
             break;
         }
     }
