23867a484b1cea4ccc373f0f03d5e8df0684c437
liujinye <liujinye@xiaomi.com>
2020-01-06 15:06:01 +0800

    Merge branch 'dev-7.0' of https://github.com/littlevgl/lvgl into dev-7.0

--- src/lv_objx/lv_bar.h
+++ src/lv_objx/lv_bar.h
@@ -13,11 +13,7 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-#ifdef LV_CONF_INCLUDE_SIMPLE
-#include "lv_conf.h"
-#else
-#include "../../../lv_conf.h"
-#endif
+#include "../lv_conf_internal.h"
 
 #if LV_USE_BAR != 0
 
@@ -43,14 +39,25 @@ extern "C" {
 /** log2(LV_BAR_ANIM_STATE_END) used to normalize data*/
 #define LV_BAR_ANIM_STATE_NORM 8
 
-LV_EXPORT_CONST_INT(LV_BAR_ANIM_STATE_START);
-LV_EXPORT_CONST_INT(LV_BAR_ANIM_STATE_END);
-LV_EXPORT_CONST_INT(LV_BAR_ANIM_STATE_INV);
-LV_EXPORT_CONST_INT(LV_BAR_ANIM_STATE_NORM);
-
 /**********************
  *      TYPEDEFS
  **********************/
+
+enum {
+	LV_BAR_TYPE_NORMAL,
+	LV_BAR_TYPE_SYM,
+	LV_BAR_TYPE_CUSTOM
+};
+typedef uint8_t lv_bar_type_t;
+
+#if LV_USE_ANIMATION
+typedef struct {
+    lv_obj_t * bar;
+	lv_anim_value_t anim_start;
+    lv_anim_value_t anim_end;
+	lv_anim_value_t anim_state;
+} lv_bar_anim_t;
+#endif
 
 /** Data of bar*/
 typedef struct
@@ -61,13 +68,14 @@ typedef struct
     int16_t cur_value; /*Current value of the bar*/
     int16_t min_value; /*Minimum value of the bar*/
     int16_t max_value; /*Maximum value of the bar*/
+    int16_t start_value; /*Start value of the bar*/
+    lv_area_t indic_area;   /*Save the indicator area. MIght be used by derived types*/
 #if LV_USE_ANIMATION
-    lv_anim_value_t anim_start;
-    lv_anim_value_t anim_end;
-    lv_anim_value_t anim_state;
     lv_anim_value_t anim_time;
+    lv_bar_anim_t cur_value_anim;
+	lv_bar_anim_t start_value_anim;
 #endif
-    uint8_t sym : 1;                /*Symmetric: means the center is around zero value*/
+	uint8_t type : 2;			/*Type of bar*/
     const lv_style_t * style_indic; /*Style of the indicator*/
 } lv_bar_ext_t;
 
@@ -103,6 +111,14 @@ lv_obj_t * lv_bar_create(lv_obj_t * par, const lv_obj_t * copy);
 void lv_bar_set_value(lv_obj_t * bar, int16_t value, lv_anim_enable_t anim);
 
 /**
+ * Set a new start value on the bar
+ * @param bar pointer to a bar object
+ * @param value new start value
+ * @param anim LV_ANIM_ON: set the value with an animation; LV_ANIM_OFF: change the value immediatelly
+ */
+void lv_bar_set_start_value(lv_obj_t * bar, int16_t start_value, lv_anim_enable_t anim);
+
+/**
  * Set minimum and the maximum values of a bar
  * @param bar pointer to the bar object
  * @param min minimum value
@@ -111,12 +127,11 @@ void lv_bar_set_value(lv_obj_t * bar, int16_t value, lv_anim_enable_t anim);
 void lv_bar_set_range(lv_obj_t * bar, int16_t min, int16_t max);
 
 /**
- * Make the bar symmetric to zero. The indicator will grow from zero instead of the minimum
- * position.
- * @param bar pointer to a bar object
- * @param en true: enable disable symmetric behavior; false: disable
+ * Set the type of bar.
+ * @param bar pointer to bar object
+ * @param type bar type
  */
-void lv_bar_set_sym(lv_obj_t * bar, bool en);
+void lv_bar_set_type(lv_obj_t * bar, lv_bar_type_t type);
 
 /**
  * Set the animation time of the bar
@@ -145,6 +160,13 @@ void lv_bar_set_style(lv_obj_t * bar, lv_bar_style_t type, const lv_style_t * st
 int16_t lv_bar_get_value(const lv_obj_t * bar);
 
 /**
+ * Get the start value of a bar
+ * @param bar pointer to a bar object
+ * @return the start value of the bar
+ */
+int16_t lv_bar_get_start_value(const lv_obj_t * bar);
+
+/**
  * Get the minimum value of a bar
  * @param bar pointer to a bar object
  * @return the minimum value of the bar
@@ -159,18 +181,18 @@ int16_t lv_bar_get_min_value(const lv_obj_t * bar);
 int16_t lv_bar_get_max_value(const lv_obj_t * bar);
 
 /**
- * Get whether the bar is symmetric or not.
- * @param bar pointer to a bar object
- * @return true: symmetric is enabled; false: disable
+ * Get the type of bar.
+ * @param bar pointer to bar object
+ * @return bar type
  */
-bool lv_bar_get_sym(lv_obj_t * bar);
+lv_bar_type_t lv_bar_get_type(lv_obj_t * bar);
 
 /**
  * Get the animation time of the bar
  * @param bar pointer to a bar object
  * @return the animation time in milliseconds.
  */
-uint16_t lv_bar_get_anim_time(lv_obj_t * bar);
+uint16_t lv_bar_get_anim_time(const lv_obj_t * bar);
 
 /**
  * Get a style of a bar
