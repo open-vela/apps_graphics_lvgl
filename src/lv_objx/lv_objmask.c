4079286b15060208f3c8c6abef1b4a78f74c5fb6
openvela-robot <openvela-robot@xiaomi.com>
2020-01-14 21:16:48 +0800

    minor fixes

--- src/lv_objx/lv_objmask.c
+++ src/lv_objx/lv_objmask.c
@@ -77,7 +77,7 @@ lv_obj_t * lv_objmask_create(lv_obj_t * par, const lv_obj_t * copy)
 
     /*Init the new object mask object mask*/
     if(copy == NULL) {
-        lv_objmask_set_style(new_objmask, LV_OBJMASK_STYLE_BG, &lv_style_transp);
+        lv_obj_reset_style(new_objmask, LV_OBJMASK_PART_MAIN);
 
     }
     /*TODO: Copy an existing object mask*/
