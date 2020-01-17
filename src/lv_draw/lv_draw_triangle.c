1c59f34c9503290169b3ad30554224a753b1d202
openvela-robot <openvela-robot@xiaomi.com>
2020-01-17 15:30:19 +0800

    merge dev-7.0

--- src/lv_draw/lv_draw_triangle.c
+++ src/lv_draw/lv_draw_triangle.c
@@ -155,7 +155,7 @@ void lv_draw_polygon(const lv_point_t * points, uint16_t point_cnt, const lv_are
 
 
 
-    lv_draw_rect(&poly_coords, &poly_mask, style, opa_scale);
+    lv_draw_rect(&poly_coords, &poly_mask, style);
 
     lv_draw_mask_remove_custom(mp);
 
