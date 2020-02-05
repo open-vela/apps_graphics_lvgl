097ba8e42d31be5a87a13740c81d232097e2feaa
openvela-robot <openvela-robot@xiaomi.com>
2020-02-05 10:40:50 +0800

    improve arc invalidation

--- src/lv_draw/lv_draw_img.h
+++ src/lv_draw/lv_draw_img.h
@@ -29,26 +29,11 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
-typedef struct {
-    lv_opa_t opa;
-
-    uint16_t angle;
-    lv_point_t pivot;
-    uint16_t zoom;
-
-    lv_opa_t recolor_opa;
-    lv_color_t recolor;
-
-    lv_blend_mode_t blend_mode;
-
-    uint8_t antialias       :1;
-} lv_draw_img_dsc_t;
 
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
-void lv_draw_img_dsc_init(lv_draw_img_dsc_t * dsc);
 /**
  * Draw an image
  * @param coords the coordinates of the image
@@ -60,7 +45,8 @@ void lv_draw_img_dsc_init(lv_draw_img_dsc_t * dsc);
  * @param antialias anti-alias transformations (rotate, zoom) or not
  * @param opa_scale scale down all opacities by the factor
  */
-void lv_draw_img(const lv_area_t * coords, const lv_area_t * mask, const void * src, lv_draw_img_dsc_t * dsc);
+void lv_draw_img(const lv_area_t * coords, const lv_area_t * mask, const void * src, const lv_style_t * style,
+			uint16_t angle, lv_point_t * center, uint16_t zoom, bool antialaias, lv_opa_t opa_scale);
 
 /**
  * Get the type of an image source
--- src/lv_objx/lv_preload.h
+++ src/lv_objx/lv_preload.h
@@ -68,13 +68,9 @@ typedef struct
     lv_preload_dir_t anim_dir : 1;   /*Animation Direction*/
 } lv_preload_ext_t;
 
-/*Parts of the preloader*/
+/*Styles*/
 enum {
-    LV_PRELOAD_PART_BG = LV_ARC_PART_BG,
-    LV_PRELOAD_PART_ARC = LV_ARC_PART_ARC,
-    _LV_PRELOAD_PART_VIRTUAL_LAST,
-
-    _LV_PRELOAD_PART_REAL_LAST = _LV_ARC_PART_REAL_LAST,
+    LV_PRELOAD_STYLE_MAIN,
 };
 typedef uint8_t lv_preload_style_t;
 
