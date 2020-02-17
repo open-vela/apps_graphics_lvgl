641b810aacd6ace87ec752cce7f24fbc16941491
liujinye <liujinye@xiaomi.com>
2020-02-17 14:36:32 +0800

    style transition fixes

--- src/lv_misc/lv_math.h
+++ src/lv_misc/lv_math.h
@@ -19,7 +19,13 @@ extern "C" {
  *      DEFINES
  *********************/
 #define LV_MATH_MIN(a, b) ((a) < (b) ? (a) : (b))
+#define LV_MATH_MIN3(a, b, c) (LV_MATH_MIN(LV_MATH_MIN(a,b), c))
+#define LV_MATH_MIN4(a, b, c, d) (LV_MATH_MIN(LV_MATH_MIN(a,b), LV_MATH_MIN(c,d)))
+
 #define LV_MATH_MAX(a, b) ((a) > (b) ? (a) : (b))
+#define LV_MATH_MAX3(a, b, c) (LV_MATH_MAX(LV_MATH_MAX(a,b), c))
+#define LV_MATH_MAX4(a, b, c, d) (LV_MATH_MAX(LV_MATH_MAX(a,b), LV_MATH_MAX(c,d)))
+
 #define LV_MATH_ABS(x) ((x) > 0 ? (x) : (-(x)))
 
 #define LV_TRIGO_SIN_MAX 32767
@@ -31,6 +37,12 @@ extern "C" {
 /**********************
  *      TYPEDEFS
  **********************/
+
+typedef struct {
+     uint16_t i;
+     uint16_t f;
+ }lv_sqrt_res_t;
+
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -54,6 +66,8 @@ int16_t lv_trigo_sin(int16_t angle);
  */
 int32_t lv_bezier3(uint32_t t, int32_t u0, int32_t u1, int32_t u2, int32_t u3);
 
+void lv_sqrt(uint32_t x, lv_sqrt_res_t * q);
+
 /**
  * Calculate the atan2 of a vector.
  * @param x
@@ -61,13 +75,6 @@ int32_t lv_bezier3(uint32_t t, int32_t u0, int32_t u1, int32_t u2, int32_t u3);
  * @return the angle in degree calculated from the given parameters in range of [0..360]
  */
 uint16_t lv_atan2(int x, int y);
-
-/**
- * Calculate the integer square root of a number.
- * @param num
- * @return square root of 'num'
- */
-uint32_t lv_sqrt(uint32_t num);
 
 /**********************
  *      MACROS
