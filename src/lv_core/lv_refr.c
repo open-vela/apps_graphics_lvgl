7c95bb703d72bcead8bb4145d661463c54d5c437
openvela-robot <openvela-robot@xiaomi.com>
2020-02-26 23:04:45 +0800

    Merge branch 'master' of https://github.com/littlevgl/lvgl

--- src/lv_core/lv_refr.c
+++ src/lv_core/lv_refr.c
@@ -17,7 +17,7 @@
 #include "../lv_draw/lv_draw.h"
 
 #if defined(LV_GC_INCLUDE)
-    #include LV_GC_INCLUDE
+#include LV_GC_INCLUDE
 #endif /* LV_ENABLE_GC */
 
 /*********************
@@ -73,14 +73,9 @@ void lv_refr_init(void)
  */
 void lv_refr_now(lv_disp_t * disp)
 {
-#if LV_USE_ANIMATION
-    lv_anim_refr_now();
-#endif
-
     if(disp) {
         lv_disp_refr_task(disp->refr_task);
-    }
-    else {
+    } else {
         lv_disp_t * d;
         d = lv_disp_get_next(NULL);
         while(d) {
@@ -125,19 +120,17 @@ void lv_inv_area(lv_disp_t * disp, const lv_area_t * area_p)
         /*Save only if this area is not in one of the saved areas*/
         uint16_t i;
         for(i = 0; i < disp->inv_p; i++) {
-            if(lv_area_is_in(&com_area, &disp->inv_areas[i], 0) != false) return;
+            if(lv_area_is_in(&com_area, &disp->inv_areas[i]) != false) return;
         }
 
         /*Save the area*/
         if(disp->inv_p < LV_INV_BUF_SIZE) {
             lv_area_copy(&disp->inv_areas[disp->inv_p], &com_area);
-        }
-        else {   /*If no place for the area add the screen*/
+        } else { /*If no place for the area add the screen*/
             disp->inv_p = 0;
             lv_area_copy(&disp->inv_areas[disp->inv_p], &scr_area);
         }
         disp->inv_p++;
-        lv_task_set_prio(disp->refr_task, LV_REFR_TASK_PRIO);
     }
 }
 
@@ -171,16 +164,7 @@ void lv_disp_refr_task(lv_task_t * task)
 
     uint32_t start = lv_tick_get();
 
-    /* Ensure the task does not run again automatically.
-     * This is done before refreshing in case refreshing invalidates something else.
-     */
-    lv_task_set_prio(task, LV_TASK_PRIO_OFF);
-
     disp_refr = task->user_data;
-
-
-    //    extern rect_cache_t cache[];
-    //    extern uint32_t cp;
 
     lv_refr_join_area();
 
@@ -233,7 +217,7 @@ void lv_disp_refr_task(lv_task_t * task)
         }
     }
 
-    lv_mem_buf_free_all();
+    lv_draw_free_buf();
 
     LV_LOG_TRACE("lv_refr_task: ready");
 }
@@ -350,8 +334,7 @@ static void lv_refr_area(const lv_area_t * area_p)
                 LV_LOG_WARN("Can't set VDB height using the round function. (Wrong round_cb or to "
                             "small VDB)");
                 return;
-            }
-            else {
+            } else {
                 max_row = tmp.y2 + 1;
             }
         }
@@ -435,13 +418,10 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
     lv_obj_t * found_p = NULL;
 
     /*If this object is fully cover the draw area check the children too */
-    if(lv_area_is_in(area_p, &obj->coords, 0) && obj->hidden == 0) {
-        lv_design_res_t design_res = obj->design_cb ? obj->design_cb(obj, area_p,
-                                                                     LV_DESIGN_COVER_CHK) : LV_DESIGN_RES_NOT_COVER;
-        if(design_res == LV_DESIGN_RES_MASKED) return NULL;
-
+    if(lv_area_is_in(area_p, &obj->coords) && obj->hidden == 0) {
         lv_obj_t * i;
-        LV_LL_READ(obj->child_ll, i) {
+        LV_LL_READ(obj->child_ll, i)
+        {
             found_p = lv_refr_get_top_obj(area_p, i);
 
             /*If a children is ok then break*/
@@ -450,9 +430,11 @@ static lv_obj_t * lv_refr_get_top_obj(const lv_area_t * area_p, lv_obj_t * obj)
             }
         }
 
-        /*If no better children use this object*/
+        /*If no better children check this object*/
         if(found_p == NULL) {
-            if(design_res == LV_DESIGN_RES_COVER) {
+            const lv_style_t * style = lv_obj_get_style(obj);
+            if(style->body.opa == LV_OPA_COVER && obj->design_cb(obj, area_p, LV_DESIGN_COVER_CHK) != false &&
+               lv_obj_get_opa_scale(obj) == LV_OPA_COVER) {
                 found_p = obj;
             }
         }
@@ -494,7 +476,7 @@ static void lv_refr_obj_and_children(lv_obj_t * top_p, const lv_area_t * mask_p)
         }
 
         /*Call the post draw design function of the parents of the to object*/
-        if(par->design_cb) par->design_cb(par, mask_p, LV_DESIGN_DRAW_POST);
+        par->design_cb(par, mask_p, LV_DESIGN_DRAW_POST);
 
         /*The new border will be there last parents,
          *so the 'younger' brothers of parent will be refreshed*/
@@ -532,24 +514,13 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
     if(union_ok != false) {
 
         /* Redraw the object */
-        if(obj->design_cb) obj->design_cb(obj, &obj_ext_mask, LV_DESIGN_DRAW_MAIN);
+        obj->design_cb(obj, &obj_ext_mask, LV_DESIGN_DRAW_MAIN);
 
 #if MASK_AREA_DEBUG
         static lv_color_t debug_color = LV_COLOR_RED;
-        lv_draw_rect_dsc_t draw_dsc;
-        lv_draw_rect_dsc_init(&draw_dsc);
-        draw_dsc.bg_color.full = debug_color.full;
-        draw_dsc.bg_opa = LV_OPA_20;
-        draw_dsc.border_width = 2;
-        draw_dsc.border_opa = LV_OPA_50;
-        draw_dsc.border_color.full = (debug_color.full + 0x13) * 9;
-
-        lv_draw_rect(&obj_ext_mask, &obj_ext_mask, &draw_dsc);
+        lv_draw_fill(&obj_ext_mask, &obj_ext_mask, debug_color, LV_OPA_50);
         debug_color.full *= 17;
         debug_color.full += 0xA1;
-#if LV_COLOR_DEPTH == 32
-        debug_color.ch.alpha = 0xff;
-#endif
 #endif
         /*Create a new 'obj_mask' without 'ext_size' because the children can't be visible there*/
         lv_obj_get_coords(obj, &obj_area);
@@ -558,7 +529,8 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
             lv_area_t mask_child; /*Mask from obj and its child*/
             lv_obj_t * child_p;
             lv_area_t child_area;
-            LV_LL_READ_BACK(obj->child_ll, child_p) {
+            LV_LL_READ_BACK(obj->child_ll, child_p)
+            {
                 lv_obj_get_coords(child_p, &child_area);
                 ext_size = child_p->ext_draw_pad;
                 child_area.x1 -= ext_size;
@@ -578,7 +550,7 @@ static void lv_refr_obj(lv_obj_t * obj, const lv_area_t * mask_ori_p)
         }
 
         /* If all the children are redrawn make 'post draw' design */
-        if(obj->design_cb) obj->design_cb(obj, &obj_ext_mask, LV_DESIGN_DRAW_POST);
+        obj->design_cb(obj, &obj_ext_mask, LV_DESIGN_DRAW_POST);
     }
 }
 
