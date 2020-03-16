e46a60b3c524e650a57acd7aa7f3bb2e046fd67c
openvela-robot <openvela-robot@xiaomi.com>
2020-03-16 15:46:31 +0800

    imporve light theme + minor fixes

--- src/lv_widgets/lv_dropdown.c
+++ src/lv_widgets/lv_dropdown.c
@@ -178,26 +178,6 @@ void lv_dropdown_set_text(lv_obj_t * ddlist, const char * txt)
 }
 
 /**
- * Clear any options in a drop down list.  Static or dynamic.
- * @param ddlist pointer to drop down list object
- */
-void lv_dropdown_clear_options(lv_obj_t * ddlist)
-{
-    LV_ASSERT_OBJ(ddlist, LV_OBJX_NAME);
-    lv_dropdown_ext_t * ext = lv_obj_get_ext_attr(ddlist);
-    if(ext->options == NULL) return;
-
-    if(ext->static_txt == 0)
-        lv_mem_free(ext->options);
-
-    ext->options = NULL;
-    ext->static_txt = 0;
-    ext->option_cnt = 0;
-
-    lv_obj_invalidate(ddlist);
-}
-
-/**
  * Set the options in a drop down list from a string
  * @param ddlist pointer to drop down list object
  * @param options a string with '\n' separated options. E.g. "One\nTwo\nThree"
@@ -282,18 +262,8 @@ void lv_dropdown_add_option(lv_obj_t * ddlist, const char * option, uint16_t pos
 
     lv_dropdown_ext_t * ext = lv_obj_get_ext_attr(ddlist);
 
-    /*Convert static options to dynmaic*/
-    if(ext->static_txt != 0) {
-        char * static_options = ext->options;
-        size_t len = strlen(static_options) + 1;
-
-        ext->options = lv_mem_alloc(len);
-        LV_ASSERT_MEM(ext->options);
-        if(ext->options == NULL) return;
-
-        strcpy(ext->options, static_options);
-        ext->static_txt = 0;
-    }
+    /*Can not append to static options*/
+    if(ext->static_txt != 0) return;
 
     /*Allocate space for the new option*/
     size_t old_len = (ext->options == NULL) ? 0 : strlen(ext->options);
@@ -635,19 +605,22 @@ void lv_dropdown_open(lv_obj_t * ddlist, lv_anim_enable_t anim)
     else if(ext->dir == LV_DROPDOWN_DIR_RIGHT)lv_obj_align(ext->page, ddlist, LV_ALIGN_OUT_RIGHT_TOP, 0, 0);
 
     lv_obj_t * scr = lv_scr_act();
+    bool moved = false;
     if(ext->dir != LV_DROPDOWN_DIR_UP) {
         if(ext->page->coords.y2 > scr->coords.y2) {
             lv_obj_set_y(ext->page, lv_obj_get_y(ext->page) - (ext->page->coords.y2 - scr->coords.y2));
+            moved = true;
         }
     }
     else {
         if(ext->page->coords.y1 < 0) {
             lv_obj_set_y(ext->page, 0);
+            moved = true;
         }
     }
 
 #if LV_USE_ANIMATION
-    if(anim == LV_ANIM_ON && ext->dir != LV_DROPDOWN_DIR_UP) {
+    if(anim == LV_ANIM_ON && ext->dir != LV_DROPDOWN_DIR_UP && !moved) {
         lv_anim_t a;
         lv_anim_init(&a);
         lv_anim_set_var(&a, ddlist);
