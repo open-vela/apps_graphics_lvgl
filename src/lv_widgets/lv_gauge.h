3ec6b647eb9ca29cdbd8f58b722e8dfd85621f51
openvela-robot <openvela-robot@xiaomi.com>
2020-03-17 13:54:49 +0800

    Merge branch 'dev-7.0' of https://github.com/littlevgl/lvgl into dev-7.0

--- src/lv_widgets/lv_gauge.h
+++ src/lv_widgets/lv_gauge.h
@@ -35,8 +35,6 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
-typedef void (*value_format_type)(lv_obj_t * gauge, char buf[], int bufsize, lv_gauge_value_t value);
-
 /*Data of gauge*/
 typedef struct {
     lv_linemeter_ext_t lmeter; /*Ext. of ancestor*/
@@ -49,7 +47,6 @@ typedef struct {
     lv_style_list_t style_strong;
     uint8_t needle_count;             /*Number of needles*/
     uint8_t label_count;              /*Number of labels on the scale*/
-    value_format_type value_format_cb;
 } lv_gauge_ext_t;
 
 /*Styles*/
@@ -146,13 +143,6 @@ static inline void lv_gauge_set_angle_offset(lv_obj_t * gauge, uint16_t angle)
  * @param pivot_y the Y coordinate of rotation center of the image
  */
 void lv_gauge_set_needle_img(lv_obj_t * gauge, const void * img, lv_coord_t pivot_x, lv_coord_t pivot_y);
-
-/**
- * Assign a function to format gauge values
- * @param gauge pointer to a gauge object
- * @param value_format_fn pointer to function of value_format_type
- */
-void lv_gauge_set_formatter(lv_obj_t * gauge, value_format_type value_format_fn);
 
 /*=====================
  * Getter functions
