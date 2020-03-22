bf71ca079f68d4fc55f3be15c477b0ca1ce04c9c
liujinye <liujinye@xiaomi.com>
2020-03-22 13:43:13 +0800

    lv_mbox: remove incorrect assignment (#1415)

--- src/lv_misc/lv_task.h
+++ src/lv_misc/lv_task.h
@@ -14,7 +14,11 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-#include "../lv_conf_internal.h"
+#ifdef LV_CONF_INCLUDE_SIMPLE
+#include "lv_conf.h"
+#else
+#include "../../../lv_conf.h"
+#endif
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -27,8 +31,6 @@ extern "C" {
 #ifndef LV_ATTRIBUTE_TASK_HANDLER
 #define LV_ATTRIBUTE_TASK_HANDLER
 #endif
-
-#define LV_NO_TASK_READY 0xFFFFFFFF
 /**********************
  *      TYPEDEFS
  **********************/
@@ -57,7 +59,8 @@ typedef uint8_t lv_task_prio_t;
 /**
  * Descriptor of a lv_task
  */
-typedef struct _lv_task_t {
+typedef struct _lv_task_t
+{
     uint32_t period; /**< How often the task should run */
     uint32_t last_run; /**< Last time the task ran */
     lv_task_cb_t task_cb; /**< Task function */
@@ -81,9 +84,8 @@ void lv_task_core_init(void);
 
 /**
  * Call it  periodically to handle lv_tasks.
- * @return time till it needs to be run next (in ms)
  */
-LV_ATTRIBUTE_TASK_HANDLER uint32_t lv_task_handler(void);
+LV_ATTRIBUTE_TASK_HANDLER void lv_task_handler(void);
 
 //! @endcond
 
