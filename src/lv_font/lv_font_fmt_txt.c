f1846085bc55358110c663faabd83f92876928a6
openvela-robot <openvela-robot@xiaomi.com>
2020-04-30 11:26:35 +0800

    win: fix button's size

--- src/lv_font/lv_font_fmt_txt.c
+++ src/lv_font/lv_font_fmt_txt.c
@@ -26,7 +26,7 @@ typedef enum {
     RLE_STATE_SINGLE = 0,
     RLE_STATE_REPEATE,
     RLE_STATE_COUNTER,
-}rle_state_t;
+} rle_state_t;
 
 /**********************
  *  STATIC PROTOTYPES
@@ -38,11 +38,10 @@ static int32_t kern_pair_8_compare(const void * ref, const void * element);
 static int32_t kern_pair_16_compare(const void * ref, const void * element);
 
 static void decompress(const uint8_t * in, uint8_t * out, lv_coord_t w, lv_coord_t h, uint8_t bpp);
-static void decompress_line(uint8_t * out, lv_coord_t w);
-static uint8_t get_bits(const uint8_t * in, uint32_t bit_pos, uint8_t len);
-static void bits_write(uint8_t * out, uint32_t bit_pos, uint8_t val, uint8_t len);
-static void rle_init(const uint8_t * in,  uint8_t bpp);
-static uint8_t rle_next(void);
+static inline void decompress_line(uint8_t * out, lv_coord_t w);
+static inline uint8_t get_bits(const uint8_t * in, uint32_t bit_pos, uint8_t len);
+static inline void rle_init(const uint8_t * in,  uint8_t bpp);
+static inline uint8_t rle_next(void);
 
 
 /**********************
@@ -76,6 +75,8 @@ static rle_state_t rle_state;
  */
 const uint8_t * lv_font_get_bitmap_fmt_txt(const lv_font_t * font, uint32_t unicode_letter)
 {
+    if(unicode_letter == '\t') unicode_letter = ' ';
+
     lv_font_fmt_txt_dsc_t * fdsc = (lv_font_fmt_txt_dsc_t *) font->dsc;
     uint32_t gid = get_glyph_dsc_id(font, unicode_letter);
     if(!gid) return NULL;
@@ -86,29 +87,19 @@ const uint8_t * lv_font_get_bitmap_fmt_txt(const lv_font_t * font, uint32_t unic
         if(gdsc) return &fdsc->glyph_bitmap[gdsc->bitmap_index];
     }
     /*Handle compressed bitmap*/
-    else
-    {
+    else {
         static uint8_t * buf = NULL;
 
         uint32_t gsize = gdsc->box_w * gdsc->box_h;
         if(gsize == 0) return NULL;
 
-        uint32_t buf_size = gsize;
-        /*Compute memory size needed to hold decompressed glyph, rounding up*/
-        switch(fdsc->bpp) {
-        case 1: buf_size = (gsize + 7) >> 3;  break;
-        case 2: buf_size = (gsize + 3) >> 2;  break;
-        case 3: buf_size = (gsize + 1) >> 1;  break;
-        case 4: buf_size = (gsize + 1) >> 1;  break;
-        }
-
-        if(lv_mem_get_size(buf) < buf_size) {
-            buf = lv_mem_realloc(buf, buf_size);
+        if(lv_mem_get_size(buf) < gsize) {
+            buf = lv_mem_realloc(buf, gsize);
             LV_ASSERT_MEM(buf);
             if(buf == NULL) return NULL;
         }
 
-        decompress(&fdsc->glyph_bitmap[gdsc->bitmap_index], buf, gdsc->box_w , gdsc->box_h, (uint8_t)fdsc->bpp);
+        decompress(&fdsc->glyph_bitmap[gdsc->bitmap_index], buf, gdsc->box_w, gdsc->box_h, (uint8_t)fdsc->bpp);
         return buf;
     }
 
@@ -124,8 +115,14 @@ const uint8_t * lv_font_get_bitmap_fmt_txt(const lv_font_t * font, uint32_t unic
  * @return true: descriptor is successfully loaded into `dsc_out`.
  *         false: the letter was not found, no data is loaded to `dsc_out`
  */
-bool lv_font_get_glyph_dsc_fmt_txt(const lv_font_t * font, lv_font_glyph_dsc_t * dsc_out, uint32_t unicode_letter, uint32_t unicode_letter_next)
+bool lv_font_get_glyph_dsc_fmt_txt(const lv_font_t * font, lv_font_glyph_dsc_t * dsc_out, uint32_t unicode_letter,
+                                   uint32_t unicode_letter_next)
 {
+    bool is_tab = false;
+    if(unicode_letter == '\t') {
+        unicode_letter = ' ';
+        is_tab = true;
+    }
     lv_font_fmt_txt_dsc_t * fdsc = (lv_font_fmt_txt_dsc_t *) font->dsc;
     uint32_t gid = get_glyph_dsc_id(font, unicode_letter);
     if(!gid) return false;
@@ -143,7 +140,10 @@ bool lv_font_get_glyph_dsc_fmt_txt(const lv_font_t * font, lv_font_glyph_dsc_t *
 
     int32_t kv = ((int32_t)((int32_t)kvalue * fdsc->kern_scale) >> 4);
 
-    uint32_t adv_w = gdsc->adv_w + kv;
+    uint32_t adv_w = gdsc->adv_w;
+    if(is_tab) adv_w *= 2;
+
+    adv_w += kv;
     adv_w  = (adv_w + (1 << 3)) >> 4;
 
     dsc_out->adv_w = adv_w;
@@ -151,7 +151,9 @@ bool lv_font_get_glyph_dsc_fmt_txt(const lv_font_t * font, lv_font_glyph_dsc_t *
     dsc_out->box_w = gdsc->box_w;
     dsc_out->ofs_x = gdsc->ofs_x;
     dsc_out->ofs_y = gdsc->ofs_y;
-    dsc_out->bpp   = (uint8_t)fdsc->bpp;
+    dsc_out->bpp   = fdsc->bitmap_format == LV_FONT_FMT_TXT_PLAIN ? (uint8_t)fdsc->bpp : 8;
+
+    if(is_tab) dsc_out->box_w = dsc_out->box_w * 2;
 
     return true;
 }
@@ -184,7 +186,8 @@ static uint32_t get_glyph_dsc_id(const lv_font_t * font, uint32_t letter)
             glyph_id = fdsc->cmaps[i].glyph_id_start + gid_ofs_8[rcp];
         }
         else if(fdsc->cmaps[i].type == LV_FONT_FMT_TXT_CMAP_SPARSE_TINY) {
-            uint8_t * p = lv_utils_bsearch(&rcp, fdsc->cmaps[i].unicode_list, fdsc->cmaps[i].list_length, sizeof(fdsc->cmaps[i].unicode_list[0]), unicode_list_compare);
+            uint8_t * p = lv_utils_bsearch(&rcp, fdsc->cmaps[i].unicode_list, fdsc->cmaps[i].list_length,
+                                           sizeof(fdsc->cmaps[i].unicode_list[0]), unicode_list_compare);
 
             if(p) {
                 lv_uintptr_t ofs = (lv_uintptr_t)(p - (uint8_t *) fdsc->cmaps[i].unicode_list);
@@ -193,10 +196,11 @@ static uint32_t get_glyph_dsc_id(const lv_font_t * font, uint32_t letter)
             }
         }
         else if(fdsc->cmaps[i].type == LV_FONT_FMT_TXT_CMAP_SPARSE_FULL) {
-            uint8_t * p = lv_utils_bsearch(&rcp, fdsc->cmaps[i].unicode_list, fdsc->cmaps[i].list_length, sizeof(fdsc->cmaps[i].unicode_list[0]), unicode_list_compare);
+            uint8_t * p = lv_utils_bsearch(&rcp, fdsc->cmaps[i].unicode_list, fdsc->cmaps[i].list_length,
+                                           sizeof(fdsc->cmaps[i].unicode_list[0]), unicode_list_compare);
 
             if(p) {
-                lv_uintptr_t ofs = (lv_uintptr_t)(p - (uint8_t*) fdsc->cmaps[i].unicode_list);
+                lv_uintptr_t ofs = (lv_uintptr_t)(p - (uint8_t *) fdsc->cmaps[i].unicode_list);
                 ofs = ofs >> 1;     /*The list stores `uint16_t` so the get the index divide by 2*/
                 const uint8_t * gid_ofs_16 = fdsc->cmaps[i].glyph_id_ofs_list;
                 glyph_id = fdsc->cmaps[i].glyph_id_start + gid_ofs_16[ofs];
@@ -237,7 +241,8 @@ static int8_t get_kern_value(const lv_font_t * font, uint32_t gid_left, uint32_t
                 ofs = ofs >> 1;     /*ofs is for pair, divide by 2 to refer as a single value*/
                 value = kdsc->values[ofs];
             }
-        } else if(kdsc->glyph_ids_size == 1) {
+        }
+        else if(kdsc->glyph_ids_size == 1) {
             /* Use binary search to find the kern value.
              * The pairs are ordered left_id first, then right_id secondly. */
             const uint16_t * g_ids = kdsc->glyph_ids;
@@ -246,15 +251,17 @@ static int8_t get_kern_value(const lv_font_t * font, uint32_t gid_left, uint32_t
 
             /*If the `g_id_both` were found get its index from the pointer*/
             if(kid_p) {
-                lv_uintptr_t ofs = (lv_uintptr_t) (kid_p - (const uint8_t *)g_ids);
+                lv_uintptr_t ofs = (lv_uintptr_t)(kid_p - (const uint8_t *)g_ids);
                 ofs = ofs >> 4;     /*ofs is 4 byte pairs, divide by 4 to refer as a single value*/
                 value = kdsc->values[ofs];
             }
 
-        } else {
+        }
+        else {
             /*Invalid value*/
         }
-    } else {
+    }
+    else {
         /*Kern classes*/
         const lv_font_fmt_txt_kern_classes_t * kdsc = fdsc->kern_dsc;
         uint8_t left_class = kdsc->left_class_mapping[gid_left];
@@ -263,7 +270,7 @@ static int8_t get_kern_value(const lv_font_t * font, uint32_t gid_left, uint32_t
         /* If class = 0, kerning not exist for that glyph
          * else got the value form `class_pair_values` 2D array*/
         if(left_class > 0 && right_class > 0) {
-            value = kdsc->class_pair_values[(left_class-1)* kdsc->right_class_cnt + (right_class-1)];
+            value = kdsc->class_pair_values[(left_class - 1) * kdsc->right_class_cnt + (right_class - 1)];
         }
 
     }
@@ -300,23 +307,41 @@ static int32_t kern_pair_16_compare(const void * ref, const void * element)
  */
 static void decompress(const uint8_t * in, uint8_t * out, lv_coord_t w, lv_coord_t h, uint8_t bpp)
 {
-    uint32_t wrp = 0;
-    uint8_t wr_size = bpp;
-    if(bpp == 3) wr_size = 4;
-
     rle_init(in, bpp);
 
-    uint8_t * line_buf = lv_draw_get_buf(w * 2);
-    uint8_t * line_buf1 = line_buf;
-    uint8_t * line_buf2 = line_buf + w;
+    const uint8_t * bpp_opa_table;
+    switch(bpp) {
+        case 1:
+            bpp_opa_table = _lv_bpp1_opa_table;
+            break;
+        case 2:
+            bpp_opa_table = _lv_bpp2_opa_table;
+            break;
+        case 3:
+            bpp_opa_table = _lv_bpp3_opa_table;
+            break;
+        case 4:
+            bpp_opa_table = _lv_bpp4_opa_table;
+            break;
+        case 8:
+            bpp_opa_table = _lv_bpp8_opa_table;
+            break;
+        default:
+            LV_LOG_WARN("Invalid bpp (%d)", bpp);
+            return;
+    }
+
+    uint8_t * line_buf1 = lv_mem_buf_get(w);
+    uint8_t * line_buf2 = lv_mem_buf_get(w);
 
     decompress_line(line_buf1, w);
 
     lv_coord_t y;
     lv_coord_t x;
+
     for(x = 0; x < w; x++) {
-        bits_write(out,wrp, line_buf1[x], bpp);
-        wrp += wr_size;
+        *out = bpp_opa_table[line_buf1[x]];
+        out++;
     }
 
     for(y = 1; y < h; y++) {
@@ -324,10 +349,13 @@ static void decompress(const uint8_t * in, uint8_t * out, lv_coord_t w, lv_coord
 
         for(x = 0; x < w; x++) {
             line_buf1[x] = line_buf2[x] ^ line_buf1[x];
-            bits_write(out,wrp, line_buf1[x], bpp);
-            wrp += wr_size;
+            *out = bpp_opa_table[line_buf1[x]];
+            out++;
         }
     }
+
+    lv_mem_buf_release(line_buf1);
+    lv_mem_buf_release(line_buf2);
 }
 
 /**
@@ -335,7 +363,7 @@ static void decompress(const uint8_t * in, uint8_t * out, lv_coord_t w, lv_coord
  * @param out output buffer
  * @param w width of the line in pixel count
  */
-static void decompress_line(uint8_t * out, lv_coord_t w)
+static inline void decompress_line(uint8_t * out, lv_coord_t w)
 {
     lv_coord_t i;
     for(i = 0; i < w; i++) {
@@ -350,52 +378,42 @@ static void decompress_line(uint8_t * out, lv_coord_t w)
  * @param len number of bits to read (must be <= 8).
  * @return the read bits
  */
-static uint8_t get_bits(const uint8_t * in, uint32_t bit_pos, uint8_t len)
+static inline uint8_t get_bits(const uint8_t * in, uint32_t bit_pos, uint8_t len)
 {
-    uint8_t res = 0;
-    uint32_t byte_pos = bit_pos >> 3;
-    bit_pos = bit_pos & 0x7;
-    uint8_t bit_mask = (uint16_t)((uint16_t) 1 << len) - 1;
-    uint16_t in16 = (in[byte_pos] << 8) + in[byte_pos + 1];
-
-    res = (in16 >> (16 - bit_pos - len)) & bit_mask;
-    return res;
-}
-
-/**
- * Write `val` data to `bit_pos` position of `out`. The write can NOT cross byte boundary.
- * @param out buffer where to write
- * @param bit_pos bit index to write
- * @param val value to write
- * @param len length of bits to write from `val`. (Counted from the LSB).
- * @note `len == 3` will be converted to `len = 4` and `val` will be upscaled too
- */
-static void bits_write(uint8_t * out, uint32_t bit_pos, uint8_t val, uint8_t len)
-{
-    if(len == 3) {
-        len = 4;
-        switch(val) {
-        case 0: val = 0; break;
-        case 1: val = 2; break;
-        case 2: val = 4; break;
-        case 3: val = 6; break;
-        case 4: val = 9; break;
-        case 5: val = 11; break;
-        case 6: val = 13; break;
-        case 7: val = 15; break;
-        }
+    uint8_t bit_mask;
+    switch(len) {
+        case 1:
+            bit_mask = 0x1;
+            break;
+        case 2:
+            bit_mask = 0x3;
+            break;
+        case 3:
+            bit_mask = 0x7;
+            break;
+        case 4:
+            bit_mask = 0xF;
+            break;
+        case 8:
+            bit_mask = 0xFF;
+            break;
+        default:
+            bit_mask = (uint16_t)((uint16_t) 1 << len) - 1;
     }
 
-    uint16_t byte_pos = bit_pos >> 3;
+    uint32_t byte_pos = bit_pos >> 3;
     bit_pos = bit_pos & 0x7;
-    bit_pos = 8 - bit_pos - len;
 
-    uint8_t bit_mask = (uint16_t)((uint16_t) 1 << len) - 1;
-    out[byte_pos] &= ((~bit_mask) << bit_pos);
-    out[byte_pos] |= (val << bit_pos);
+    if(bit_pos + len >= 8) {
+        uint16_t in16 = (in[byte_pos] << 8) + in[byte_pos + 1];
+        return (in16 >> (16 - bit_pos - len)) & bit_mask;
+    }
+    else {
+        return (in[byte_pos] >> (8 - bit_pos - len)) & bit_mask;
+    }
 }
 
-static void rle_init(const uint8_t * in,  uint8_t bpp)
+static inline void rle_init(const uint8_t * in,  uint8_t bpp)
 {
     rle_in = in;
     rle_bpp = bpp;
@@ -405,7 +423,7 @@ static void rle_init(const uint8_t * in,  uint8_t bpp)
     rle_cnt = 0;
 }
 
-static uint8_t rle_next(void)
+static inline uint8_t rle_next(void)
 {
     uint8_t v = 0;
     uint8_t ret = 0;
@@ -431,14 +449,16 @@ static uint8_t rle_next(void)
                 rle_rdp += 6;
                 if(rle_cnt != 0) {
                     rle_state = RLE_STATE_COUNTER;
-                } else {
+                }
+                else {
                     ret = get_bits(rle_in, rle_rdp, rle_bpp);
                     rle_prev_v = ret;
                     rle_rdp += rle_bpp;
                     rle_state = RLE_STATE_SINGLE;
                 }
             }
-        } else {
+        }
+        else {
             ret = get_bits(rle_in, rle_rdp, rle_bpp);
             rle_prev_v = ret;
             rle_rdp += rle_bpp;
