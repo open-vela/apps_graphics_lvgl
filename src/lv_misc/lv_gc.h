f400d5bc3e45fff4187b976fecefbb3896840f29
liujinye <liujinye@xiaomi.com>
2020-05-06 09:40:26 +0800

    chart: fix overflow in tick drawing

--- src/lv_misc/lv_gc.h
+++ src/lv_misc/lv_gc.h
@@ -13,13 +13,18 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
+
+#ifdef LV_CONF_INCLUDE_SIMPLE
+#include "lv_conf.h"
+#else
+#include "../../../lv_conf.h"
+#endif
+
 #include <stdint.h>
 #include <stdbool.h>
 #include "lv_mem.h"
 #include "lv_ll.h"
-#include "lv_task.h"
 #include "../lv_draw/lv_img_cache.h"
-#include "../lv_draw/lv_draw.h"
 
 /*********************
  *      DEFINES
@@ -34,10 +39,9 @@ extern "C" {
     f(lv_ll_t, _lv_anim_ll)                                        \
     f(lv_ll_t, _lv_group_ll)                                       \
     f(lv_ll_t, _lv_img_defoder_ll)                                 \
-    f(lv_ll_t, _lv_obj_style_trans_ll)                             \
     f(lv_img_cache_entry_t*, _lv_img_cache_array)                  \
-    f(lv_task_t*, _lv_task_act)                                         \
-    f(lv_mem_buf_arr_t , _lv_mem_buf)                              \
+    f(void*, _lv_task_act)                                         \
+    f(void*, _lv_draw_buf)
 
 #define LV_DEFINE_ROOT(root_type, root_name) root_type root_name;
 #define LV_ROOTS LV_ITERATE_ROOTS(LV_DEFINE_ROOT)
