36e3fbf530b89aca1fe4f5fc2e31236580c79663
liujinye <liujinye@xiaomi.com>
2020-05-18 10:48:21 +0800

    Merge branch 'dev-7.0' of https://github.com/littlevgl/lvgl into dev-7.0

--- src/lv_themes/lv_theme_material.c
+++ src/lv_themes/lv_theme_material.c
@@ -601,6 +601,10 @@ static void checkbox_init(void)
     lv_style_set_pattern_image(&cb_bullet, LV_STATE_CHECKED, LV_SYMBOL_OK);
     lv_style_set_pattern_recolor(&cb_bullet, LV_STATE_CHECKED, LV_COLOR_WHITE);
     lv_style_set_text_font(&cb_bullet, LV_STATE_CHECKED, theme.font_small);
+    lv_style_set_pad_left(&cb_bullet, LV_STATE_DEFAULT, LV_DPX(3));
+    lv_style_set_pad_right(&cb_bullet, LV_STATE_DEFAULT, LV_DPX(3));
+    lv_style_set_pad_top(&cb_bullet, LV_STATE_DEFAULT, LV_DPX(3));
+    lv_style_set_pad_bottom(&cb_bullet, LV_STATE_DEFAULT, LV_DPX(3));
 #endif
 }
 
@@ -737,6 +741,7 @@ static void ddlist_init(void)
 
     style_init_reset(&ddlist_page);
     lv_style_set_text_line_space(&ddlist_page, LV_STATE_DEFAULT, LV_DPX(20));
+    lv_style_set_clip_corner(&ddlist_page, LV_STATE_DEFAULT, true);
 
     style_init_reset(&ddlist_sel);
     lv_style_set_bg_opa(&ddlist_sel, LV_STATE_DEFAULT, LV_OPA_COVER);
