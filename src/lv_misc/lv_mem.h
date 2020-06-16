d67dd502bc3295b08b0b83d797d50b3f255d8fd9
openvela-robot <openvela-robot@xiaomi.com>
2020-06-16 16:24:37 +0800

    Finalise changes for new Chart Functions

--- src/lv_misc/lv_mem.h
+++ src/lv_misc/lv_mem.h
@@ -60,7 +60,7 @@ extern lv_mem_buf_arr_t _lv_mem_buf;
  **********************/
 
 /**
- * Initialize the dyn_mem module (work memory and other variables)
+ * Initiaize the dyn_mem module (work memory and other variables)
  */
 void _lv_mem_init(void);
 
