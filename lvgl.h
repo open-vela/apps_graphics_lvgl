7e3353c62a18c908df076a503080f10dfa43339b
openvela-robot <openvela-robot@xiaomi.com>
2020-06-20 00:00:31 +0800

    Add theme->apply_cb to replace theme->apply_xcb to make it compatible with the Micropython binding

--- lvgl.h
+++ lvgl.h
@@ -2,7 +2,7 @@
  * @file lvgl.h
  * Include all LittleV GL related headers
  */
-
+ 
 #ifndef LVGL_H
 #define LVGL_H
 
@@ -53,7 +53,6 @@ extern "C" {
 #include "src/lv_widgets/lv_keyboard.h"
 #include "src/lv_widgets/lv_dropdown.h"
 #include "src/lv_widgets/lv_roller.h"
-#include "src/lv_widgets/lv_rotary.h"
 #include "src/lv_widgets/lv_textarea.h"
 #include "src/lv_widgets/lv_canvas.h"
 #include "src/lv_widgets/lv_win.h"
