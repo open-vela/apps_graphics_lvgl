4bb95e7c60fa41e2e674e0f60ee3d75ee396d85e
liujinye <liujinye@xiaomi.com>
2020-07-05 12:25:44 +0800

    Add slew rate limited angle delta based on threshold of degress/sec

--- src/lv_widgets/lv_btn.c
+++ src/lv_widgets/lv_btn.c
@@ -289,7 +289,6 @@ static lv_res_t lv_btn_signal(lv_obj_t * btn, lv_signal_t sign, void * param)
         }
     }
     else if(sign == LV_SIGNAL_CONTROL) {
-#if LV_USE_GROUP
         char c = *((char *)param);
         if(c == LV_KEY_RIGHT || c == LV_KEY_UP) {
             if(lv_btn_get_checkable(btn)) {
@@ -310,7 +309,6 @@ static lv_res_t lv_btn_signal(lv_obj_t * btn, lv_signal_t sign, void * param)
                 if(res != LV_RES_OK) return res;
             }
         }
-#endif
     }
 
     return res;
