e54ac685cc54a34558b27b6282c300ed06dde59b
liujinye <liujinye@xiaomi.com>
2020-07-16 13:06:32 +0800

    scroll: set default style values

--- src/lv_core/lv_obj.h
+++ src/lv_core/lv_obj.h
@@ -139,6 +139,9 @@ enum {
     LV_SIGNAL_RELEASED,          /**< User pressed object for a short period of time, then released it. Not called if dragged. */
     LV_SIGNAL_LONG_PRESS,        /**< Object has been pressed for at least `LV_INDEV_LONG_PRESS_TIME`.  Not called if dragged.*/
     LV_SIGNAL_LONG_PRESS_REP,    /**< Called after `LV_INDEV_LONG_PRESS_TIME` in every `LV_INDEV_LONG_PRESS_REP_TIME` ms.  Not called if dragged.*/
+    LV_SIGNAL_SCROLL_BEGIN,      /**< The scrolling has just begun  */
+    LV_SIGNAL_SCROLL,            /**< The object has been scrolled */
+    LV_SIGNAL_SCROLL_END,        /**< The scrolling has ended */
     LV_SIGNAL_DRAG_BEGIN,
     LV_SIGNAL_DRAG_THROW_BEGIN,
     LV_SIGNAL_DRAG_END,
@@ -198,6 +201,7 @@ typedef struct _lv_obj_t {
     lv_ll_t child_ll;       /**< Linked list to store the children objects*/
 
     lv_area_t coords; /**< Coordinates of the object (x1, y1, x2, y2)*/
+    lv_point_t scroll; /**< The current X/Y scroll offset*/
 
     lv_event_cb_t event_cb; /**< Event callback function */
     lv_signal_cb_t signal_cb; /**< Object type specific signal function*/
@@ -226,8 +230,10 @@ typedef struct _lv_obj_t {
     uint8_t adv_hittest     : 1; /**< 1: Use advanced hit-testing (slower) */
     uint8_t gesture_parent  : 1; /**< 1: Parent will be gesture instead*/
     uint8_t focus_parent    : 1; /**< 1: Parent will be focused instead*/
+    lv_drag_dir_t scroll_elastic  : 3; /**< In which directions the object can be scrolled */
 
-    lv_drag_dir_t drag_dir  : 3; /**<  Which directions the object can be dragged in */
+    lv_drag_dir_t drag_dir    : 3; /**< In which directions the object can be dragged */
+    lv_drag_dir_t scroll_dir  : 3; /**< In which directions the object can be scrolled */
     lv_bidi_dir_t base_dir  : 2; /**< Base direction of texts related to this object */
 
 #if LV_USE_GROUP != 0
@@ -528,6 +534,84 @@ void lv_obj_realign(lv_obj_t * obj);
  * @param en true: enable auto realign; false: disable auto realign
  */
 void lv_obj_set_auto_realign(lv_obj_t * obj, bool en);
+
+/**
+ * Moves all children with horizontally or vertically.
+ * It doesn't take into account any limits so any values are possible
+ * @param obj pointer to an object whose children should be moved
+ * @param x pixel to move horizontally
+ * @param y pixels to move vertically
+ */
+void lv_obj_scroll_by_raw(lv_obj_t * obj, lv_coord_t x, lv_coord_t y);
+
+/**
+ * Moves all children with horizontally or vertically.
+ * Limits the scroll to the bounding box of the children.
+ * @param obj pointer to an object whose children should be moved
+ * @param x pixel to move horizontally
+ * @param y pixels to move vertically
+ */
+void lv_obj_scroll_by(lv_obj_t * obj, lv_coord_t x, lv_coord_t y, lv_anim_enable_t anim_en);
+
+/**
+ * Scroll the a given x coordinate to the left side of obj.
+ * @param obj pointer to an object which should be scrolled
+ * @param x the x coordinate to scroll to
+ * @param y the y coordinate to scroll to
+ */
+void lv_obj_scroll_to(lv_obj_t * obj, lv_coord_t x, lv_coord_t y, lv_anim_enable_t anim_en);
+
+/**
+ * Scroll the a given x coordinate to the left side of obj.
+ * @param obj pointer to an object which should be scrolled
+ * @param x the x coordinate to scroll to
+ */
+void lv_obj_scroll_to_x(lv_obj_t * obj, lv_coord_t x, lv_anim_enable_t anim_en);
+
+/**
+ * Scroll the a given y coordinate to the top side of obj.
+ * @param obj pointer to an object which should be scrolled
+ * @param y the y coordinate to scroll to
+ */
+void lv_obj_scroll_to_y(lv_obj_t * obj, lv_coord_t y, lv_anim_enable_t anim_en);
+
+
+/**
+ * Return the height of the area above the parent.
+ * That is the number of pixels the object can be scrolled down.
+ * Normally positive but can be negative when scrolled inside.
+ * @param obj
+ * @return
+ */
+lv_coord_t lv_obj_get_scroll_top(lv_obj_t * obj);
+
+/**
+ * Return the height of the area below the parent.
+ * That is the number of pixels the object can be scrolled up.
+ * Normally positive but can be negative when scrolled inside.
+ * @param obj
+ * @return
+ */
+lv_coord_t lv_obj_get_scroll_bottom(lv_obj_t * obj);
+
+
+/**
+ * Return the weight of the area on the left the parent.
+ * That is the number of pixels the object can be scrolled down.
+ * Normally positive but can be negative when scrolled inside.
+ * @param obj
+ * @return
+ */
+lv_coord_t lv_obj_get_scroll_left(lv_obj_t * obj);
+
+/**
+ * Return the width of the area below the object.
+ * That is the number of pixels the object can be scrolled left.
+ * Normally positive but can be negative when scrolled inside.
+ * @param obj
+ * @return
+ */
+lv_coord_t lv_obj_get_scroll_right(lv_obj_t * obj);
 
 /**
  * Set the size of an extended clickable area
@@ -836,6 +920,14 @@ lv_res_t lv_event_send_refresh(lv_obj_t * obj);
  * @param obj pointer to an object or NULL to refresh all objects of all displays
  */
 void lv_event_send_refresh_recursive(lv_obj_t * obj);
+
+/**
+ * Queue the sending of LV_EVENT_REFRESH event to an object and all of its children.
+ * The events won't be sent immediately but after `LV_DISP_DEF_REFR_PERIOD` delay.
+ * It is useful to refresh object only on a reasonable rate if this function is called very often.
+ * @param obj pointer to an object or NULL to refresh all objects of all displays
+ */
+void lv_event_queue_refresh_recursive(lv_obj_t * obj);
 
 /**
  * Call an event function with an object, event, and data.
@@ -1235,6 +1327,13 @@ bool lv_obj_get_drag(const lv_obj_t * obj);
  * @return bitwise OR of allowed directions an object can be dragged in
  */
 lv_drag_dir_t lv_obj_get_drag_dir(const lv_obj_t * obj);
+
+/**
+ * Get the directions an object can be scrolled
+ * @param obj pointer to an object
+ * @return bitwise OR of allowed directions an object can be dragged in
+ */
+lv_drag_dir_t lv_obj_get_scroll_dir(const lv_obj_t * obj);
 
 /**
  * Get the drag throw enable attribute of an object
