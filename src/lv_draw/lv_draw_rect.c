a814c024bd36512b836ba562ccdba3b4a3b5f49a
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 01:29:54 +0800

    Adding changes to changelog and replacing line style comments with block comments.

--- src/lv_draw/lv_draw_rect.c
+++ src/lv_draw/lv_draw_rect.c
@@ -30,10 +30,8 @@
 LV_ATTRIBUTE_FAST_MEM static void draw_bg(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
 LV_ATTRIBUTE_FAST_MEM static void draw_border(const lv_area_t * coords, const lv_area_t * clip,
                                               lv_draw_rect_dsc_t * dsc);
-
-#if LV_USE_OUTLINE
-    static void draw_outline(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
-#endif
+static void draw_outline(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
+LV_ATTRIBUTE_FAST_MEM static inline lv_color_t grad_get(lv_draw_rect_dsc_t * dsc, lv_coord_t s, lv_coord_t i);
 #if LV_USE_SHADOW
 LV_ATTRIBUTE_FAST_MEM static void draw_shadow(const lv_area_t * coords, const lv_area_t * clip,
                                               lv_draw_rect_dsc_t * dsc);
@@ -41,17 +39,9 @@ LV_ATTRIBUTE_FAST_MEM static void shadow_draw_corner_buf(const lv_area_t * coord
                                                          lv_coord_t r);
 LV_ATTRIBUTE_FAST_MEM static void shadow_blur_corner(lv_coord_t size, lv_coord_t sw, uint16_t * sh_ups_buf);
 #endif
-
-#if LV_USE_PATTERN
-    static void draw_pattern(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
-#endif
-
-#if LV_USE_VALUE_STR
-    static void draw_value_str(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
-#endif
-static void draw_full_border(const lv_area_t * area_inner, const lv_area_t * area_outer, const lv_area_t * clip,
-                             lv_coord_t radius, lv_color_t color, lv_opa_t opa, lv_blend_mode_t blend_mode);
-LV_ATTRIBUTE_FAST_MEM static inline lv_color_t grad_get(lv_draw_rect_dsc_t * dsc, lv_coord_t s, lv_coord_t i);
+static void draw_pattern(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
+static void draw_value(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc);
+static void draw_full_border(const lv_area_t * area_inner, const lv_area_t * area_outer, const lv_area_t * clip, lv_coord_t radius, lv_color_t color, lv_opa_t opa, lv_blend_mode_t blend_mode);
 
 /**********************
  *  STATIC VARIABLES
@@ -106,19 +96,10 @@ void lv_draw_rect(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect
 #endif
 
     draw_bg(coords, clip, dsc);
-
-#if LV_USE_PATTERN
     draw_pattern(coords, clip, dsc);
-#endif
     draw_border(coords, clip, dsc);
-
-#if LV_USE_VALUE_STR
-    draw_value_str(coords, clip, dsc);
-#endif
-
-#if LV_USE_OUTLINE
+    draw_value(coords, clip, dsc);
     draw_outline(coords, clip, dsc);
-#endif
 
     LV_ASSERT_MEM_INTEGRITY();
 }
@@ -486,8 +467,8 @@ LV_ATTRIBUTE_FAST_MEM static void draw_border(const lv_area_t * coords, const lv
 
         for(h = draw_area.y1; h <= draw_area.y2; h++) {
             if(normal ||
-               (top_only && fill_area.y1 <= coords->y1 + corner_size) ||
-               (bottom_only && fill_area.y1 >= coords->y2 - corner_size)) {
+                    (top_only && fill_area.y1 <= coords->y1 + corner_size) ||
+                    (bottom_only && fill_area.y1 >= coords->y2 - corner_size)) {
                 _lv_memset_ff(mask_buf, draw_area_w);
                 mask_res = lv_draw_mask_apply(mask_buf, vdb->area.x1 + draw_area.x1, vdb->area.y1 + h, draw_area_w);
                 _lv_blend_fill(clip, &fill_area, color, mask_buf + buf_ofs, mask_res, opa, blend_mode);
@@ -1147,7 +1128,6 @@ LV_ATTRIBUTE_FAST_MEM static void shadow_blur_corner(lv_coord_t size, lv_coord_t
 
 #endif
 
-#if LV_USE_OUTLINE
 static void draw_outline(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc)
 {
     if(dsc->outline_opa <= LV_OPA_MIN) return;
@@ -1173,13 +1153,9 @@ static void draw_outline(const lv_area_t * coords, const lv_area_t * clip, lv_dr
     area_outer.y1 -= dsc->outline_width;
     area_outer.y2 += dsc->outline_width;
 
-    draw_full_border(&area_inner, &area_outer, clip, dsc->radius, dsc->outline_color, dsc->outline_opa,
-                     dsc->outline_blend_mode);
+    draw_full_border(&area_inner, &area_outer, clip, dsc->radius, dsc->outline_color, dsc->outline_opa, dsc->outline_blend_mode);
 }
-#endif
 
-
-#if LV_USE_PATTERN
 static void draw_pattern(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc)
 {
     if(dsc->pattern_image == NULL) return;
@@ -1280,11 +1256,9 @@ static void draw_pattern(const lv_area_t * coords, const lv_area_t * clip, lv_dr
         lv_draw_mask_remove_id(radius_mask_id);
     }
 }
-#endif
 
 
-#if LV_USE_VALUE_STR
-static void draw_value_str(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc)
+static void draw_value(const lv_area_t * coords, const lv_area_t * clip, lv_draw_rect_dsc_t * dsc)
 {
     if(dsc->value_str == NULL) return;
     if(dsc->value_opa <= LV_OPA_MIN) return;
@@ -1317,10 +1291,8 @@ static void draw_value_str(const lv_area_t * coords, const lv_area_t * clip, lv_
 
     lv_draw_label(&value_area, clip, &label_dsc, dsc->value_str, NULL);
 }
-#endif
 
-static void draw_full_border(const lv_area_t * area_inner, const lv_area_t * area_outer, const lv_area_t * clip,
-                             lv_coord_t radius, lv_color_t color, lv_opa_t opa, lv_blend_mode_t blend_mode)
+static void draw_full_border(const lv_area_t * area_inner, const lv_area_t * area_outer, const lv_area_t * clip, lv_coord_t radius, lv_color_t color, lv_opa_t opa, lv_blend_mode_t blend_mode)
 {
     uint8_t other_mask_cnt = lv_draw_mask_get_cnt();
     bool simple_mode = true;
