170388b1961c3865c3271a88e080662be6e3d14e
openvela-robot <openvela-robot@xiaomi.com>
2020-12-15 20:13:53 +0800

    Update dev version

--- src/lv_widgets/lv_chart.h
+++ src/lv_widgets/lv_chart.h
@@ -74,6 +74,7 @@ typedef struct {
     lv_color_t color;
     uint16_t start_point;
     uint8_t ext_buf_assigned : 1;
+    uint8_t hidden : 1;
     lv_chart_axis_t y_axis  : 1;
 } lv_chart_series_t;
 
@@ -123,7 +124,7 @@ typedef struct {
 /*Parts of the chart*/
 enum {
     LV_CHART_PART_BG = LV_OBJ_PART_MAIN,
-    LV_CHART_PART_SERIES_BG,
+    LV_CHART_PART_SERIES_BG = _LV_OBJ_PART_VIRTUAL_LAST,
     LV_CHART_PART_SERIES,
     LV_CHART_PART_CURSOR
 };
@@ -154,6 +155,13 @@ lv_obj_t * lv_chart_create(lv_obj_t * par, const lv_obj_t * copy);
 lv_chart_series_t * lv_chart_add_series(lv_obj_t * chart, lv_color_t color);
 
 /**
+ * Deallocate and remove a data series from a chart
+ * @param chart pointer to a chart object
+ * @param series pointer to a data series on 'chart'
+ */
+void lv_chart_remove_series(lv_obj_t * chart, lv_chart_series_t * series);
+
+/**
  * Add a cursor with a given color
  * @param chart pointer to chart object
  * @param color color of the cursor
@@ -168,6 +176,15 @@ lv_chart_cursor_t * lv_chart_add_cursor(lv_obj_t * chart, lv_color_t color, lv_c
  * @param series pointer to the chart's series to clear
  */
 void lv_chart_clear_series(lv_obj_t * chart, lv_chart_series_t * series);
+
+/**
+ * Hide/Unhide a single series of a chart.
+ * @param chart pointer to a chart object.
+ * @param series pointer to a series object
+ * @param hide true: hide the series
+ */
+void lv_chart_hide_series(lv_obj_t * chart, lv_chart_series_t * series, bool hide);
+
 
 /*=====================
  * Setter functions
