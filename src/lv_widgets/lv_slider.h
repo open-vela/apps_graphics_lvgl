bbcd8fb9af18018fc1f386fd3903d2b3f9140e3c
liujinye <liujinye@xiaomi.com>
2020-12-15 19:59:27 +0800

    Release v7.8.1

--- src/lv_widgets/lv_slider.h
+++ src/lv_widgets/lv_slider.h
@@ -40,30 +40,23 @@ enum {
 };
 typedef uint8_t lv_slider_type_t;
 
-LV_CLASS_DECLARE_START(lv_slider, lv_bar)
-
-#define _lv_slider_constructor   void (*constructor)(struct _lv_obj_t * obj, struct _lv_obj_t * parent, const struct _lv_obj_t * copy)
-
-#define _lv_slider_data                                       \
-  _lv_bar_data                                                \
-  lv_style_list_t style_knob; /*Style of the knob*/           \
-  lv_area_t left_knob_area;                                   \
-  lv_area_t right_knob_area;                                  \
-  int16_t * value_to_set; /* Which bar value to set */        \
-  uint8_t dragging : 1;       /*1: the slider is being dragged*/ \
-  uint8_t left_knob_focus : 1; /*1: with encoder now the right knob can be adjusted*/
-
-#define _lv_slider_class_dsc        \
-  _lv_bar_class_dsc               \
-
-LV_CLASS_DECLARE_END(lv_slider, lv_bar)
+/*Data of slider*/
+typedef struct {
+    lv_bar_ext_t bar; /*Ext. of ancestor*/
+    /*New data for this type */
+    lv_style_list_t style_knob; /*Style of the knob*/
+    lv_area_t left_knob_area;
+    lv_area_t right_knob_area;
+    int16_t * value_to_set; /* Which bar value to set */
+    uint8_t dragging : 1;       /*1: the slider is being dragged*/
+    uint8_t left_knob_focus : 1; /*1: with encoder now the right knob can be adjusted*/
+} lv_slider_ext_t;
 
 /** Built-in styles of slider*/
 enum {
-    LV_SLIDER_PART_MAIN = LV_BAR_PART_MAIN, /** Slider background style. */
-    LV_SLIDER_PART_INDIC = LV_BAR_PART_INDIC, /** Slider indicator (filled area) style. */
-    LV_SLIDER_PART_KNOB = _LV_BAR_PART_VIRTUAL_LAST, /** Slider knob style. */
-    _LV_SLIDER_PART_VIRTUAL_LAST
+    LV_SLIDER_PART_BG, /** Slider background style. */
+    LV_SLIDER_PART_INDIC, /** Slider indicator (filled area) style. */
+    LV_SLIDER_PART_KNOB, /** Slider knob style. */
 };
 
 /**********************
@@ -72,12 +65,11 @@ enum {
 
 /**
  * Create a slider objects
- * @param parent pointer to an object, it will be the parent of the new slider
- * @param copy DEPRECATED, will be removed in v9.
- *             Pointer to an other slider to copy.
+ * @param par pointer to an object, it will be the parent of the new slider
+ * @param copy pointer to a slider object, if not NULL then the new object will be copied from it
  * @return pointer to the created slider
  */
-lv_obj_t * lv_slider_create(lv_obj_t * parent, const lv_obj_t * copy);
+lv_obj_t * lv_slider_create(lv_obj_t * par, const lv_obj_t * copy);
 
 /*=====================
  * Setter functions
@@ -151,10 +143,7 @@ static inline void lv_slider_set_type(lv_obj_t * slider, lv_slider_type_t type)
  * @param slider pointer to a slider object
  * @return the value of the main knob of the slider
  */
-static inline int16_t lv_slider_get_value(const lv_obj_t * slider)
-{
-    return lv_bar_get_value(slider);
-}
+int16_t lv_slider_get_value(const lv_obj_t * slider);
 
 /**
  * Get the value of the left knob of a slider
