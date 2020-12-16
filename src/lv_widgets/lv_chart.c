e869476e6b2bda9d34b9dd1d12009a51a2bc97da
openvela-robot <openvela-robot@xiaomi.com>
2020-12-16 10:22:13 +0800

    experiements with improving styles

--- src/lv_widgets/lv_chart.c
+++ src/lv_widgets/lv_chart.c
@@ -155,7 +155,7 @@ lv_obj_t * lv_chart_create(lv_obj_t * par, const lv_obj_t * copy)
         _lv_memcpy(&ext->secondary_y_axis, &ext_copy->secondary_y_axis, sizeof(lv_chart_axis_cfg_t));
 
         /*Refresh the style with new signal function*/
-        lv_obj_refresh_style(chart, LV_OBJ_PART_ALL, LV_STYLE_PROP_ALL);
+        _lv_obj_refresh_style(chart, LV_OBJ_PART_ALL, LV_STYLE_PROP_ALL);
     }
 
     LV_LOG_INFO("chart created");
@@ -195,7 +195,6 @@ lv_chart_series_t * lv_chart_add_series(lv_obj_t * chart, lv_color_t color)
 
     ser->start_point = 0;
     ser->ext_buf_assigned = false;
-    ser->hidden = 0;
     ser->y_axis = LV_CHART_AXIS_PRIMARY_Y;
 
     uint16_t i;
@@ -208,33 +207,6 @@ lv_chart_series_t * lv_chart_add_series(lv_obj_t * chart, lv_color_t color)
     return ser;
 }
 
-/**
- * Deallocate and remove a data series from a chart
- * @param chart pointer to a chart object
- * @param series pointer to a data series on 'chart'
- */
-void lv_chart_remove_series(lv_obj_t * chart, lv_chart_series_t * series)
-{
-    LV_ASSERT_OBJ(chart, LV_OBJX_NAME);
-    LV_ASSERT_NULL(series);
-    
-    if(chart == NULL || series == NULL) return;
-    lv_chart_ext_t * ext = lv_obj_get_ext_attr(chart);
-    if(!series->ext_buf_assigned && series->points) lv_mem_free(series->points);
-
-    _lv_ll_remove(&ext->series_ll, series);
-    lv_mem_free(series);
-
-    return;
-}
-
-/**
- * Add a cursor with a given color
- * @param chart pointer to chart object
- * @param color color of the cursor
- * @param dir direction of the cursor. `LV_CHART_CURSOR_RIGHT/LEFT/TOP/DOWN`. OR-ed values are possible
- * @return pointer to the created cursor
- */
 lv_chart_cursor_t  * lv_chart_add_cursor(lv_obj_t * chart, lv_color_t color, lv_cursor_direction_t axes)
 {
     LV_ASSERT_OBJ(chart, LV_OBJX_NAME);
@@ -273,22 +245,6 @@ void lv_chart_clear_series(lv_obj_t * chart, lv_chart_series_t * series)
 
     series->start_point = 0;
 }
-
-/**
- * Hide/Unhide a single series of a chart.
- * @param chart pointer to a chart object.
- * @param series pointer to a series object
- * @param hide true: hide the series
- */
-void lv_chart_hide_series(lv_obj_t * chart, lv_chart_series_t * series, bool hide)
-{
-    LV_ASSERT_OBJ(chart, LV_OBJX_NAME);
-    LV_ASSERT_NULL(series);
-
-    series->hidden = hide ? 1 : 0;
-    lv_chart_refresh(chart);
-}
-
 
 /*=====================
  * Setter functions
@@ -374,7 +330,7 @@ void lv_chart_set_point_count(lv_obj_t * chart, uint16_t point_cnt)
 
     if(point_cnt < 1) point_cnt = 1;
 
-    _LV_LL_READ_BACK(ext->series_ll, ser) {
+    _LV_LL_READ_BACK(&ext->series_ll, ser) {
         if(!ser->ext_buf_assigned) {
             if(ser->start_point != 0) {
                 lv_coord_t * new_points = lv_mem_alloc(sizeof(lv_coord_t) * point_cnt);
@@ -710,7 +666,6 @@ void lv_chart_set_cursor_point(lv_obj_t * chart, lv_chart_cursor_t * cursor, lv_
     lv_chart_refresh(chart);
 }
 
-
 /*=====================
  * Getter functions
  *====================*/
@@ -855,12 +810,12 @@ lv_coord_t lv_chart_get_x_from_index(lv_obj_t * chart, lv_chart_series_t * ser, 
     else if(ext->type & LV_CHART_TYPE_COLUMN) {
         lv_coord_t col_w = w / ((_lv_ll_get_len(&ext->series_ll) + 1) * ext->point_cnt); /* Suppose + 1 series as separator*/
         lv_chart_series_t * itr_ser = NULL;
-        lv_style_int_t col_space = lv_obj_get_style_pad_inner(chart, LV_CHART_PART_SERIES);
+        lv_style_int_t col_space = lv_obj_get_style_pad_left(chart, LV_CHART_PART_SERIES);
 
         x = (int32_t)((int32_t)w * id) / ext->point_cnt;
         x += col_w / 2; /*Start offset*/
 
-        _LV_LL_READ_BACK(ext->series_ll, itr_ser) {
+        _LV_LL_READ_BACK(&ext->series_ll, itr_ser) {
             if(itr_ser == ser) break;
             x += col_w;
         }
@@ -992,7 +947,7 @@ static lv_res_t lv_chart_signal(lv_obj_t * chart, lv_signal_t sign, void * param
 
     res = ancestor_signal(chart, sign, param);
     if(res != LV_RES_OK) return res;
-    if(sign == LV_SIGNAL_GET_TYPE) return lv_obj_handle_get_type_signal(param, LV_OBJX_NAME);
+    if(sign == LV_SIGNAL_GET_TYPE) return _lv_obj_handle_get_type_signal(param, LV_OBJX_NAME);
 
     lv_chart_ext_t * ext = lv_obj_get_ext_attr(chart);
 
@@ -1008,9 +963,9 @@ static lv_res_t lv_chart_signal(lv_obj_t * chart, lv_signal_t sign, void * param
         }
         _lv_ll_clear(&ext->series_ll);
 
-        lv_obj_clean_style_list(chart, LV_CHART_PART_SERIES);
-        lv_obj_clean_style_list(chart, LV_CHART_PART_CURSOR);
-        lv_obj_clean_style_list(chart, LV_CHART_PART_SERIES_BG);
+        _lv_obj_reset_style_list_no_refr(chart, LV_CHART_PART_SERIES);
+        _lv_obj_reset_style_list_no_refr(chart, LV_CHART_PART_CURSOR);
+        _lv_obj_reset_style_list_no_refr(chart, LV_CHART_PART_SERIES_BG);
     }
 
     return res;
@@ -1179,8 +1134,7 @@ static void draw_series_line(lv_obj_t * chart, const lv_area_t * series_area, co
     if(point_radius > line_dsc.width / 2) line_dsc.raw_end = 1;
 
     /*Go through all data lines*/
-    _LV_LL_READ_BACK(ext->series_ll, ser) {
-    	if (ser->hidden) continue;
+    _LV_LL_READ_BACK(&ext->series_ll, ser) {
         line_dsc.color = ser->color;
         point_dsc.bg_color = ser->color;
         area_dsc.bg_color = ser->color;
@@ -1295,7 +1249,7 @@ static void draw_series_column(lv_obj_t * chart, const lv_area_t * series_area, 
     lv_chart_series_t * ser;
     lv_coord_t col_w = w / ((_lv_ll_get_len(&ext->series_ll) + 1) * ext->point_cnt); /* Suppose + 1 series as separator*/
     lv_coord_t x_ofs = col_w / 2;                                    /*Shift with a half col.*/
-    lv_style_int_t col_space = lv_obj_get_style_pad_inner(chart, LV_CHART_PART_SERIES);
+    lv_style_int_t col_space = lv_obj_get_style_pad_left(chart, LV_CHART_PART_SERIES);
 
     lv_draw_rect_dsc_t col_dsc;
     lv_draw_rect_dsc_init(&col_dsc);
@@ -1316,8 +1270,7 @@ static void draw_series_column(lv_obj_t * chart, const lv_area_t * series_area, 
         x_act += series_area->x1 + x_ofs;
 
         /*Draw the current point of all data line*/
-        _LV_LL_READ_BACK(ext->series_ll, ser) {
-        	if (ser->hidden) continue;
+        _LV_LL_READ_BACK(&ext->series_ll, ser) {
             lv_coord_t start_point = ext->update_mode == LV_CHART_UPDATE_MODE_SHIFT ? ser->start_point : 0;
 
             col_a.x1 = x_act;
@@ -1374,7 +1327,7 @@ static void draw_cursors(lv_obj_t * chart, const lv_area_t * series_area, const 
     if(point_radius > line_dsc.width / 2) line_dsc.raw_end = 1;
 
     /*Go through all cursor lines*/
-    _LV_LL_READ_BACK(ext->cursors_ll, cursor) {
+    _LV_LL_READ_BACK(&ext->cursors_ll, cursor) {
         line_dsc.color = cursor->color;
         point_dsc.bg_color = cursor->color;
 
