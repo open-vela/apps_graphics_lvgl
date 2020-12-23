8af600773cf52cecffe8a83636a586c305f3fac8
liujinye <liujinye@xiaomi.com>
2020-12-24 00:24:49 +0800

    test style caching

--- src/lv_widgets/lv_slider.h
+++ src/lv_widgets/lv_slider.h
@@ -40,24 +40,24 @@ enum {
 };
 typedef uint8_t lv_slider_type_t;
 
-/*Data of slider*/
-typedef struct {
-    lv_bar_ext_t bar; /*Ext. of ancestor*/
-    /*New data for this type */
-    lv_style_list_t style_knob; /*Style of the knob*/
-    lv_area_t left_knob_area;
-    lv_area_t right_knob_area;
-    int16_t * value_to_set; /* Which bar value to set */
-    uint8_t dragging : 1;       /*1: the slider is being dragged*/
-    uint8_t left_knob_focus : 1; /*1: with encoder now the right knob can be adjusted*/
-} lv_slider_ext_t;
+LV_CLASS_DECLARE_START(lv_slider, lv_bar)
 
-/** Built-in styles of slider*/
-enum {
-    LV_SLIDER_PART_BG, /** Slider background style. */
-    LV_SLIDER_PART_INDIC, /** Slider indicator (filled area) style. */
-    LV_SLIDER_PART_KNOB, /** Slider knob style. */
-};
+#define _lv_slider_constructor   void (*constructor)(struct _lv_obj_t * obj, struct _lv_obj_t * parent, const struct _lv_obj_t * copy)
+
+#define _lv_slider_data                                       \
+  _lv_bar_data                                                \
+  lv_area_t left_knob_area;                                   \
+  lv_area_t right_knob_area;                                  \
+  int16_t * value_to_set; /* Which bar value to set */        \
+  uint8_t dragging : 1;       /*1: the slider is being dragged*/ \
+  uint8_t left_knob_focus : 1; /*1: with encoder now the right knob can be adjusted*/
+
+#define _lv_slider_class_dsc        \
+  _lv_bar_class_dsc               \
+
+LV_CLASS_DECLARE_END(lv_slider, lv_bar)
+
+extern lv_slider_class_t lv_slider;
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -65,11 +65,12 @@ enum {
 
 /**
  * Create a slider objects
- * @param par pointer to an object, it will be the parent of the new slider
- * @param copy pointer to a slider object, if not NULL then the new object will be copied from it
+ * @param parent pointer to an object, it will be the parent of the new slider
+ * @param copy DEPRECATED, will be removed in v9.
+ *             Pointer to an other slider to copy.
  * @return pointer to the created slider
  */
-lv_obj_t * lv_slider_create(lv_obj_t * par, const lv_obj_t * copy);
+lv_obj_t * lv_slider_create(lv_obj_t * parent, const lv_obj_t * copy);
 
 /*=====================
  * Setter functions
@@ -143,7 +144,10 @@ static inline void lv_slider_set_type(lv_obj_t * slider, lv_slider_type_t type)
  * @param slider pointer to a slider object
  * @return the value of the main knob of the slider
  */
-int16_t lv_slider_get_value(const lv_obj_t * slider);
+static inline int16_t lv_slider_get_value(const lv_obj_t * slider)
+{
+    return lv_bar_get_value(slider);
+}
 
 /**
  * Get the value of the left knob of a slider
