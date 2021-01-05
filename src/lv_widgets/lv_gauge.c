72d0ce2b7d9decc3415b35cf6479b82fcfec4747
openvela-robot <openvela-robot@xiaomi.com>
2021-01-05 13:01:07 +0800

    add table and btnmatrix

--- src/lv_widgets/lv_gauge.c
+++ src/lv_widgets/lv_gauge.c
@@ -124,7 +124,7 @@ lv_obj_t * lv_gauge_create(lv_obj_t * par, const lv_obj_t * copy)
         ext->format_cb   = copy_ext->format_cb;
 
         /*Refresh the style with new signal function*/
-        lv_obj_refresh_style(gauge, LV_OBJ_PART_ALL, LV_STYLE_PROP_ALL);
+        _lv_obj_refresh_style(gauge, LV_OBJ_PART_ALL, LV_STYLE_PROP_ALL);
     }
 
     LV_LOG_INFO("gauge created");
@@ -197,9 +197,15 @@ void lv_gauge_set_value(lv_obj_t * gauge, uint8_t needle_id, int32_t value)
     int32_t old_value = ext->values[needle_id];
     ext->values[needle_id] = value;
 
-    lv_coord_t r      = lv_obj_get_width(gauge) / 2;
-    lv_coord_t x_ofs  = gauge->coords.x1 + r;
-    lv_coord_t y_ofs  = gauge->coords.y1 + r;
+    //    lv_obj_invalidate(gauge);
+
+    lv_coord_t pad = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_NEEDLE);
+    lv_coord_t left = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t right = lv_obj_get_style_pad_right(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t top = lv_obj_get_style_pad_top(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t r      = (lv_obj_get_width(gauge) - left - right) / 2 - pad;
+    lv_coord_t x_ofs  = gauge->coords.x1 + r + left + pad;
+    lv_coord_t y_ofs  = gauge->coords.y1 + r + top + pad;
     uint16_t angle    = lv_linemeter_get_scale_angle(gauge);
     int16_t angle_ofs = 90 + (360 - angle) / 2 + lv_gauge_get_angle_offset(gauge);
     lv_point_t p_mid;
@@ -454,14 +460,14 @@ static lv_res_t lv_gauge_signal(lv_obj_t * gauge, lv_signal_t sign, void * param
     /* Include the ancient signal function */
     res = ancestor_signal(gauge, sign, param);
     if(res != LV_RES_OK) return res;
-    if(sign == LV_SIGNAL_GET_TYPE) return lv_obj_handle_get_type_signal(param, LV_OBJX_NAME);
+    if(sign == LV_SIGNAL_GET_TYPE) return _lv_obj_handle_get_type_signal(param, LV_OBJX_NAME);
 
     lv_gauge_ext_t * ext = lv_obj_get_ext_attr(gauge);
     if(sign == LV_SIGNAL_CLEANUP) {
         lv_mem_free(ext->values);
         ext->values = NULL;
-        lv_obj_clean_style_list(gauge, LV_GAUGE_PART_NEEDLE);
-        lv_obj_clean_style_list(gauge, LV_GAUGE_PART_MAJOR);
+        _lv_obj_reset_style_list_no_refr(gauge, LV_GAUGE_PART_NEEDLE);
+        _lv_obj_reset_style_list_no_refr(gauge, LV_GAUGE_PART_MAJOR);
     }
 
     return res;
@@ -503,11 +509,11 @@ static lv_style_list_t * lv_gauge_get_style(lv_obj_t * gauge, uint8_t part)
 static void lv_gauge_draw_labels(lv_obj_t * gauge, const lv_area_t * mask)
 {
     lv_gauge_ext_t * ext     = lv_obj_get_ext_attr(gauge);
-    lv_style_int_t scale_width = lv_obj_get_style_scale_width(gauge, LV_GAUGE_PART_MAJOR);
-    lv_style_int_t left = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_MAIN);
-    lv_style_int_t right = lv_obj_get_style_pad_right(gauge, LV_GAUGE_PART_MAIN);
-    lv_style_int_t top = lv_obj_get_style_pad_top(gauge, LV_GAUGE_PART_MAIN);
-    lv_style_int_t txt_pad = lv_obj_get_style_pad_inner(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t scale_width = lv_obj_get_style_scale_width(gauge, LV_GAUGE_PART_MAJOR);
+    lv_coord_t left = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t right = lv_obj_get_style_pad_right(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t top = lv_obj_get_style_pad_top(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t txt_pad = left;
     lv_coord_t r             = (lv_obj_get_width(gauge) - left - right) / 2 - scale_width - txt_pad;
     lv_coord_t x_ofs         = gauge->coords.x1 + r + left + scale_width + txt_pad;
     lv_coord_t y_ofs         = gauge->coords.y1 + r + top + scale_width + txt_pad;
@@ -563,10 +569,10 @@ static void lv_gauge_draw_needle(lv_obj_t * gauge, const lv_area_t * clip_area)
 {
     lv_gauge_ext_t * ext     = lv_obj_get_ext_attr(gauge);
 
-    lv_style_int_t pad = lv_obj_get_style_pad_inner(gauge, LV_GAUGE_PART_NEEDLE);
-    lv_style_int_t left = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_MAIN);
-    lv_style_int_t right = lv_obj_get_style_pad_right(gauge, LV_GAUGE_PART_MAIN);
-    lv_style_int_t top = lv_obj_get_style_pad_top(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t pad = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_NEEDLE);
+    lv_coord_t left = lv_obj_get_style_pad_left(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t right = lv_obj_get_style_pad_right(gauge, LV_GAUGE_PART_MAIN);
+    lv_coord_t top = lv_obj_get_style_pad_top(gauge, LV_GAUGE_PART_MAIN);
 
     lv_coord_t r      = (lv_obj_get_width(gauge) - left - right) / 2 - pad;
     lv_coord_t x_ofs  = gauge->coords.x1 + r + left + pad;
@@ -632,7 +638,7 @@ static void lv_gauge_draw_needle(lv_obj_t * gauge, const lv_area_t * clip_area)
     lv_draw_rect_dsc_t mid_dsc;
     lv_draw_rect_dsc_init(&mid_dsc);
     lv_obj_init_draw_rect_dsc(gauge, LV_GAUGE_PART_NEEDLE, &mid_dsc);
-    lv_style_int_t size = lv_obj_get_style_size(gauge, LV_GAUGE_PART_NEEDLE) / 2;
+    lv_coord_t size = lv_obj_get_style_size(gauge, LV_GAUGE_PART_NEEDLE) / 2;
     lv_area_t nm_cord;
     nm_cord.x1 = x_ofs - size;
     nm_cord.y1 = y_ofs - size;
