8fdc4477f61e69ce63a21b9bba92d8e4494c7088
openvela-robot <openvela-robot@xiaomi.com>
2021-01-08 21:30:14 +0800

    change data inheritance model (drop ext)

--- src/lv_draw/lv_draw_rect.h
+++ src/lv_draw/lv_draw_rect.h
@@ -24,61 +24,54 @@ extern "C" {
  **********************/
 
 typedef struct {
-    lv_style_int_t radius;
+    lv_coord_t radius;
 
     /*Background*/
     lv_color_t bg_color;
     lv_color_t bg_grad_color;
     lv_grad_dir_t bg_grad_dir;
-    lv_style_int_t bg_main_color_stop;
-    lv_style_int_t bg_grad_color_stop;
+    uint8_t bg_main_color_stop;
+    uint8_t bg_grad_color_stop;
     lv_opa_t bg_opa;
     lv_blend_mode_t bg_blend_mode;
 
     /*Border*/
     lv_color_t border_color;
-    lv_style_int_t border_width;
-    lv_style_int_t border_side;
+    lv_coord_t border_width;
+    lv_border_side_t border_side;
     lv_opa_t border_opa;
     lv_blend_mode_t border_blend_mode;
     uint8_t border_post : 1;        /*There is a border it will be drawn later. */
 
     /*Outline*/
     lv_color_t outline_color;
-    lv_style_int_t outline_width;
-    lv_style_int_t outline_pad;
+    lv_coord_t outline_width;
+    lv_coord_t outline_pad;
     lv_opa_t outline_opa;
     lv_blend_mode_t outline_blend_mode;
 
     /*Shadow*/
     lv_color_t shadow_color;
-    lv_style_int_t shadow_width;
-    lv_style_int_t shadow_ofs_x;
-    lv_style_int_t shadow_ofs_y;
-    lv_style_int_t shadow_spread;
+    lv_coord_t shadow_width;
+    lv_coord_t shadow_ofs_x;
+    lv_coord_t shadow_ofs_y;
+    lv_coord_t shadow_spread;
     lv_opa_t shadow_opa;
     lv_blend_mode_t shadow_blend_mode;
 
-    /*Pattern*/
-    const void * pattern_image;
-    const lv_font_t * pattern_font;
-    lv_color_t pattern_recolor;
-    lv_opa_t pattern_opa;
-    lv_opa_t pattern_recolor_opa;
-    uint8_t pattern_repeat : 1;
-    lv_blend_mode_t pattern_blend_mode;
-
-    /*Value*/
-    const char * value_str;
-    const lv_font_t * value_font;
-    lv_opa_t value_opa;
-    lv_color_t value_color;
-    lv_style_int_t value_ofs_x;
-    lv_style_int_t value_ofs_y;
-    lv_style_int_t value_letter_space;
-    lv_style_int_t value_line_space;
-    lv_align_t value_align;
-    lv_blend_mode_t value_blend_mode;
+    /*Content*/
+    const void * content_src;
+    const void * content_img;
+    lv_align_t content_align;
+    lv_coord_t content_ofs_x;
+    lv_coord_t content_ofs_y;
+    lv_opa_t content_opa;
+    const lv_font_t * content_font;
+    lv_opa_t content_recolor_opa;
+    lv_blend_mode_t content_blend_mode;
+    lv_color_t content_color;
+    lv_coord_t content_letter_space;
+    lv_coord_t content_line_space;
 } lv_draw_rect_dsc_t;
 
 /**********************
