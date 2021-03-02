5cbcd556b328753a6a333cd8c22ec9f2c6aa01e7
openvela-robot <openvela-robot@xiaomi.com>
2021-03-03 00:59:00 +0800

    Merge pull request #2120 from xiaoxiang781216/style2
    fix(style): align the enum entry in lv_style_prop_t

--- src/lv_font/lv_font_montserrat_14.c
+++ src/lv_font/lv_font_montserrat_14.c
@@ -2107,10 +2107,8 @@ static const lv_font_fmt_txt_kern_classes_t kern_classes =
  *  ALL CUSTOM DATA
  *--------------------*/
 
-static  lv_font_fmt_txt_glyph_cache_t cache;
-
 /*Store all the custom data of the font*/
-static const lv_font_fmt_txt_dsc_t font_dsc = {
+static lv_font_fmt_txt_dsc_t font_dsc = {
     .glyph_bitmap = gylph_bitmap,
     .glyph_dsc = glyph_dsc,
     .cmaps = cmaps,
@@ -2119,8 +2117,7 @@ static const lv_font_fmt_txt_dsc_t font_dsc = {
     .cmap_num = 2,
     .bpp = 4,
     .kern_classes = 1,
-    .bitmap_format = 0,
-    .cache = &cache
+    .bitmap_format = 0
 };
 
 /*-----------------
@@ -2128,7 +2125,7 @@ static const lv_font_fmt_txt_dsc_t font_dsc = {
  *----------------*/
 
 /*Initialize a public general font descriptor*/
-const lv_font_t lv_font_montserrat_14 = {
+lv_font_t lv_font_montserrat_14 = {
     .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
     .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
     .line_height = 16,          /*The maximum line height required by the font*/
