5e916c61ccc2616b42d86dfe111b561f55dc1c00
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2021-03-16 16:31:32 +0100

    Release v7.11.0
    Change-Id: I333d54da59eeb5b968739d7c3bea63770e413248

--- src/lv_hal/lv_hal_disp.h
+++ src/lv_hal/lv_hal_disp.h
@@ -21,7 +21,7 @@ extern "C" {
 #include "../lv_misc/lv_color.h"
 #include "../lv_misc/lv_area.h"
 #include "../lv_misc/lv_ll.h"
-#include "../lv_misc/lv_timer.h"
+#include "../lv_misc/lv_task.h"
 
 /*********************
  *      DEFINES
@@ -38,10 +38,8 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
-struct _lv_obj_t;
-struct _lv_disp_t;
-struct _lv_disp_drv_t;
-struct _lv_theme_t;
+struct _disp_t;
+struct _disp_drv_t;
 
 /**
  * Structure for holding display buffer information.
@@ -56,11 +54,11 @@ typedef struct {
     lv_area_t area;
     /*1: flushing is in progress. (It can't be a bit field because when it's cleared from IRQ Read-Modify-Write issue might occur)*/
     volatile int flushing;
-    /*1: It was the last chunk to flush. (It can't be a bit field because when it's cleared from IRQ Read-Modify-Write issue might occur)*/
+    /*1: It was the last chunk to flush. (It can't be a bi tfield because when it's cleared from IRQ Read-Modify-Write issue might occur)*/
     volatile int flushing_last;
     volatile uint32_t last_area         : 1; /*1: the last area is being rendered*/
     volatile uint32_t last_part         : 1; /*1: the last part of the current area is being rendered*/
-} lv_disp_draw_buf_t;
+} lv_disp_buf_t;
 
 
 typedef enum {
@@ -71,111 +69,129 @@ typedef enum {
 } lv_disp_rot_t;
 
 /**
- * Display Driver structure to be registered by HAL.
- * Only its pointer will be saved in `lv_disp_t` so it should be declared as
- * `static lv_disp_drv_t my_drv` or allocated dynamically.
+ * Display Driver structure to be registered by HAL
  */
-typedef struct _lv_disp_drv_t {
+typedef struct _disp_drv_t {
 
     lv_coord_t hor_res; /**< Horizontal resolution. */
     lv_coord_t ver_res; /**< Vertical resolution. */
 
-    /** Pointer to a buffer initialized with `lv_disp_draw_buf_init()`.
+    /** Pointer to a buffer initialized with `lv_disp_buf_init()`.
      * LVGL will use this buffer(s) to draw the screens contents */
-    lv_disp_draw_buf_t * draw_buf;
+    lv_disp_buf_t * buffer;
 
+#if LV_ANTIALIAS
+    uint32_t antialiasing : 1; /**< 1: antialiasing is enabled on this display. */
+#endif
+    uint32_t rotated : 2;
     uint32_t sw_rotate : 1; /**< 1: use software rotation (slower) */
-    uint32_t antialiasing : 1; /**< 1: anti-aliasing is enabled on this display. */
-    uint32_t rotated : 2; /**< 1: turn the display by 90 degree. @warning Does not update coordinates for you!*/
 
+#if LV_COLOR_SCREEN_TRANSP
     /**Handle if the screen doesn't have a solid (opa == LV_OPA_COVER) background.
      * Use only if required because it's slower.*/
     uint32_t screen_transp : 1;
+#endif
 
     /** DPI (dot per inch) of the display.
-     * Set to `LV_DPI_DEF` from `lv_conf.h` by default.
+     * Set to `LV_DPI` from `lv_Conf.h` by default.
      */
     uint32_t dpi : 10;
 
-    /** MANDATORY: Write the internal buffer (draw_buf) to the display. 'lv_disp_flush_ready()' has to be
+    /** MANDATORY: Write the internal buffer (VDB) to the display. 'lv_disp_flush_ready()' has to be
      * called when finished */
-    void (*flush_cb)(struct _lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
+    void (*flush_cb)(struct _disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
 
     /** OPTIONAL: Extend the invalidated areas to match with the display drivers requirements
      * E.g. round `y` to, 8, 16 ..) on a monochrome display*/
-    void (*rounder_cb)(struct _lv_disp_drv_t * disp_drv, lv_area_t * area);
+    void (*rounder_cb)(struct _disp_drv_t * disp_drv, lv_area_t * area);
 
     /** OPTIONAL: Set a pixel in a buffer according to the special requirements of the display
      * Can be used for color format not supported in LittelvGL. E.g. 2 bit -> 4 gray scales
      * @note Much slower then drawing with supported color formats. */
-    void (*set_px_cb)(struct _lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+    void (*set_px_cb)(struct _disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
                       lv_color_t color, lv_opa_t opa);
 
     /** OPTIONAL: Called after every refresh cycle to tell the rendering and flushing time + the
      * number of flushed pixels */
-    void (*monitor_cb)(struct _lv_disp_drv_t * disp_drv, uint32_t time, uint32_t px);
+    void (*monitor_cb)(struct _disp_drv_t * disp_drv, uint32_t time, uint32_t px);
 
     /** OPTIONAL: Called periodically while lvgl waits for operation to be completed.
      * For example flushing or GPU
      * User can execute very simple tasks here or yield the task */
-    void (*wait_cb)(struct _lv_disp_drv_t * disp_drv);
+    void (*wait_cb)(struct _disp_drv_t * disp_drv);
 
     /** OPTIONAL: Called when lvgl needs any CPU cache that affects rendering to be cleaned */
-    void (*clean_dcache_cb)(struct _lv_disp_drv_t * disp_drv);
+    void (*clean_dcache_cb)(struct _disp_drv_t * disp_drv);
 
     /** OPTIONAL: called to wait while the gpu is working */
-    void (*gpu_wait_cb)(struct _lv_disp_drv_t * disp_drv);
+    void (*gpu_wait_cb)(struct _disp_drv_t * disp_drv);
+
+#if LV_USE_GPU
+
+    /** OPTIONAL: Blend two memories using opacity (GPU only)*/
+    void (*gpu_blend_cb)(struct _disp_drv_t * disp_drv, lv_color_t * dest, const lv_color_t * src, uint32_t length,
+                         lv_opa_t opa);
 
     /** OPTIONAL: Fill a memory with a color (GPU only)*/
-    void (*gpu_fill_cb)(struct _lv_disp_drv_t * disp_drv, lv_color_t * dest_buf, lv_coord_t dest_width,
+    void (*gpu_fill_cb)(struct _disp_drv_t * disp_drv, lv_color_t * dest_buf, lv_coord_t dest_width,
                         const lv_area_t * fill_area, lv_color_t color);
+#endif
 
     /** On CHROMA_KEYED images this color will be transparent.
-     * `LV_COLOR_CHROMA_KEY` by default. (lv_conf.h)*/
+     * `LV_COLOR_TRANSP` by default. (lv_conf.h)*/
     lv_color_t color_chroma_key;
 
 #if LV_USE_USER_DATA
-    void * user_data; /**< Custom display driver user data */
+    lv_disp_drv_user_data_t user_data; /**< Custom display driver user data */
 #endif
 
 } lv_disp_drv_t;
+
+struct _lv_obj_t;
 
 /**
  * Display structure.
  * @note `lv_disp_drv_t` should be the first member of the structure.
  */
-typedef struct _lv_disp_t {
+typedef struct _disp_t {
     /**< Driver to the display*/
-    lv_disp_drv_t * driver;
+    lv_disp_drv_t driver;
 
-    /**< A timer which periodically checks the dirty areas and refreshes them*/
-    lv_timer_t * refr_timer;
-
-    /**< The theme assigned to the screen*/
-    struct _lv_theme_t * theme;
+    /**< A task which periodically checks the dirty areas and refreshes them*/
+    lv_task_t * refr_task;
 
     /** Screens of the display*/
-    struct _lv_obj_t ** screens;    /**< Array of screen objects. */
-    struct _lv_obj_t * act_scr;     /**< Currently active screen on this display */
-    struct _lv_obj_t * prev_scr;    /**< Previous screen. Used during screen animations */
-    struct _lv_obj_t * scr_to_load; /**< The screen prepared to load in lv_scr_load_anim*/
-    struct _lv_obj_t * top_layer;   /**< @see lv_disp_get_layer_top */
-    struct _lv_obj_t * sys_layer;   /**< @see lv_disp_get_layer_sys */
-    uint32_t screen_cnt;
-    uint8_t del_prev  : 1;          /**< 1: Automatically delete the previous screen when the screen load animation is ready */
+    lv_ll_t scr_ll;
+    struct _lv_obj_t * act_scr;         /**< Currently active screen on this display */
+    struct _lv_obj_t * prev_scr;        /**< Previous screen. Used during screen animations */
+#if LV_USE_ANIMATION
+    struct _lv_obj_t * scr_to_load;     /**< The screen prepared to load in lv_scr_load_anim*/
+#endif
+    struct _lv_obj_t * top_layer; /**< @see lv_disp_get_layer_top */
+    struct _lv_obj_t * sys_layer; /**< @see lv_disp_get_layer_sys */
 
-    lv_opa_t bg_opa;                /**<Opacity of the background color or wallpaper */
-    lv_color_t bg_color;            /**< Default display color when screens are transparent*/
-    const void * bg_img;            /**< An image source to display as wallpaper*/
+uint8_t del_prev  :
+    1;        /**< 1: Automatically delete the previous screen when the screen load animation is ready */
+
+    lv_color_t bg_color;          /**< Default display color when screens are transparent*/
+    const void * bg_img;       /**< An image source to display as wallpaper*/
+    lv_opa_t bg_opa;              /**<Opacity of the background color or wallpaper */
 
     /** Invalidated (marked to redraw) areas*/
     lv_area_t inv_areas[LV_INV_BUF_SIZE];
     uint8_t inv_area_joined[LV_INV_BUF_SIZE];
-    uint16_t inv_p;
+    uint32_t inv_p : 10;
 
     /*Miscellaneous data*/
-    uint32_t last_activity_time;        /**< Last time when there was activity on this display */
+    uint32_t last_activity_time; /**< Last time there was activity on this display */
 } lv_disp_t;
+
+typedef enum {
+    LV_DISP_SIZE_SMALL,
+    LV_DISP_SIZE_MEDIUM,
+    LV_DISP_SIZE_LARGE,
+    LV_DISP_SIZE_EXTRA_LARGE,
+} lv_disp_size_t;
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -191,7 +207,7 @@ void lv_disp_drv_init(lv_disp_drv_t * driver);
 
 /**
  * Initialize a display buffer
- * @param draw_buf pointer `lv_draw_buf_t` variable to initialize
+ * @param disp_buf pointer `lv_disp_buf_t` variable to initialize
  * @param buf1 A buffer to be used by LVGL to draw the image.
  *             Always has to specified and can't be NULL.
  *             Can be an array allocated by the user. E.g. `static lv_color_t disp_buf1[1024 * 10]`
@@ -204,12 +220,12 @@ void lv_disp_drv_init(lv_disp_drv_t * driver);
  * sent. Set to `NULL` if unused.
  * @param size_in_px_cnt size of the `buf1` and `buf2` in pixel count.
  */
-void lv_disp_draw_buf_init(lv_disp_draw_buf_t * draw_buf, void * buf1, void * buf2, uint32_t size_in_px_cnt);
+void lv_disp_buf_init(lv_disp_buf_t * disp_buf, void * buf1, void * buf2, uint32_t size_in_px_cnt);
 
 /**
  * Register an initialized display driver.
  * Automatically set the first display as active.
- * @param driver pointer to an initialized 'lv_disp_drv_t' variable. Only its pointer is saved!
+ * @param driver pointer to an initialized 'lv_disp_drv_t' variable (can be local variable)
  * @return pointer to the new display or NULL on error
  */
 lv_disp_t * lv_disp_drv_register(lv_disp_drv_t * driver);
@@ -228,7 +244,7 @@ void lv_disp_drv_update(lv_disp_t * disp, lv_disp_drv_t * new_drv);
 void lv_disp_remove(lv_disp_t * disp);
 
 /**
- * Set a default display. The new screens will be created on it by default.
+ * Set a default screen. The new screens will be created on it by default.
  * @param disp pointer to a display
  */
 void lv_disp_set_default(lv_disp_t * disp);
@@ -267,6 +283,12 @@ bool lv_disp_get_antialiasing(lv_disp_t * disp);
  */
 lv_coord_t lv_disp_get_dpi(lv_disp_t * disp);
 
+/**
+ * Get the size category of the display based on it's hor. res. and dpi.
+ * @param disp pointer to a display (NULL to use the default display)
+ * @return LV_DISP_SIZE_SMALL/MEDIUM/LARGE/EXTRA_LARGE
+ */
+lv_disp_size_t lv_disp_get_size_category(lv_disp_t * disp);
 
 /**
  * Set the rotation of this display.
@@ -312,7 +334,7 @@ lv_disp_t * lv_disp_get_next(lv_disp_t * disp);
  * @param disp pointer to a display
  * @return pointer to the internal buffers
  */
-lv_disp_draw_buf_t * lv_disp_get_draw_buf(lv_disp_t * disp);
+lv_disp_buf_t * lv_disp_get_buf(lv_disp_t * disp);
 
 /**
  * Get the number of areas in the buffer
--- src/lv_misc/lv_math.h
+++ src/lv_misc/lv_math.h
@@ -19,6 +19,23 @@ extern "C" {
 /*********************
  *      DEFINES
  *********************/
+#define LV_MATH_MIN(a, b) ((a) < (b) ? (a) : (b))
+#define LV_MATH_MIN3(a, b, c) (LV_MATH_MIN(LV_MATH_MIN(a,b), c))
+#define LV_MATH_MIN4(a, b, c, d) (LV_MATH_MIN(LV_MATH_MIN(a,b), LV_MATH_MIN(c,d)))
+
+#define LV_MATH_MAX(a, b) ((a) > (b) ? (a) : (b))
+#define LV_MATH_MAX3(a, b, c) (LV_MATH_MAX(LV_MATH_MAX(a,b), c))
+#define LV_MATH_MAX4(a, b, c, d) (LV_MATH_MAX(LV_MATH_MAX(a,b), LV_MATH_MAX(c,d)))
+
+#define LV_MATH_ABS(x) ((x) > 0 ? (x) : (-(x)))
+
+#define LV_MATH_UDIV255(x) ((uint32_t)((uint32_t) (x) * 0x8081) >> 0x17)
+
+#define LV_IS_SIGNED(t) (((t)(-1)) < ((t) 0))
+#define LV_UMAX_OF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | (0xFULL << ((sizeof(t) * 8ULL) - 4ULL)))
+#define LV_SMAX_OF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | (0x7ULL << ((sizeof(t) * 8ULL) - 4ULL)))
+#define LV_MAX_OF(t) ((unsigned long) (LV_IS_SIGNED(t) ? LV_SMAX_OF(t) : LV_UMAX_OF(t)))
+
 #define LV_TRIGO_SIN_MAX 32767
 #define LV_TRIGO_SHIFT 15 /**<  >> LV_TRIGO_SHIFT to normalize*/
 
@@ -44,12 +61,7 @@ typedef struct {
  * @param angle
  * @return sinus of 'angle'. sin(-90) = -32767, sin(90) = 32767
  */
-LV_ATTRIBUTE_FAST_MEM int16_t lv_trigo_sin(int16_t angle);
-
-static inline LV_ATTRIBUTE_FAST_MEM int16_t lv_trigo_cos(int16_t angle)
-{
-    return lv_trigo_sin(angle + 90);
-}
+LV_ATTRIBUTE_FAST_MEM int16_t _lv_trigo_sin(int16_t angle);
 
 //! @endcond
 
@@ -62,7 +74,7 @@ static inline LV_ATTRIBUTE_FAST_MEM int16_t lv_trigo_cos(int16_t angle)
  * @param u3 end values in range of [0..LV_BEZIER_VAL_MAX]
  * @return the value calculated from the given parameters in range of [0..LV_BEZIER_VAL_MAX]
  */
-uint32_t lv_bezier3(uint32_t t, uint32_t u0, uint32_t u1, uint32_t u2, uint32_t u3);
+uint32_t _lv_bezier3(uint32_t t, uint32_t u0, uint32_t u1, uint32_t u2, uint32_t u3);
 
 /**
  * Calculate the atan2 of a vector.
@@ -70,7 +82,7 @@ uint32_t lv_bezier3(uint32_t t, uint32_t u0, uint32_t u1, uint32_t u2, uint32_t 
  * @param y
  * @return the angle in degree calculated from the given parameters in range of [0..360]
  */
-uint16_t lv_atan2(int x, int y);
+uint16_t _lv_atan2(int x, int y);
 
 //! @cond Doxygen_Suppress
 
@@ -78,13 +90,13 @@ uint16_t lv_atan2(int x, int y);
  * Get the square root of a number
  * @param x integer which square root should be calculated
  * @param q store the result here. q->i: integer part, q->f: fractional part in 1/256 unit
- * @param mask optional to skip some iterations if the magnitude of the root is known.
+ * @param mask: optional to skip some iterations if the magnitude of the root is known.
  * Set to 0x8000 by default.
  * If root < 16: mask = 0x80
  * If root < 256: mask = 0x800
  * Else: mask = 0x8000
  */
-LV_ATTRIBUTE_FAST_MEM void lv_sqrt(uint32_t x, lv_sqrt_res_t * q, uint32_t mask);
+LV_ATTRIBUTE_FAST_MEM void _lv_sqrt(uint32_t x, lv_sqrt_res_t * q, uint32_t mask);
 
 //! @endcond
 
@@ -94,7 +106,7 @@ LV_ATTRIBUTE_FAST_MEM void lv_sqrt(uint32_t x, lv_sqrt_res_t * q, uint32_t mask)
  * @param power
  * @return base raised to the power exponent
  */
-int64_t lv_pow(int64_t base, int8_t exp);
+int64_t _lv_pow(int64_t base, int8_t exp);
 
 /**
  * Get the mapped of a number given an input and output range
@@ -105,35 +117,11 @@ int64_t lv_pow(int64_t base, int8_t exp);
  * @param max_out max output range
  * @return the mapped number
  */
-int32_t lv_map(int32_t x, int32_t min_in, int32_t max_in, int32_t min, int32_t max);
-
-/**
- * Get a pseudo random number in the given range
- * @param min   the minimum value
- * @param max   the maximum value
- * @return return the random number. min <= return_value <= max
- */
-uint32_t lv_rand(uint32_t min, uint32_t max);
+int32_t _lv_map(int32_t x, int32_t min_in, int32_t max_in, int32_t min, int32_t max);
 
 /**********************
  *      MACROS
  **********************/
-#define LV_MIN(a, b) ((a) < (b) ? (a) : (b))
-#define LV_MIN3(a, b, c) (LV_MIN(LV_MIN(a,b), c))
-#define LV_MIN4(a, b, c, d) (LV_MIN(LV_MIN(a,b), LV_MIN(c,d)))
-
-#define LV_MAX(a, b) ((a) > (b) ? (a) : (b))
-#define LV_MAX3(a, b, c) (LV_MAX(LV_MAX(a,b), c))
-#define LV_MAX4(a, b, c, d) (LV_MAX(LV_MAX(a,b), LV_MAX(c,d)))
-
-#define LV_CLAMP(min, val, max) (LV_MAX(min, (LV_MIN(val, max))))
-
-#define LV_ABS(x) ((x) > 0 ? (x) : (-(x)))
-
-#define LV_IS_SIGNED(t) (((t)(-1)) < ((t) 0))
-#define LV_UMAX_OF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | (0xFULL << ((sizeof(t) * 8ULL) - 4ULL)))
-#define LV_SMAX_OF(t) (((0x1ULL << ((sizeof(t) * 8ULL) - 1ULL)) - 1ULL) | (0x7ULL << ((sizeof(t) * 8ULL) - 4ULL)))
-#define LV_MAX_OF(t) ((unsigned long) (LV_IS_SIGNED(t) ? LV_SMAX_OF(t) : LV_UMAX_OF(t)))
 
 #ifdef __cplusplus
 } /* extern "C" */
