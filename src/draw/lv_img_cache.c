a1aa5f9393d27f89ee67ed15d76aa63b6f5fd988
openvela-robot <openvela-robot@xiaomi.com>
2021-04-15 18:32:14 +0800

    Merge branch 'feat/new-event'

--- src/draw/lv_img_cache.c
+++ src/draw/lv_img_cache.c
@@ -60,7 +60,7 @@
  * @param color color The color of the image with `LV_IMG_CF_ALPHA_...`
  * @return pointer to the cache entry or NULL if can open the image
  */
-lv_img_cache_entry_t * _lv_img_cache_open(const void * src, lv_color_t color, int32_t frame_id)
+lv_img_cache_entry_t * _lv_img_cache_open(const void * src, lv_color_t color)
 {
     /*Is the image cached?*/
     lv_img_cache_entry_t * cached_src = NULL;
@@ -83,7 +83,6 @@ lv_img_cache_entry_t * _lv_img_cache_open(const void * src, lv_color_t color, in
 
     for(i = 0; i < entry_cnt; i++) {
         if(color.full == cache[i].dec_dsc.color.full &&
-           frame_id == cache[i].dec_dsc.frame_id &&
            lv_img_cache_match(src, cache[i].dec_dsc.src)) {
             /*If opened increment its life.
              *Image difficult to open should live longer to keep avoid frequent their recaching.
@@ -120,7 +119,7 @@ lv_img_cache_entry_t * _lv_img_cache_open(const void * src, lv_color_t color, in
 #endif
     /*Open the image and measure the time to open*/
     uint32_t t_start  = lv_tick_get();
-    lv_res_t open_res = lv_img_decoder_open(&cached_src->dec_dsc, src, color, frame_id);
+    lv_res_t open_res = lv_img_decoder_open(&cached_src->dec_dsc, src, color);
     if(open_res == LV_RES_INV) {
         LV_LOG_WARN("Image draw cannot open the image resource");
         lv_memset_00(cached_src, sizeof(lv_img_cache_entry_t));
