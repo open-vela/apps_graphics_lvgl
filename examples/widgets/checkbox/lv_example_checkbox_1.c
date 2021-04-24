806bb90875d7ec8d65ce4730188465cddd2b674d
openvela-robot <openvela-robot@xiaomi.com>
2021-04-24 17:34:09 +0800

    refactor(color) small fixes (#2211)

--- examples/widgets/checkbox/lv_example_checkbox_1.c
+++ examples/widgets/checkbox/lv_example_checkbox_1.c
@@ -36,8 +36,6 @@ void lv_example_checkbox_1(void)
     lv_obj_add_state(cb, LV_STATE_CHECKED | LV_STATE_DISABLED);
     lv_checkbox_set_text(cb, "Melon");
     lv_obj_add_event_cb(cb, event_handler, LV_EVENT_ALL, NULL);
-
-    lv_obj_update_layout(cb);
 }
 
 #endif
