998a0ba0307629c132f58156638272bfdc93d545
openvela-robot <openvela-robot@xiaomi.com>
2023-12-25 21:08:00 +0800

    fix(event): use 16bit for lvgl event to avoid overlowing
    For applications need more event, extend the mask to use bit15 to allow
    more events.
    
    Signed-off-by: Xuxingliang <xuxingliang@xiaomi.com>

--- src/draw/vg_lite/lv_draw_vg_lite_label.c
+++ src/draw/vg_lite/lv_draw_vg_lite_label.c
@@ -137,7 +137,7 @@ static void draw_letter_bitmap(lv_draw_vg_lite_unit_t * u, const lv_draw_glyph_d
     lv_vg_lite_buffer_init(
         &src_buf,
         dsc->bitmap,
-        lv_vg_lite_width_align(lv_area_get_width(&image_area)),
+        lv_area_get_width(&image_area),
         lv_area_get_height(&image_area),
         VG_LITE_A8,
         false);
@@ -215,7 +215,6 @@ static void draw_letter_outline(lv_draw_vg_lite_unit_t * u, const lv_draw_glyph_
 
     lv_vg_lite_path_t * outline = (lv_vg_lite_path_t *)dsc->bitmap;
     lv_point_t pos = {dsc->letter_coords->x1, dsc->letter_coords->y1};
-
     /* calc convert matrix */
     float scale = FT_F26DOT6_TO_PATH_SCALE(lv_freetype_outline_get_scale(dsc->g->resolved_font));
     vg_lite_matrix_t matrix;
