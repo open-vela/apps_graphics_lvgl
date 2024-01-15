9b2627ccfb9add7e4b37cf30a5e151c283ef5317
Gabriel Wang <gabriel.wang@arm.com>
2024-01-15 18:50:03 +0000

    chore(cmsis-pack): prepare for LVGL9 release (#5323)
    Co-authored-by: Gabor Kiss-Vamosi <kisvegabor@gmail.com>

--- src/draw/sw/blend/arm2d/lv_blend_arm2d.h
+++ src/draw/sw/blend/arm2d/lv_blend_arm2d.h
@@ -1,10 +1,10 @@
 /**
- * @file lv_blend_to_helium.h
+ * @file lv_blend_arm2d.h
  *
  */
 
-#ifndef LV_BLEND_TO_HELIUM_H
-#define LV_BLEND_TO_HELIUM_H
+#ifndef LV_BLEND_ARM2D_H
+#define LV_BLEND_ARM2D_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,11 +16,7 @@ extern "C" {
 
 #include "../../../../lv_conf_internal.h"
 
-#ifdef LV_DRAW_SW_HELIUM_CUSTOM_INCLUDE
-#include LV_DRAW_SW_HELIUM_CUSTOM_INCLUDE
-#endif
-
-#if LV_USE_DRAW_ARM2D
+#if LV_USE_DRAW_ARM2D_SYNC
 
 #define __ARM_2D_IMPL__
 #include "arm_2d.h"
@@ -1436,10 +1432,10 @@ static inline lv_result_t _lv_argb8888_blend_normal_to_argb8888_mix_mask_opa_hel
  *      MACROS
  **********************/
 
-#endif /* LV_USE_DRAW_ARM2D */
+#endif /* LV_USE_DRAW_ARM2D_SYNC */
 
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
 
-#endif /*LV_BLEND_TO_HELIUM_H*/
+#endif /*LV_BLEND_ARM2D_H*/
