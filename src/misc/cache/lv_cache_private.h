b4f8865e904268ae63af1109819e51a68c189300
Benign X <1341398182@qq.com>
2024-02-24 21:28:16 +0800

    doc(cache): add doc strings for cache APIs (#5718)

--- src/misc/cache/lv_cache_private.h
+++ src/misc/cache/lv_cache_private.h
@@ -92,8 +92,20 @@ typedef void (*lv_cache_remove_cb_t)(lv_cache_t * cache, lv_cache_entry_t * entr
  */
 typedef void (*lv_cache_drop_cb_t)(lv_cache_t * cache, const void * key, void * user_data);
 
-typedef void (*lv_cache_clear_cb_t)(lv_cache_t * cache, void * user_data);
+/**
+ * The cache drop all function, used by the cache class to remove all cache entries from the cache and free the memory.
+ */
+typedef void (*lv_cache_drop_all_cb_t)(lv_cache_t * cache, void * user_data);
+
+/**
+ * The cache get victim function, used by the cache class to get a victim entry to be evicted.
+ */
 typedef lv_cache_entry_t * (*lv_cache_get_victim_cb)(lv_cache_t * cache, void * user_data);
+
+/**
+ * The cache reserve condition function, used by the cache class to check if a new entry can be added to the cache without exceeding its maximum size.
+ * See @lv_cache_reserve_cond_res_t for the possible results.
+ */
 typedef lv_cache_reserve_cond_res_t (*lv_cache_reserve_cond_cb)(lv_cache_t * cache, const void * key, size_t size,
                                                                 void * user_data);
 
