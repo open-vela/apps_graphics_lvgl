7afd291d13c4d19815a9cf9100a2a00bf2b3f012
YanXiaowei <yanxiaowei@xiaomi.com>
2024-04-30 10:58:55 +0800

    Supplement API documentation
    Change-Id: I6fb5fd3c7832d9904be7903a297f66585dba8dfa

--- src/display/lv_display.h
+++ src/display/lv_display.h
@@ -532,6 +532,8 @@ void lv_display_delete_refr_timer(lv_display_t * disp);
 
 /**
  * Register vsync event of a display. `LV_EVENT_VSYNC` event will be sent periodically.
+ * Please don't use it in display event listeners.
+ *
  * @param disp      pointer to a display
  * @param event_cb      an event callback
  * @param user_data     optional user_data
@@ -540,6 +542,8 @@ bool lv_display_register_vsync_event(lv_display_t * disp, lv_event_cb_t event_cb
 
 /**
  * Unregister vsync event of a display. `LV_EVENT_VSYNC` event won't be sent periodically.
+ * Please don't use it in display event listeners.
+ *
  * @param disp      pointer to a display
  * @param event_cb      an event callback
  * @param user_data     optional user_data
