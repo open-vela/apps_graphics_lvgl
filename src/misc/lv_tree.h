22f6708c2fc4b7965dc229aec4bdaa268f76a472
pengyiqiang <pengyiqiang@xiaomi.com>
2024-05-23 12:09:35 +0800

    chore(tree): standard naming
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/misc/lv_tree.h
+++ src/misc/lv_tree.h
@@ -17,9 +17,16 @@ extern "C" {
 #include <stddef.h>
 #include <stdbool.h>
 
+/*********************
+ *      DEFINES
+ *********************/
+
+#define LV_TREE_NODE(n) ((lv_tree_node_t*)(n))
+
 /**********************
  *      TYPEDEFS
  **********************/
+
 struct _lv_tree_node_t;
 
 /**
@@ -48,27 +55,50 @@ enum {
 };
 typedef uint8_t lv_tree_walk_mode_t;
 
+typedef bool (*lv_tree_traverse_cb_t)(const lv_tree_node_t * node, void * user_data);
+typedef bool (*lv_tree_before_cb_t)(const lv_tree_node_t * node, void * user_data);
+typedef void (*lv_tree_after_cb_t)(const lv_tree_node_t * node, void * user_data);
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
 extern const lv_tree_class_t lv_tree_node_class;
 
+/**
+ * @brief Create a tree node
+ * @param class_p pointer to a class of the node
+ * @param parent pointer to the parent node (or NULL if it's the root node)
+ * @return pointer to the new node
+ */
 lv_tree_node_t * lv_tree_node_create(const lv_tree_class_t * class_p, lv_tree_node_t * parent);
 
+/**
+ * @brief Delete a tree node and all its children recursively
+ * @param node pointer to the node to delete
+ */
 void lv_tree_node_delete(lv_tree_node_t * node);
 
-typedef bool (*traverse_cb)(const lv_tree_node_t * node, void * user_data);
-typedef bool (*before_cb)(const lv_tree_node_t * node, void * user_data);
-typedef void (*after_cb)(const lv_tree_node_t * node, void * user_data);
-
-bool lv_tree_walk(const lv_tree_node_t * node, lv_tree_walk_mode_t mode, traverse_cb cb, before_cb bcb, after_cb acb,
-                  void * data);
+/**
+ * @brief Walk the tree recursively and call a callback function on each node
+ * @param node pointer to the root node of the tree
+ * @param mode LV_TREE_WALK_PRE_ORDER or LV_TREE_WALK_POST_ORDER
+ * @param cb callback function to call on each node
+ * @param bcb callback function to call before visiting a node
+ * @param acb callback function to call after visiting a node
+ * @param user_data user data to pass to the callback functions
+ * @return true: traversal is finished; false: traversal breaked
+ */
+bool lv_tree_walk(const lv_tree_node_t * node,
+                  lv_tree_walk_mode_t mode,
+                  lv_tree_traverse_cb_t cb,
+                  lv_tree_before_cb_t bcb,
+                  lv_tree_after_cb_t acb,
+                  void * user_data);
 
 /**********************
  *      MACROS
  **********************/
-#define LV_TREE_NODE(n) ((lv_tree_node_t*)(n))
 
 #ifdef __cplusplus
 } /*extern "C"*/
