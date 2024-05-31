dee7b6c9770092e4ea5e08bc7f3ea4bc39e6cb32
Benign X <1341398182@qq.com>
2024-05-31 19:57:15 +0800

    fix(draw_buf): fix user defined draw_buf alloc/destroy not paired
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- src/libs/bin_decoder/lv_bin_decoder.c
+++ src/libs/bin_decoder/lv_bin_decoder.c
@@ -360,7 +360,7 @@ void lv_bin_decoder_close(lv_image_decoder_t * decoder, lv_image_decoder_dsc_t *
 
     decoder_data_t * decoder_data = dsc->user_data;
     if(decoder_data && decoder_data->decoded_partial) {
-        lv_draw_buf_destroy(decoder_data->decoded_partial);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoder_data->decoded_partial);
         decoder_data->decoded_partial = NULL;
     }
 
@@ -409,7 +409,7 @@ lv_result_t lv_bin_decoder_get_area(lv_image_decoder_t * decoder, lv_image_decod
         decoded = lv_draw_buf_reshape(decoder_data->decoded_partial, cf_decoded, w_px, 1, LV_STRIDE_AUTO);
         if(decoded == NULL) {
             if(decoder_data->decoded_partial != NULL) {
-                lv_draw_buf_destroy(decoder_data->decoded_partial);
+                lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoder_data->decoded_partial);
                 decoder_data->decoded_partial = NULL;
             }
             decoded = lv_draw_buf_create_user(image_cache_draw_buf_handlers, w_px, 1, cf_decoded, LV_STRIDE_AUTO);
@@ -536,8 +536,8 @@ static void free_decoder_data(lv_image_decoder_dsc_t * dsc)
         lv_free(decoder_data->f);
     }
 
-    if(decoder_data->decoded) lv_draw_buf_destroy(decoder_data->decoded);
-    if(decoder_data->decompressed) lv_draw_buf_destroy(decoder_data->decompressed);
+    if(decoder_data->decoded) lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoder_data->decoded);
+    if(decoder_data->decompressed) lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoder_data->decompressed);
     lv_free(decoder_data->palette);
     lv_free(decoder_data);
     dsc->user_data = NULL;
@@ -645,7 +645,7 @@ static lv_result_t decode_indexed(lv_image_decoder_t * decoder, lv_image_decoder
     decoder_data->decoded = decoded; /*Free when decoder closes*/
     if(dsc->src_type == LV_IMAGE_SRC_FILE && !is_compressed) {
         decoder_data->palette = (void *)palette; /*Free decoder data on close*/
-        lv_draw_buf_destroy(draw_buf_indexed);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, draw_buf_indexed);
     }
 
     return LV_RESULT_OK;
@@ -655,7 +655,7 @@ exit_with_buf:
         decoder_data->palette = NULL;
     }
 
-    if(draw_buf_indexed) lv_draw_buf_destroy(draw_buf_indexed);
+    if(draw_buf_indexed) lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, draw_buf_indexed);
     return LV_RESULT_INVALID;
 #else
     LV_UNUSED(stride);
@@ -727,7 +727,7 @@ static lv_result_t load_indexed(lv_image_decoder_t * decoder, lv_image_decoder_d
         res = fs_read_file_at(f, sizeof(lv_image_header_t), data, palette_len, &rn);
         if(res != LV_FS_RES_OK || rn != palette_len) {
             LV_LOG_WARN("Read palette failed: %d", res);
-            lv_draw_buf_destroy(decoded);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoded);
             return LV_RESULT_INVALID;
         }
 
@@ -736,7 +736,7 @@ static lv_result_t load_indexed(lv_image_decoder_t * decoder, lv_image_decoder_d
            lv_fs_tell(f, &data_len) != LV_FS_RES_OK ||
            data_len < decoded->data_size) {
             LV_LOG_WARN("Failed to get file to size");
-            lv_draw_buf_destroy(decoded);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoded);
             return LV_RESULT_INVALID;
         }
 
@@ -746,7 +746,7 @@ static lv_result_t load_indexed(lv_image_decoder_t * decoder, lv_image_decoder_d
         res = fs_read_file_at(f, data_offset, data, data_len, &rn);
         if(res != LV_FS_RES_OK || rn != data_len) {
             LV_LOG_WARN("Read indexed image failed: %d", res);
-            lv_draw_buf_destroy(decoded);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoded);
             return LV_RESULT_INVALID;
         }
 
@@ -787,7 +787,7 @@ static lv_result_t decode_rgb(lv_image_decoder_t * decoder, lv_image_decoder_dsc
     res = fs_read_file_at(f, sizeof(lv_image_header_t), img_data, len, &rn);
     if(res != LV_FS_RES_OK || rn != len) {
         LV_LOG_WARN("Read rgb file failed: %d", res);
-        lv_draw_buf_destroy(decoded);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoded);
         return LV_RESULT_INVALID;
     }
 
@@ -844,7 +844,7 @@ static lv_result_t decode_alpha_only(lv_image_decoder_t * decoder, lv_image_deco
         res = fs_read_file_at(decoder_data->f, sizeof(lv_image_header_t), img_data, file_len, &rn);
         if(res != LV_FS_RES_OK || rn != file_len) {
             LV_LOG_WARN("Read header failed: %d", res);
-            lv_draw_buf_destroy(decoded);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decoded);
             return LV_RESULT_INVALID;
         }
     }
@@ -1106,12 +1106,12 @@ static lv_result_t decompress_image(lv_image_decoder_dsc_t * dsc, const lv_image
         len = lv_rle_decompress(input, input_len, output, out_len, pixel_byte);
         if(len != compressed->decompressed_size) {
             LV_LOG_WARN("Decompress failed: %" LV_PRIu32 ", got: %" LV_PRIu32, out_len, len);
-            lv_draw_buf_destroy(decompressed);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decompressed);
             return LV_RESULT_INVALID;
         }
 #else
         LV_LOG_WARN("RLE decompress is not enabled");
-        lv_draw_buf_destroy(decompressed);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decompressed);
         return LV_RESULT_INVALID;
 #endif
     }
@@ -1123,19 +1123,19 @@ static lv_result_t decompress_image(lv_image_decoder_dsc_t * dsc, const lv_image
         len = LZ4_decompress_safe(input, output, input_len, out_len);
         if(len < 0 || (uint32_t)len != compressed->decompressed_size) {
             LV_LOG_WARN("Decompress failed: %" LV_PRId32 ", got: %" LV_PRId32, out_len, len);
-            lv_draw_buf_destroy(decompressed);
+            lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decompressed);
             return LV_RESULT_INVALID;
         }
 #else
         LV_LOG_WARN("LZ4 decompress is not enabled");
-        lv_draw_buf_destroy(decompressed);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decompressed);
         return LV_RESULT_INVALID;
 #endif
     }
     else {
         LV_UNUSED(img_data);
         LV_LOG_WARN("Unknown compression method: %d", compressed->method);
-        lv_draw_buf_destroy(decompressed);
+        lv_draw_buf_destroy_user(image_cache_draw_buf_handlers, decompressed);
         return LV_RESULT_INVALID;
     }
 
