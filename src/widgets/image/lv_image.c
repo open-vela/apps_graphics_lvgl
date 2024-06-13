9b481432730cacd2305b06744882b37957fbcbbf
VIFEX <vifextech@foxmail.com>
2024-06-14 01:21:08 +0800

    feat(vg_lite): add image clip corner support (#6121)
    Change-Id: I6b106006d5003067d4813e8680f48782fa945eaa

--- src/widgets/image/lv_image.c
+++ src/widgets/image/lv_image.c
@@ -748,6 +748,9 @@ static void draw_image(lv_event_t * e)
             lv_area_t img_area = {obj->coords.x1, obj->coords.y1,
                                   obj->coords.x1 + img->w - 1, obj->coords.y1 + img->h - 1
                                  };
+
+            draw_dsc.clip_radius = lv_obj_get_style_radius(obj, LV_PART_MAIN);
+
             if(img->align < _LV_IMAGE_ALIGN_AUTO_TRANSFORM) {
                 lv_area_align(&obj->coords, &img_area, img->align, img->offset.x, img->offset.y);
             }
