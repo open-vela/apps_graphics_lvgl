4ef7b8bfcb6c33f55651d6847c17629da41d1f3e
pengyiqiang <pengyiqiang@xiaomi.com>
2024-07-12 17:14:03 +0800

    perf(vg_lite): add stroke path cache to improve drawing performance
    Change-Id: I56cbe9fb6eade183840383e5e03d07cb039b283c

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -572,6 +572,16 @@
     #endif
 #endif
 
+/* VG-Lite stroke maximum cache number.
+ */
+#ifndef LV_VG_LITE_STROKE_CACHE_CNT
+    #ifdef CONFIG_LV_VG_LITE_STROKE_CACHE_CNT
+        #define LV_VG_LITE_STROKE_CACHE_CNT CONFIG_LV_VG_LITE_STROKE_CACHE_CNT
+    #else
+        #define LV_VG_LITE_STROKE_CACHE_CNT 32
+    #endif
+#endif
+
 /* Enable A4/A8 image support */
 #ifndef LV_VG_LITE_USE_ALPHA_IMAGE
     #ifdef CONFIG_LV_VG_LITE_USE_ALPHA_IMAGE
