6f336aafc1973205d87a39de1c181b30d858bc5d
Benign X <1341398182@qq.com>
2024-08-03 04:06:53 +0800

    feat(text_word_process): add word_process module
    Change-Id: Ie280960063a3a38d99b895b3c2b4d758541b6456

---
+++ src/misc/lv_text_word_process.h
@@ -0,0 +1,80 @@
+/**
+* @file lv_text_word_process.h
+*
+ */
+
+
+#ifndef LV_TEXT_WORD_PROCESS_H
+#define LV_TEXT_WORD_PROCESS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_types.h"
+
+#include "lv_assert.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+typedef enum {
+    LV_TEXT_WORD_PROCESS_LATIN,
+    LV_TEXT_WORD_PROCESS_CJK,
+    LV_TEXT_WORD_PROCESS_HYPHEN,
+    LV_TEXT_WORD_PROCESS_NUMBER,
+    LV_TEXT_WORD_PROCESS_OPEN_PUNCTUATION,
+    LV_TEXT_WORD_PROCESS_CLOSE_PUNCTUATION,
+    LV_TEXT_WORD_PROCESS_RETURN,
+    LV_TEXT_WORD_PROCESS_NEWLINE,
+    LV_TEXT_WORD_PROCESS_SPACE,
+    LV_TEXT_WORD_PROCESS_TAB,
+    LV_TEXT_WORD_PROCESS_UNKNOWN,
+} lv_text_word_process_word_type_t;
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+typedef struct {
+    uint32_t start;
+    uint32_t end;
+    uint32_t brk;
+} lv_text_position_t;
+
+typedef struct {
+    lv_text_position_t pos;
+    lv_text_word_process_word_type_t type;
+    uint32_t real_width;
+    uint32_t ideal_width;
+} lv_text_word_process_word_info_t;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+lv_iter_t * lv_text_char_process_iter_create(const char * txt);
+
+void lv_text_char_process_iter_destroy(lv_iter_t * iter);
+
+lv_iter_t * lv_text_word_process_iter_create(const char * txt, const lv_font_t * font, int32_t letter_space,
+                                             int32_t remaining_width, uint8_t flag);
+
+void lv_text_word_process_iter_destroy(lv_iter_t * iter);
+
+/*************************
+ *    GLOBAL VARIABLES
+ *************************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LV_TEXT_WORD_PROCESS_H*/
