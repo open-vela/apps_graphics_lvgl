115010849e000d1f7092f49b85cb965e64bb69c5
pengyiqiang <pengyiqiang@xiaomi.com>
2024-08-09 16:15:28 +0800

    feat(draw): add physical clipping area to solve the scaling accuracy problem
    Change-Id: Ia64d4ba727db86634026f3ef2b952fc669ed6555

--- src/widgets/canvas/lv_canvas.c
+++ src/widgets/canvas/lv_canvas.c
@@ -331,6 +331,7 @@ void lv_canvas_init_layer(lv_obj_t * obj, lv_layer_t * layer)
     layer->color_format = header->cf;
     layer->buf_area = canvas_area;
     layer->_clip_area = canvas_area;
+    layer->phy_clip_area = canvas_area;
 }
 
 void lv_canvas_finish_layer(lv_obj_t * canvas, lv_layer_t * layer)
