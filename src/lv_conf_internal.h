5ea83ff9ad0434e8069ef79eae2a6ee4d28fc4c6
liujp <liujp@xiaomi.com>
2024-09-04 20:46:17 +0800

    fix(image): image scale add expand pixel
    Signed-off-by: liujp <liujp@xiaomi.com>

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -28,7 +28,7 @@
 #define LV_DRAW_SW_ASM_HELIUM       2
 #define LV_DRAW_SW_ASM_CUSTOM       255
 
-#define LV_ARRAY_DEFAULT_CAPACITY   4
+#define LV_ARRAY_DEFAULT_CAPACITY   8
 
 /* Handle special Kconfig options */
 #ifndef LV_KCONFIG_IGNORE
