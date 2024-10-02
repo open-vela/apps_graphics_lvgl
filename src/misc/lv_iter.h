4ced0dd0224f109db0fc3800837335dbe178827b
Benign X <1341398182@qq.com>
2024-10-03 00:58:48 +0800

    chore(iter): add more safe checker and const qualifier
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/misc/lv_iter.h
+++ src/misc/lv_iter.h
@@ -48,7 +48,7 @@ lv_iter_t * lv_iter_create(void * instance, uint32_t elem_size, uint32_t context
  * @param iter           `lv_iter_t` object create before
  * @return the iter context
  */
-void * lv_iter_get_context(lv_iter_t * iter);
+void * lv_iter_get_context(const lv_iter_t * iter);
 
 /**
  * Destroy the iterator object, and release the context. Other resources allocated by the user are not released.
