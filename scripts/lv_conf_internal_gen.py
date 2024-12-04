b93baac4c4d4a5ac63a9b428c8859c1bd0b08774
duxiaoliang <duxiaoliang@xiaomi.com>
2024-12-04 20:11:27 +0800

    fix(LV_ARRAY_DEFAULT_CAPACITY):modify LV_ARRAY_DEFAULT_CAPACITY to 4
    Signed-off-by: duxiaoliang <duxiaoliang@xiaomi.com>

--- scripts/lv_conf_internal_gen.py
+++ scripts/lv_conf_internal_gen.py
@@ -50,7 +50,7 @@ fout.write(
 #define LV_DRAW_SW_ASM_HELIUM       2
 #define LV_DRAW_SW_ASM_CUSTOM       255
 
-#define LV_ARRAY_DEFAULT_CAPACITY   8
+#define LV_ARRAY_DEFAULT_CAPACITY   4
 
 /* Handle special Kconfig options */
 #ifndef LV_KCONFIG_IGNORE
