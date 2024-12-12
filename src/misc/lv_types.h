1791baa4fa0f2993cbb8077cf8d168fe9b2f843c
Benign X <1341398182@qq.com>
2024-12-12 16:44:13 +0800

    feat(arc_label): add new widget arc label support

--- src/misc/lv_types.h
+++ src/misc/lv_types.h
@@ -152,6 +152,8 @@ typedef struct _lv_image_decoder_t lv_image_decoder_t;
 struct _lv_event_list_t;
 typedef struct _lv_event_list_t lv_event_list_t;
 
+typedef struct _lv_arc_label_t lv_arc_label_t;
+
 struct _lv_iter_t;
 typedef struct _lv_iter_t lv_iter_t;
 
