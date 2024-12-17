0cb528faeaeda281d094f38a2c90b755f04b655d
zhangjipeng <zhangjipeng@xiaomi.com>
2024-12-17 14:01:51 +0800

    add rgba color attribute for svg
    Signed-off-by: zhangjipeng <zhangjipeng@xiaomi.com>

--- tests/src/lv_test_conf_full.h
+++ tests/src/lv_test_conf_full.h
@@ -81,6 +81,7 @@
 #define LV_USE_LZ4_INTERNAL     1
 #define LV_USE_VECTOR_GRAPHIC   1
 #define LV_USE_SVG              1
+#define LV_USE_SVG_ANIMATION    1
 #define LV_USE_SVG_DEBUG        1
 #define LV_USE_PROFILER         1
 #define LV_PROFILER_INCLUDE     "lv_profiler_builtin.h"
