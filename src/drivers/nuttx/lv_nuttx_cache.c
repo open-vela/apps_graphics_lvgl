7a67a0f55c2309a607d25bcd693b6cd1398af005
pengyiqiang <pengyiqiang@xiaomi.com>
2025-04-07 15:37:03 +0800

    fix(font): add the missing flush cache operation
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/drivers/nuttx/lv_nuttx_cache.c
+++ src/drivers/nuttx/lv_nuttx_cache.c
@@ -18,6 +18,7 @@
  *      DEFINES
  *********************/
 #define image_cache_draw_buf_handlers &(LV_GLOBAL_DEFAULT()->image_cache_draw_buf_handlers)
+#define font_draw_buf_handlers &(LV_GLOBAL_DEFAULT()->font_draw_buf_handlers)
 
 /**********************
  *      TYPEDEFS
@@ -51,6 +52,10 @@ void lv_nuttx_cache_init(void)
     handlers = image_cache_draw_buf_handlers;
     handlers->invalidate_cache_cb = invalidate_cache;
     handlers->flush_cache_cb = flush_cache;
+
+    handlers = font_draw_buf_handlers;
+    handlers->invalidate_cache_cb = invalidate_cache;
+    handlers->flush_cache_cb = flush_cache;
 }
 
 void lv_nuttx_cache_deinit(void)
@@ -60,6 +65,10 @@ void lv_nuttx_cache_deinit(void)
     handlers->flush_cache_cb = NULL;
 
     handlers = image_cache_draw_buf_handlers;
+    handlers->invalidate_cache_cb = NULL;
+    handlers->flush_cache_cb = NULL;
+
+    handlers = font_draw_buf_handlers;
     handlers->invalidate_cache_cb = NULL;
     handlers->flush_cache_cb = NULL;
 }
--- src/libs/tiny_ttf/lv_tiny_ttf.c
+++ src/libs/tiny_ttf/lv_tiny_ttf.c
@@ -372,6 +372,7 @@ static bool tiny_ttf_cache_create_cb(tiny_ttf_cache_data_t * node, void * user_d
     uint32_t stride = draw_buf->header.stride;
     stbtt_MakeGlyphBitmap(info, draw_buf->data, w, h, stride, dsc->scale, dsc->scale, g1);
 
+    lv_draw_buf_flush_cache(draw_buf, NULL);
     node->draw_buf = draw_buf;
     return true;
 }
