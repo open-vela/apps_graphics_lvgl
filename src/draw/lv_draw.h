6b1849ce1e973e5c9ec1d7d747f5cb16625f788b
terry.rong <rongyichang@xiaomi.com>
2025-04-10 17:40:50 +0800

    perf(draw): support draw task dsc alloc together to reduce the malloc call times (#8070)
    Signed-off-by: rongyichang <rongyichang@xiaomi.com>

--- src/draw/lv_draw.h
+++ src/draw/lv_draw.h
@@ -52,7 +52,9 @@ typedef enum {
     LV_DRAW_TASK_TYPE_TRIANGLE,
     LV_DRAW_TASK_TYPE_MASK_RECTANGLE,
     LV_DRAW_TASK_TYPE_MASK_BITMAP,
+#if LV_USE_VECTOR_GRAPHIC
     LV_DRAW_TASK_TYPE_VECTOR,
+#endif
 } lv_draw_task_type_t;
 
 typedef enum {
@@ -249,10 +251,11 @@ void * lv_draw_create_unit(size_t size);
  * Add an empty draw task to the draw task list of a layer.
  * @param layer     pointer to a layer
  * @param coords    the coordinates of the draw task
+ * @param type      current drawing object's type of the draw task
  * @return          the created draw task which needs to be
  *                  further configured e.g. by added a draw descriptor
  */
-lv_draw_task_t * lv_draw_add_task(lv_layer_t * layer, const lv_area_t * coords);
+lv_draw_task_t * lv_draw_add_task(lv_layer_t * layer, const lv_area_t * coords, lv_draw_task_type_t type);
 
 /**
  * Needs to be called when a draw task is created and configured.
