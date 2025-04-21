af45221bd5f48faccdfa5a16a12dc9eb3ed4d721
pengyiqiang <pengyiqiang@xiaomi.com>
2025-04-21 11:26:03 +0800

    feat(clang-format.sh): add lv_conf_template.h sync check
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- clang-format.sh
+++ clang-format.sh
@@ -6,11 +6,15 @@ cd ${CUR_DIR}
 # run LVGL code formatter
 ${CUR_DIR}/scripts/code-format.py
 
+# generate lv_conf_internal.h
+${CUR_DIR}/scripts/lv_conf_internal_gen.py
+
 # set the option to fail a pipeline if any command within it fails
 set -o pipefail
 
 if ! (git diff --exit-code --color=always | tee /tmp/lvgl_diff.patch); then
-    echo "Please apply the preceding diff to your code or run scripts/code-format.py"
+    echo "Please apply the preceding diff to your code."
+    echo "Or run scripts/code-format.py and scripts/lv_conf_internal_gen.py to update your code."
     exit 1
 fi
 
