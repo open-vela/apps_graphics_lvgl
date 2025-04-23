d66bc79baba1da23836371531c1fe71c6500bfa8
Zhe Weng <wengzhe@xiaomi.com>
2025-04-23 16:44:21 +0800

    chore(nuttx): deprecate uv vsync init with uv init partial
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- src/drivers/nuttx/lv_nuttx_libuv.h
+++ src/drivers/nuttx/lv_nuttx_libuv.h
@@ -29,6 +29,20 @@ extern "C" {
  *      TYPEDEFS
  **********************/
 
+typedef enum {
+    LV_NUTTX_UV_INIT_NONE = 0,
+    LV_NUTTX_UV_INIT_TIMER = 1 << 0,
+    LV_NUTTX_UV_INIT_FB = 1 << 1,
+    LV_NUTTX_UV_INIT_INPUT = 1 << 2,
+    LV_NUTTX_UV_INIT_UINPUT = 1 << 3,
+    LV_NUTTX_UV_INIT_MOUSE = 1 << 4,
+    LV_NUTTX_UV_INIT_CONTROL = 1 << 5,
+    LV_NUTTX_UV_INIT_VSYNC = 1 << 6,
+    LV_NUTTX_UV_INIT_ALL = LV_NUTTX_UV_INIT_TIMER | LV_NUTTX_UV_INIT_FB | LV_NUTTX_UV_INIT_INPUT |
+                           LV_NUTTX_UV_INIT_UINPUT | LV_NUTTX_UV_INIT_MOUSE | LV_NUTTX_UV_INIT_CONTROL |
+                           LV_NUTTX_UV_INIT_VSYNC,
+} lv_nuttx_uv_init_t;
+
 typedef struct {
     void * loop;
     lv_display_t * disp;
@@ -53,20 +67,25 @@ typedef struct {
 void * lv_nuttx_uv_init(lv_nuttx_uv_t * uv_info);
 
 /**
+ * Initialize the uv_loop using the provided configuration information.
+ * @param uv_info Pointer to the lv_nuttx_uv_t structure to be initialized.
+ * @param partial The partial mode to initialize the uv_loop.
+ */
+void * lv_nuttx_uv_init_partial(lv_nuttx_uv_t * uv_info, lv_nuttx_uv_init_t partial);
+
+/**
  * Deinitialize the uv_loop configuration for NuttX porting layer.
  * @param data Pointer to user data.
  */
 void lv_nuttx_uv_deinit(void ** data);
 
 /**
- * Initialize the uv vsync if you just need to initialize the vsync only.
- * @param uv_vsync Pointer to the lv_nuttx_uv_vsync_t structure.
+ * @deprecated Use lv_nuttx_uv_init_partial() and lv_nuttx_uv_deinit() instead.
  */
 void * lv_nuttx_uv_vsync_init(lv_nuttx_uv_vsync_t * uv_vsync);
 
 /**
- * Deinitialize the uv vsync configuration for NuttX porting layer.
- * @param data Pointer to user data.
+ * @deprecated Use lv_nuttx_uv_init_partial() and lv_nuttx_uv_deinit() instead.
  */
 void lv_nuttx_uv_vsync_deinit(void ** data);
 /**********************
