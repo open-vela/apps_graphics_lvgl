a49f0a6599010d60198654b404f0b96567866040
pengyiqiang <pengyiqiang@xiaomi.com>
2025-04-23 16:00:40 +0800

    perf(vg_lite): add grad pool to optimize memory allocation
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/draw/vg_lite/lv_draw_vg_lite_triangle.c
+++ src/draw/vg_lite/lv_draw_vg_lite_triangle.c
@@ -72,7 +72,7 @@ void lv_draw_vg_lite_triangle(lv_draw_unit_t * draw_unit, const lv_draw_triangle
     if(dsc->bg_grad.dir != LV_GRAD_DIR_NONE) {
 #if LV_USE_VECTOR_GRAPHIC
         lv_vg_lite_draw_grad_helper(
-            u,
+            u->grad_ctx,
             &u->target_buffer,
             lv_vg_lite_path_get_path(path),
             &tri_area,
