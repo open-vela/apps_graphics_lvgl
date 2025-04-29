5a182d8a1fc87a881d39a33ae099c7a6fa79b372
liujp <liujp@xiaomi.com>
2025-04-29 19:08:49 +0800

    fix(textarea): fill letter space with selection bg color
    Signed-off-by: liujp <liujp@xiaomi.com>

--- src/draw/lv_draw_label.c
+++ src/draw/lv_draw_label.c
@@ -469,9 +469,9 @@ void lv_draw_label_iterate_characters(lv_draw_unit_t * draw_unit, const lv_draw_
             letter_w = lv_font_get_glyph_width(font, letter, letter_next);
 
             /*Always set the bg_coordinates for placeholder drawing*/
-            bg_coords.x1 = pos.x;
+            bg_coords.x1 = pos.x - dsc->letter_space / 2;
             bg_coords.y1 = pos.y;
-            bg_coords.x2 = pos.x + letter_w - 1;
+            bg_coords.x2 = pos.x + letter_w - 1 + (dsc->letter_space + 1) / 2;
             bg_coords.y2 = pos.y + line_height - 1;
 
             if(i >= line_end - line_start) {
