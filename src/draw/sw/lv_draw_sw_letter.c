f9b7cfb72e18cdb390aabe67936c0c94f87aa354
xinbingnan <xinbingnan@xiaomi.com>
2025-04-30 19:46:11 +0800

    fix(draw): reduce duplicate lv_font_get_glyph_bitmap
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- src/draw/sw/lv_draw_sw_letter.c
+++ src/draw/sw/lv_draw_sw_letter.c
@@ -111,7 +111,6 @@ static void LV_ATTRIBUTE_FAST_MEM draw_letter_cb(lv_draw_unit_t * draw_unit, lv_
             case LV_FONT_GLYPH_FORMAT_A8:
             case LV_FONT_GLYPH_FORMAT_IMAGE: {
                     if(glyph_draw_dsc->rotation % 3600 == 0 && glyph_draw_dsc->format != LV_FONT_GLYPH_FORMAT_IMAGE) {
-                        glyph_draw_dsc->glyph_data = lv_font_get_glyph_bitmap(glyph_draw_dsc->g, glyph_draw_dsc->_draw_buf);
                         lv_area_t mask_area = *glyph_draw_dsc->letter_coords;
                         mask_area.x2 = mask_area.x1 + lv_draw_buf_width_to_stride(lv_area_get_width(&mask_area), LV_COLOR_FORMAT_A8) - 1;
                         lv_draw_sw_blend_dsc_t blend_dsc;
@@ -127,7 +126,6 @@ static void LV_ATTRIBUTE_FAST_MEM draw_letter_cb(lv_draw_unit_t * draw_unit, lv_
                         lv_draw_sw_blend(draw_unit, &blend_dsc);
                     }
                     else {
-                        glyph_draw_dsc->glyph_data = lv_font_get_glyph_bitmap(glyph_draw_dsc->g, glyph_draw_dsc->_draw_buf);
                         lv_draw_image_dsc_t img_dsc;
                         lv_draw_image_dsc_init(&img_dsc);
                         img_dsc.rotation = glyph_draw_dsc->rotation;
