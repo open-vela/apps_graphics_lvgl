e44929e1fe7390bf6ff472648a318ce8af84e785
pengyiqiang <pengyiqiang@xiaomi.com>
2025-05-19 11:24:41 +0800

    chore: rename clang-format.sh -> custom_check.sh & add style api check
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- custom_check.sh
+++ custom_check.sh
@@ -9,12 +9,18 @@ ${CUR_DIR}/scripts/code-format.py
 # generate lv_conf_internal.h
 ${CUR_DIR}/scripts/lv_conf_internal_gen.py
 
+# generate style API
+${CUR_DIR}/scripts/style_api_gen.py
+
 # set the option to fail a pipeline if any command within it fails
 set -o pipefail
 
 if ! (git diff --exit-code --color=always | tee /tmp/lvgl_diff.patch); then
     echo "Please apply the preceding diff to your code."
-    echo "Or run scripts/code-format.py and scripts/lv_conf_internal_gen.py to update your code."
+    echo "Or run the following commands to update your code:"
+    echo "    scripts/code-format.py             # Update code formatting"
+    echo "    scripts/lv_conf_internal_gen.py    # Generate lv_conf_internal.h"
+    echo "    scripts/style_api_gen.py           # Generate style API"
     exit 1
 fi
 
