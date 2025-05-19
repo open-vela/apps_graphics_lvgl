02b4c40f880fd3935c1762950738bb82ec349d06
pengyiqiang <pengyiqiang@xiaomi.com>
2025-05-19 11:13:58 +0800

    fix(style_api_gen): add image color key style define
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/misc/lv_style_gen.c
+++ src/misc/lv_style_gen.c
@@ -6,7 +6,9 @@
  **********************************************************************
  */
 
+
 #include "lv_style.h"
+
 
 void lv_style_set_width(lv_style_t * style, int32_t value)
 {
@@ -448,16 +450,6 @@ void lv_style_set_bg_image_recolor_opa(lv_style_t * style, lv_opa_t value)
 
 const lv_style_prop_t _lv_style_const_prop_id_BG_IMAGE_RECOLOR_OPA = LV_STYLE_BG_IMAGE_RECOLOR_OPA;
 
-void lv_style_set_image_colorkey(lv_style_t * style, const lv_image_colorkey_t * value)
-{
-    lv_style_value_t v = {
-        .ptr = value
-    };
-    lv_style_set_prop(style, LV_STYLE_IMAGE_COLORKEY, v);
-}
-
-const lv_style_prop_t _lv_style_const_prop_id_IMAGE_COLORKEY = LV_STYLE_IMAGE_COLORKEY;
-
 void lv_style_set_bg_image_tiled(lv_style_t * style, bool value)
 {
     lv_style_value_t v = {
@@ -647,6 +639,16 @@ void lv_style_set_image_recolor_opa(lv_style_t * style, lv_opa_t value)
 }
 
 const lv_style_prop_t _lv_style_const_prop_id_IMAGE_RECOLOR_OPA = LV_STYLE_IMAGE_RECOLOR_OPA;
+
+void lv_style_set_image_colorkey(lv_style_t * style, const lv_image_colorkey_t * value)
+{
+    lv_style_value_t v = {
+        .ptr = value
+    };
+    lv_style_set_prop(style, LV_STYLE_IMAGE_COLORKEY, v);
+}
+
+const lv_style_prop_t _lv_style_const_prop_id_IMAGE_COLORKEY = LV_STYLE_IMAGE_COLORKEY;
 
 void lv_style_set_line_width(lv_style_t * style, int32_t value)
 {
@@ -1122,3 +1124,4 @@ void lv_style_set_grid_cell_row_span(lv_style_t * style, int32_t value)
 
 const lv_style_prop_t _lv_style_const_prop_id_GRID_CELL_ROW_SPAN = LV_STYLE_GRID_CELL_ROW_SPAN;
 #endif /*LV_USE_GRID*/
+
