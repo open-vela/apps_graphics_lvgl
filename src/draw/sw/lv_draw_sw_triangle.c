b5810a99e91a751967fec04114105f7638b01c6c
jianglianfang <jianglianfang@xiaomi.com>
2025-05-26 11:50:05 +0800

    fix(draw): init mask_stride
    Signed-off-by: jianglianfang <jianglianfang@xiaomi.com>

--- src/draw/sw/lv_draw_sw_triangle.c
+++ src/draw/sw/lv_draw_sw_triangle.c
@@ -128,6 +128,7 @@ void lv_draw_sw_triangle(lv_draw_unit_t * draw_unit, const lv_draw_triangle_dsc_
     blend_dsc.mask_buf = mask_buf;
     blend_dsc.blend_area = &blend_area;
     blend_dsc.mask_area = &blend_area;
+    blend_dsc.mask_stride = 0;
     blend_dsc.blend_mode = LV_BLEND_MODE_NORMAL;
     blend_dsc.src_buf = NULL;
 
