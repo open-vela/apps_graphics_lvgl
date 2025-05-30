fa10cab05842f95cafa7fdc5f374cbe8ebca5045
Benign X <1341398182@qq.com>
2025-05-30 16:36:41 +0800

    chore(arc_label): rename arc_label to arclabel
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

--- examples/widgets/lv_example_widgets.h
+++ examples/widgets/lv_example_widgets.h
@@ -30,6 +30,8 @@ void lv_example_animimg_1(void);
 void lv_example_arc_1(void);
 void lv_example_arc_2(void);
 
+void lv_example_arclabel_1(void);
+
 void lv_example_bar_1(void);
 void lv_example_bar_2(void);
 void lv_example_bar_3(void);
