10b7f41b50c65436ece47d89c0a8dd81e4723ec4
mengqingtao1 <mengqingtao1@xiaomi.com>
2025-05-30 17:09:07 +0800

    feat(vg_lite): support tiled image
    Signed-off-by: mengqingtao1 <mengqingtao1@xiaomi.com>
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- src/libs/bin_decoder/lv_bin_decoder.c
+++ src/libs/bin_decoder/lv_bin_decoder.c
@@ -341,6 +341,11 @@ lv_result_t lv_bin_decoder_open(lv_image_decoder_t * decoder, lv_image_decoder_d
     }
     dsc->decoded = adjusted;
 
+    /* Copy user flags to the decoded image */
+    if(dsc->header.flags & LV_IMAGE_FLAGS_USER_MASK) {
+        lv_draw_buf_set_flag((lv_draw_buf_t *)dsc->decoded, dsc->header.flags & LV_IMAGE_FLAGS_USER_MASK);
+    }
+
     /*Do not put image to cache if it can be used directly.*/
     if(use_directly || dsc->args.no_cache) {
         if(dsc->args.flush_cache && use_directly) {
