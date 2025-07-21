lvgl/lvgl

fix(span): fix align text to center and right layout issues. (#7615)
Signed-off-by: liuhongchao <liuhongchao@xiaomi.com>

src/widgets/span/lv_span.c
@@ -1041,6 +1041,12 @@ static void lv_draw_span(lv_obj_t * obj, lv_layer_t * layer)
             uint32_t i;
             for(i = 0; i < item_cnt; i++) {
                 lv_snippet_t * pinfo = lv_get_snippet(i);
+                if(ellipsis_valid && i == item_cnt - 1) {
+                    uint32_t n_ofs = 0;
+                    lv_text_get_snippet(pinfo->txt, pinfo->font, pinfo->letter_space, max_width - txts_w,
+                                        LV_TEXT_FLAG_BREAK_ALL, &pinfo->txt_w, &n_ofs);
+                    pinfo->bytes = n_ofs;
+                }
                 txts_w = txts_w + pinfo->txt_w;
             }
             txts_w -= lv_get_snippet(item_cnt - 1)->letter_space;

open-vela/apps_graphics_lvgl

src/widgets/span/lv_span.c
@@ -1165,6 +1165,12 @@ static void lv_draw_span(lv_obj_t * obj, lv_layer_t * layer)
         uint32_t i_item;
         for(i_item = 0; i_item < item_cnt; i_item++) {
             lv_snippet_t * pinfo = lv_get_snippet(i_item);
+            if(ellipsis_valid && i_item == item_cnt - 1) {
+                uint32_t n_ofs = 0;
+                lv_text_get_snippet(pinfo->txt, pinfo->font, pinfo->letter_space, max_width - txts_w,
+                                    LV_TEXT_FLAG_BREAK_ALL, &pinfo->txt_w, &n_ofs);
+                pinfo->bytes = n_ofs;
+            }
             txts_w = txts_w + pinfo->txt_w + pinfo->letter_space;
         }
         txts_w -= lv_get_snippet(item_cnt - 1)->letter_space;