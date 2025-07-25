fe6ea795fa1435778cc11de90c332cd05061cf94
liuhongchao <liuhongchao@xiaomi.com>
2025-07-25 15:22:06 +0800

    fix(nuttx_libuv): format code
    Signed-off-by: liuhongchao <liuhongchao@xiaomi.com>

--- src/drivers/nuttx/lv_nuttx_libuv.c
+++ src/drivers/nuttx/lv_nuttx_libuv.c
@@ -82,6 +82,7 @@ static int lv_nuttx_uv_uinput_init(lv_nuttx_uv_t * uv_info, lv_nuttx_uv_ctx_t * 
 static void lv_nuttx_uv_uinput_deinit(lv_nuttx_uv_ctx_t * uv_ctx);
 
 #ifdef CONFIG_LV_USE_NUTTX_MOUSE
+    static void lv_nuttx_uv_mouse_poll_cb(uv_poll_t * handle, int status, int events);
     static int lv_nuttx_uv_mouse_init(lv_nuttx_uv_t * uv_info, lv_nuttx_uv_ctx_t * uv_ctx);
     static void lv_nuttx_uv_mouse_deinit(lv_nuttx_uv_ctx_t * uv_ctx);
 #endif
