b43206da557d0db5551b7716e32c535681b2cedb
zhouxingjian <zhouxingjian@xiaomi.com>
2025-08-22 17:45:56 +0800

    test(screenshot): use lodepng instead of libpng #8275
    Signed-off-by: zhouxingjian <zhouxingjian@xiaomi.com>

--- tests/unity/unity_support.c
+++ tests/unity/unity_support.c
@@ -20,8 +20,12 @@
 #include <stdarg.h>
 #include <sys/stat.h>
 #include "unity.h"
-#define PNG_DEBUG 3
-#include <png.h>
+#include "../../src/libs/lodepng/lodepng.h"
+
+#if LV_USE_LODEPNG == 0
+    #error "lodepng is required for screenshot compare. Enable it in lv_conf.h (LV_USE_LODEPNG 1)"
+#endif
+
 
 /*********************
  *      DEFINES
@@ -41,24 +45,14 @@
 /**********************
  *      TYPEDEFS
  **********************/
-typedef struct {
-    int width, height;
-    png_byte color_type;
-    png_byte bit_depth;
-
-    png_structp png_ptr;
-    png_infop info_ptr;
-    int number_of_passes;
-    png_bytep * row_pointers;
-} png_image_t;
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 static bool screenshot_compare(const char * fn_ref, const char * mode, uint8_t tolerance);
-static int read_png_file(png_image_t * p, const char * file_name);
-static int write_png_file(void * raw_img, uint32_t width, uint32_t height, char * file_name);
-static void png_release(png_image_t * p);
+static unsigned read_png_file(lv_draw_buf_t ** refr_draw_buf, unsigned * width, unsigned * height,
+                              const char * file_name);
+static unsigned write_png_file(void * raw_img, uint32_t width, uint32_t height, char * file_name);
 static void buf_to_xrgb8888(const uint8_t * buf_in, uint8_t * buf_out, lv_color_format_t cf_in);
 static void create_folders_if_needed(const char * path) ;
 
@@ -150,29 +144,25 @@ static bool screenshot_compare(const char * fn_ref, const char * mode, uint8_t t
     uint8_t * screen_buf = lv_draw_buf_align(last_flushed_buf, cf);
     buf_to_xrgb8888(screen_buf, screen_buf_xrgb8888, cf);
 
-    png_image_t p;
-    int res = read_png_file(&p, fn_ref_full);
-    if(res == ERR_FILE_NOT_FOUND) {
+    lv_draw_buf_t * ref_draw_buf;
+    unsigned ref_img_width = 0;
+    unsigned ref_img_height = 0;
+    unsigned res = read_png_file(&ref_draw_buf, &ref_img_width, &ref_img_height, fn_ref_full);
+    if(res) {
         TEST_PRINTF("%s%s", fn_ref_full, " was not found, creating is now from the rendered screen");
         fflush(stderr);
         write_png_file(screen_buf_xrgb8888, 800, 480, fn_ref_full);
         return true;
     }
-    else if(res == ERR_PNG) {
-        return false;
-    }
-
-    uint8_t * ptr_act = NULL;
-    const png_byte * ptr_ref = NULL;
 
     bool err = false;
-    int x, y;
-    for(y = 0; y < p.height; y++) {
+    unsigned x, y;
+    for(y = 0; y < ref_img_height; y++) {
         uint8_t * screen_buf_tmp = screen_buf_xrgb8888 + 800 * 4 * y;
-        png_byte * row = p.row_pointers[y];
-        for(x = 0; x < p.width; x++) {
-            ptr_ref = &(row[x * 3]);
-            ptr_act = screen_buf_tmp;
+        uint8_t * ref_row = (uint8_t *)ref_draw_buf->data + y * ref_draw_buf->header.stride;
+        for(x = 0; x < ref_img_width; x++) {
+            uint8_t * ptr_ref = &(ref_row[x * 4]);
+            uint8_t * ptr_act = &screen_buf_tmp[x * 4];
 
             if(LV_ABS((int32_t) ptr_act[0] - (int32_t) ptr_ref[0]) > tolerance ||
                LV_ABS((int32_t) ptr_act[1] - (int32_t) ptr_ref[1]) > tolerance ||
@@ -192,7 +182,6 @@ static bool screenshot_compare(const char * fn_ref, const char * mode, uint8_t t
                 err = true;
                 break;
             }
-            screen_buf_tmp += 4;
         }
         if(err) break;
     }
@@ -208,169 +197,26 @@ static bool screenshot_compare(const char * fn_ref, const char * mode, uint8_t t
         write_png_file(screen_buf_xrgb8888, 800, 480, fn_err_full);
     }
 
-    png_release(&p);
-
     fflush(stdout);
+    lv_draw_buf_destroy(ref_draw_buf);
     return !err;
 
 }
 
-static int read_png_file(png_image_t * p, const char * file_name)
+static unsigned read_png_file(lv_draw_buf_t ** refr_draw_buf, unsigned * width, unsigned * height,
+                              const char * file_name)
 {
-    char header[8];    // 8 is the maximum size that can be checked
-
-    /*open file and test for it being a png*/
-    FILE * fp = fopen(file_name, "rb");
-    if(!fp) {
-        TEST_PRINTF("[read_png_file %s] could not be opened for reading", file_name);
-        return ERR_FILE_NOT_FOUND;
-    }
-
-    size_t rcnt = fread(header, 1, 8, fp);
-    if(rcnt != 8 || png_sig_cmp((png_const_bytep)header, 0, 8)) {
-        TEST_PRINTF("[read_png_file %s]  not recognized as a PNG file", file_name);
-        return ERR_PNG;
-    }
-
-    /*initialize stuff*/
-    p->png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-    if(!p->png_ptr) {
-        TEST_PRINTF("[read_png_file %s] png_create_read_struct failed", file_name);
-        return ERR_PNG;
-    }
-
-    p->info_ptr = png_create_info_struct(p->png_ptr);
-    if(!p->info_ptr) {
-        TEST_PRINTF("[read_png_file %s] png_create_info_struct failed", file_name);
-        return ERR_PNG;
-    }
-    if(setjmp(png_jmpbuf(p->png_ptr))) {
-        TEST_PRINTF("[read_png_file %s] Error during init_io", file_name);
-        return ERR_PNG;
-    }
-    png_init_io(p->png_ptr, fp);
-    png_set_sig_bytes(p->png_ptr, 8);
-
-    png_read_info(p->png_ptr, p->info_ptr);
-
-    p->width = png_get_image_width(p->png_ptr, p->info_ptr);
-    p->height = png_get_image_height(p->png_ptr, p->info_ptr);
-    p->color_type = png_get_color_type(p->png_ptr, p->info_ptr);
-    p->bit_depth = png_get_bit_depth(p->png_ptr, p->info_ptr);
-
-    p->number_of_passes = png_set_interlace_handling(p->png_ptr);
-    png_read_update_info(p->png_ptr, p->info_ptr);
-
-    /*read file*/
-    if(setjmp(png_jmpbuf(p->png_ptr))) {
-        TEST_PRINTF("[read_png_file %s] Error during read_image", file_name);
-        return ERR_PNG;
-    }
-    p->row_pointers = (png_bytep *) malloc(sizeof(png_bytep) * p->height);
-
-    int y;
-    for(y = 0; y < p->height; y++)
-        p->row_pointers[y] = (png_byte *) malloc(png_get_rowbytes(p->png_ptr, p->info_ptr));
-
-    png_read_image(p->png_ptr, p->row_pointers);
-
-    fclose(fp);
-    return 0;
+    unsigned error = lodepng_decode32_file((void *)refr_draw_buf, width, height, file_name);
+    if(error) TEST_PRINTF("error %u: %s\n", error, lodepng_error_text(error));
+    return error;
 }
 
-static int write_png_file(void * raw_img, uint32_t width, uint32_t height, char * file_name)
+static unsigned write_png_file(void * raw_img, uint32_t width, uint32_t height, char * file_name)
 {
-    png_structp png_ptr;
-    png_infop info_ptr;
-
-    /* create file */
-    FILE * fp = fopen(file_name, "wb");
-    if(!fp) {
-        printf("###### %s\n", file_name);
-        fflush(stdout);
-        TEST_PRINTF("[write_png_file %s] could not be opened for writing", file_name);
-        TEST_PRINTF("%s", file_name);
-        return -1;
-    }
-
-    /* initialize stuff */
-    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-    if(!png_ptr) {
-        TEST_PRINTF("[write_png_file %s] png_create_write_struct failed", file_name);
-        return -1;
-    }
-
-    info_ptr = png_create_info_struct(png_ptr);
-    if(!info_ptr) {
-        TEST_PRINTF("[write_png_file %s] png_create_info_struct failed", file_name);
-        return -1;
-    }
-
-    if(setjmp(png_jmpbuf(png_ptr))) {
-        TEST_PRINTF("[write_png_file %s] Error during init_io", file_name);
-        return -1;
-    }
-
-    png_init_io(png_ptr, fp);
-
-    /* write header */
-    if(setjmp(png_jmpbuf(png_ptr))) {
-        TEST_PRINTF("[write_png_file %s] Error during writing header", file_name);
-        return -1;
-    }
-
-    png_set_IHDR(png_ptr, info_ptr, width, height,
-                 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-    png_write_info(png_ptr, info_ptr);
-
-    /* write bytes */
-    if(setjmp(png_jmpbuf(png_ptr))) {
-        TEST_PRINTF("[write_png_file %s] Error during writing bytes", file_name);
-        return -1;
-    }
-
-    uint8_t * raw_img8 = (uint8_t *)raw_img;
-    png_bytep * row_pointers = (png_bytep *) malloc(sizeof(png_bytep) * height);
-    for(uint32_t y = 0; y < height; y++) {
-        row_pointers[y] = malloc(3 * width);
-        uint8_t * line = raw_img8 + y * width * 4;
-        for(uint32_t x = 0; x < width; x++) {
-            row_pointers[y][x * 3 + 0] = line[x * 4 + 0];
-            row_pointers[y][x * 3 + 1] = line[x * 4 + 1];
-            row_pointers[y][x * 3 + 2] = line[x * 4 + 2];
-        }
-    }
-    png_write_image(png_ptr, row_pointers);
-
-    /* end write */
-    if(setjmp(png_jmpbuf(png_ptr))) {
-        TEST_PRINTF("[write_png_file %s] Error during end of write", file_name);
-        return -1;
-    }
-    png_write_end(png_ptr, NULL);
-
-    /* cleanup heap allocation */
-    for(uint32_t y = 0; y < height; y++) free(row_pointers[y]);
-    free(row_pointers);
-
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-
-    fclose(fp);
-    return 0;
-}
-
-static void png_release(png_image_t * p)
-{
-    int y;
-    for(y = 0; y < p->height; y++) free(p->row_pointers[y]);
-
-    free(p->row_pointers);
-
-    png_destroy_read_struct(&p->png_ptr, &p->info_ptr, NULL);
+    TEST_PRINTF("start to write png file: %s, width: %u, height: %u, img: %p\n", file_name, width, height, raw_img);
+    unsigned  error = lodepng_encode32_file(file_name, raw_img, width, height);
+    if(error) TEST_PRINTF("error %u: %s\n", error, lodepng_error_text(error));
+    return error;
 }
 
 static void buf_to_xrgb8888(const uint8_t * buf_in, uint8_t * buf_out, lv_color_format_t cf_in)
@@ -429,25 +275,25 @@ static void buf_to_xrgb8888(const uint8_t * buf_in, uint8_t * buf_out, lv_color_
 static void create_folders_if_needed(const char * path)
 {
     char * ptr;
-    char * pathCopy = strdup(path);
-    if(pathCopy == NULL) {
+    char * path_copy = lv_strdup(path);
+    if(path_copy == NULL) {
         perror("Error duplicating path");
         exit(EXIT_FAILURE);
     }
 
-    char * token = strtok_r(pathCopy, "/", &ptr);
-    char current_path[1024] = {'\0'}; // Adjust the size as needed
+    char * token = strtok_r(path_copy, "/", &ptr);
+    char current_path[1024] = {'\0'}; /* Adjust the size as needed */
     struct stat st;
 
     while(token && ptr && *ptr != '\0') {
-        strcat(current_path, token);
-        strcat(current_path, "/");
+        lv_strcat(current_path, token);
+        lv_strcat(current_path, "/");
 
         if(stat(current_path, &st) != 0) {
             // Folder doesn't exist, create it
             if(mkdir(current_path, 0777) != 0) {
                 perror("Error creating folder");
-                free(pathCopy);
+                lv_free(path_copy);
                 exit(EXIT_FAILURE);
             }
             printf("Created folder: %s\n", current_path);
@@ -456,7 +302,7 @@ static void create_folders_if_needed(const char * path)
         token = strtok_r(NULL, "/", &ptr);
     }
 
-    free(pathCopy);
+    lv_free(path_copy);
 }
 
 #endif
