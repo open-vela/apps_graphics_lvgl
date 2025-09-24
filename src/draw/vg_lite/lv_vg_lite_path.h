f0b11846249087b9cad812112c2af8613fadcaae
VIFEX <vifextech@foxmail.com>
2025-09-24 19:22:17 +0800

    fix(vg_lite): replace FLT_MIN to -FLT_MAX (#8940)
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Co-authored-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Signed-off-by: lijianjun <lijianjun@xiaomi.com>

--- src/draw/vg_lite/lv_vg_lite_path.h
+++ src/draw/vg_lite/lv_vg_lite_path.h
@@ -26,10 +26,10 @@ extern "C" {
 /**
 * It is found that thorvg cannot handle large coordinates well.
 * When the coordinates are larger than 4096, the calculation of tvgSwRle module will overflow in 32-bit system.
-* So we use FLT_MAX and FLT_MIN to write the mark to bounding_box to tell vg_lite_tvg not to add clip path to the current path.
+* So we use FLT_MAX and -FLT_MAX to write the mark to bounding_box to tell vg_lite_tvg not to add clip path to the current path.
 */
 #define PATH_COORD_MAX FLT_MAX
-#define PATH_COORD_MIN FLT_MIN
+#define PATH_COORD_MIN -FLT_MAX
 #else
 /*  18 bits is enough to represent the coordinates of path bounding box */
 #define PATH_COORD_MAX (1 << 18)
