lvgl/lvgl

fix(tests): fix vg-lite buf address not aligned (#7049)
Signed-off-by: FASTSHIFT <vifextech@foxmail.com>
Co-authored-by: pengyiqiang <pengyiqiang@xiaomi.com>

src/draw/lv_draw_buf.h
@@ -48,7 +48,7 @@ LV_EXPORT_CONST_INT(LV_STRIDE_AUTO);
  * For platform that needs special buffer alignment, call LV_DRAW_BUF_INIT_STATIC.
  */
 #define LV_DRAW_BUF_DEFINE_STATIC(name, _w, _h, _cf) \
-    static uint8_t buf_##name[LV_DRAW_BUF_SIZE(_w, _h, _cf)]; \
+    static LV_ATTRIBUTE_MEM_ALIGN uint8_t buf_##name[LV_DRAW_BUF_SIZE(_w, _h, _cf)]; \
     static lv_draw_buf_t name = { \
                                   .header = { \
                                               .magic = LV_IMAGE_HEADER_MAGIC, \

open-vela/apps_graphics_lvgl

Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

src/draw/lv_draw_buf.h
@@ -48,7 +48,7 @@ LV_EXPORT_CONST_INT(LV_STRIDE_AUTO);
  * For platform that needs special buffer alignment, call LV_DRAW_BUF_INIT_STATIC.
  */
 #define LV_DRAW_BUF_DEFINE_STATIC(name, _w, _h, _cf) \
-    static uint8_t buf_##name[_LV_DRAW_BUF_SIZE(_w, _h, _cf)]; \
+    static LV_ATTRIBUTE_MEM_ALIGN uint8_t buf_##name[_LV_DRAW_BUF_SIZE(_w, _h, _cf)]; \
     static lv_draw_buf_t name = { \
                                   .header = { \
                                               .magic = LV_IMAGE_HEADER_MAGIC, \