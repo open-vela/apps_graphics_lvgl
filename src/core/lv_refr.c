lvgl/lvgl

fix(refr): add missing area intersect check (#8240)

src/core/lv_refr.c
@@ -559,7 +559,9 @@ static void refr_sync_areas(void)
         /**
          * @todo Resize SDL window will trigger crash because of sync_area is larger than disp_area
          */
-        lv_area_intersect(sync_area, sync_area, &disp_area);
+        if(!lv_area_intersect(sync_area, sync_area, &disp_area)) {
+            continue;
+        }
 #if LV_DRAW_TRANSFORM_USE_MATRIX
         if(lv_display_get_matrix_rotation(disp_refr)) {
             lv_display_rotate_area(disp_refr, sync_area);

open-vela/apps_graphics_lvgl

Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

src/core/lv_refr.c
@@ -542,7 +542,9 @@ static void refr_sync_areas(void)
         /**
          * @todo Resize SDL window will trigger crash because of sync_area is larger than disp_area
          */
-        _lv_area_intersect(sync_area, sync_area, &disp_area);
+        if(!_lv_area_intersect(sync_area, sync_area, &disp_area)) {
+            continue;
+        }
         lv_draw_buf_copy(off_screen, sync_area, on_screen, sync_area);
     }
 